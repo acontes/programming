8daac9f948e2469c903b590a6e128e9ea75388a1
sbeucler <sbeucler@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-08-11 12:52:57 +0000

    Better exit
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/WS_BRANCH@1559 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/examples/descriptor/MiniDescrClient.java
+++ src/org/objectweb/proactive/examples/descriptor/MiniDescrClient.java
@@ -20,6 +20,8 @@ import java.io.IOException;
  */
 public class MiniDescrClient {
     static Logger logger = Logger.getLogger(MiniDescrClient.class);
+    private static final int NB_THREADS = 10;
+    private static final int NB_CALLS_PER_THREAD = 10;
     MiniDescrActive minidesca = null;
 
     public MiniDescrClient(String location) {
@@ -38,18 +40,32 @@ public class MiniDescrClient {
         virtualnode.activate();
 
         Node[] nodes = null;
+        Thread[] bombs = new Thread[NB_THREADS];
 
         try {
             nodes = virtualnode.getNodes();
 
             for (int i = 0; i < nodes.length; i++) {
-                for (int j = 0; j < 10; j++) {
-                    new Thread(new Bomber(nodes[i])).start();
+                for (int j = 0; j < NB_THREADS; j++) {
+                    bombs[j] = new Thread(new Bomber(nodes[i]));
+                    // bombs[j].start();
+
+                    // Use this line instead to make sequential calls
+                    bombs[j].run();
                 }
             }
-        } catch (NodeException e1) {
+
+            // Wait for all threads to finish before to return
+            for (int j = 0; j < NB_THREADS; j++)
+                try {
+                    bombs[j].join();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+        } catch (NodeException e) {
             // TODO Auto-generated catch block
-            e1.printStackTrace();
+            e.printStackTrace();
         }
     }
 
@@ -60,6 +76,9 @@ public class MiniDescrClient {
         } else {
             new MiniDescrClient(args[0]);
         }
+
+        System.out.println("Done");
+        System.exit(0);
     }
 
     class Bomber implements Runnable {
@@ -74,7 +93,7 @@ public class MiniDescrClient {
                 MiniDescrActive desc = (MiniDescrActive) ProActive.newActive(MiniDescrActive.class.getName(),
                         null, n);
 
-                for (int k = 0; k < 10; k++) {
+                for (int k = 0; k < NB_CALLS_PER_THREAD; k++) {
                     Message msg = desc.getComputerInfo();
                     logger.info("-+-+-+-+-+-+-+- " + msg + " -+-+-+-+-+-+-+-");
                 }
