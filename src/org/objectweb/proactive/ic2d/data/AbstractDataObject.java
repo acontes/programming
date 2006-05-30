e75ea7a4c3e61a11d52542859acc3fd6e72c43f1
mreynier <mreynier@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-05-30 12:44:14 +0000

    ajout de la "nouvelle version d'IC2D"
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/proactive_newIC2D@3430 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/ic2d/data/AbstractDataObject.java
@@ -0,0 +1,220 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2005 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.ic2d.data;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Holder class for the host data representation
+ */
+public abstract class AbstractDataObject {
+
+	/** the object's name */
+	protected String abstractDataObjectName;
+	
+	/** the object's parent */
+	protected AbstractDataObject parent;
+	/** the object's children */
+	// HashMap<String, AbstractDataObject>
+	private HashMap children;
+	
+	/** true if this object has been destroyed, false otherwise */
+	protected boolean isDestroyed;
+	/** true if this object is alive, false otherwise */
+	private boolean isAlive;
+	
+	//
+    // -- CONSTRUCTORS -----------------------------------------------
+    //
+    
+	/**
+	 * TODO comment
+	 * @param parent the object's parent
+	 */
+	protected AbstractDataObject(AbstractDataObject parent) {
+		this(parent, null);
+	}
+	
+	
+	/**
+	 * TODO comment
+	 * @param parent the object's parent
+	 * @param abstractDataObjectName the object's name
+	 */
+	protected AbstractDataObject(AbstractDataObject parent,
+        String abstractDataObjectName) {
+		
+		if (abstractDataObjectName == null) {
+            this.abstractDataObjectName = this.getClass().getName();
+        } else {
+            this.abstractDataObjectName = abstractDataObjectName;
+        }
+
+		this.parent = parent;
+		this.children = new HashMap();
+		
+		this.isDestroyed = false;
+		this.isAlive = true;
+	}
+	
+	//
+    // -- PUBLICS METHODS -----------------------------------------------
+    //
+	
+    /**
+     * Returns the object's key
+     * @return the object's key
+     */
+    public abstract String getKey();
+	
+	/**
+	 * Returns a string representing the object's name and children's names
+	 */
+    public String toString() {
+        return "DataObject " + abstractDataObjectName + "\n" +
+        children.toString();
+    }
+    
+    /**
+     * Returns the object's parent
+     * @return the object's parent
+     */
+    public AbstractDataObject getParent() {
+        return parent;
+    }
+
+    
+    /**
+     * Returns the top level parent
+     * @return the top level parent
+     */
+    public AbstractDataObject getTopLevelParent() {
+        if (parent == null) {
+            return this;
+        } else {
+            return parent.getTopLevelParent();
+        }
+    }
+    
+    
+    /**
+     * Returns an iterator over the object's children
+     * @return an iterator over the object's children
+     */
+    public Iterator childrenIterator() {
+        return children.values().iterator();
+    }
+    
+    
+    /**
+     * Destroys this object
+     */
+    public abstract void destroyObject();
+
+    
+    //
+    // -- PROTECTED METHODS -----------------------------------------------
+    //
+
+    /**
+     * Add a child to this object
+     * @param key 
+     * @param child
+     */
+    protected synchronized void putChild(String key, AbstractDataObject child) {
+        if (isDestroyed) {
+            return;
+        }
+        children.put(key, child);
+    }
+
+    /**
+     * Remove a child
+     * @param key key of the child to remove
+     */
+    protected synchronized AbstractDataObject removeChild(String key) {
+        AbstractDataObject o;
+        if (isDestroyed) {
+            // we are in the Iterator to destroy all children object :
+            // we don't want to remove from the collection not to
+            // have an exception from the iterator
+            o = (AbstractDataObject) children.get(key);
+        } else {
+            // we are asked to remove the child from elsewhere
+            o = (AbstractDataObject) children.remove(key);
+        }
+        if (o == null) {
+            return null;
+        }
+
+        o.destroy();
+        return o;
+    }
+    
+    
+    /**
+     * Destroys this object.
+     */
+    protected synchronized boolean destroy() {
+        if (isDestroyed) {
+            return false;
+        }
+
+        isDestroyed = true;
+        destroyCollection(childrenIterator());
+        children.clear();
+        parent = null;
+        return true;
+    }
+    
+    
+    /**
+     * TODO comment
+     * @param key
+     */
+    protected synchronized AbstractDataObject getChild(String key) {
+        return (AbstractDataObject) children.get(key);
+    }
+    
+    
+    /**
+     * Destroys all objects known by this object
+     * @param iterator an iterator over all objects to destroy
+     */
+    protected synchronized void destroyCollection(Iterator iterator) {
+        while (iterator.hasNext()) {
+            AbstractDataObject o = (AbstractDataObject) iterator.next();
+            o.destroyObject();
+        }
+    }
+    
+}
---
+++ src/org/objectweb/proactive/ic2d/data/HostObject.java
@@ -0,0 +1,95 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2005 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.ic2d.data;
+
+/**
+ * Holder class for the host data representation
+ */
+public class HostObject extends AbstractDataObject {
+
+    /** Name of this Host (machine's name:port) */
+    protected String hostname;
+    
+    /** Name of Operating System */
+    protected String os;
+	
+	//
+    // -- CONSTRUCTORS -----------------------------------------------
+    //
+    
+	public HostObject(WorldObject parent){
+		super(parent);
+		//TODO Change the hostnane!!!
+		this.hostname = "***HostnameTEST***";
+		this.os = "***OsTEST***";
+	}
+	
+    //
+    // -- PUBLIC METHODS -----------------------------------------------
+    //
+	
+	/**
+	 * Destroys this object
+	 */
+	public void destroyObject() {
+		getTypedParent().removeHostObject(hostname);
+	}
+	
+	/**
+	 * @return Name of this Host
+	 */
+	public String getHostName(){
+		return hostname;
+	}
+	
+	/**
+	 * Returns a string representing this host
+	 */
+    public String toString() {
+        return "Host: " + hostname + "\n" + super.toString();
+    }
+    
+    //
+    // -- PROTECTED METHOD -----------------------------------------------
+    //
+    
+    /**
+     * Returns the parent with the real type
+     */
+    protected WorldObject getTypedParent() {
+        return (WorldObject) parent;
+    }
+
+    
+	protected String getKey() {
+		return hostname+":"+os;
+	}
+}
