9ee61eb83c9e20f273ec60982c0521c363094718
jmlegait <jmlegait@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-06-12 08:32:56 +0000

    Improvement of layout managers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/proactive_newIC2D@3494 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/figures/NodeListener.java
@@ -0,0 +1,33 @@
+package org.objectweb.proactive.ic2d.monitoring.figures;
+
+import org.eclipse.draw2d.MouseEvent;
+import org.eclipse.draw2d.MouseMotionListener;
+
+public class NodeListener implements MouseMotionListener{
+
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseHover(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
