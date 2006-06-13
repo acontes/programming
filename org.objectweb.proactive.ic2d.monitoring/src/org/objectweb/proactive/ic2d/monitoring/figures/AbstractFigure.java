bf4b04330a0cd366a9d123ca8cb6641afa3e5de5
mreynier <mreynier@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-06-13 12:36:10 +0000

    Displays views (MVC) in the monitoring view.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/proactive_newIC2D@3510 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/figures/AbstractFigure.java
+++ org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/figures/AbstractFigure.java
@@ -1,3 +1,33 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2005 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.ic2d.monitoring.figures;
 
 import java.util.ArrayList;
@@ -8,20 +38,16 @@ import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
-import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.swt.graphics.Color;
 
 public abstract class AbstractFigure extends Figure{
 	
-	/* The shift to do when we add the first child */
-	private int horizontalShift = 6;
-	private int verticalShift = 20;
-	private int defaultWidth;
-	private int defaultHeight;
+	// the space between top and the first child
+	private final static int topShift = 25;
+	// The space between borders and children
+	private final static int shift = 10;
 	
 	protected List children = new ArrayList();
 	protected AbstractFigure parent;
@@ -35,84 +61,43 @@ public abstract class AbstractFigure extends Figure{
 	
 	protected static boolean showShadow = false; 
 	
-		
-	
 	//
 	// -- CONSTRUCTORS -----------------------------------------------
 	//
-	protected AbstractFigure(String text, Integer posText, int xPos, int yPos, int width, int height){
+	protected AbstractFigure(AbstractFigure parent, String text, Integer textPos, int width, int height){
 		super();
 		
-		this.defaultWidth = width;
-		this.defaultHeight = height;
-		
-			
-		// Add the label
 		BorderLayout layout = new BorderLayout();
-		
-		
-		/*FlowLayout layout = new FlowLayout();
-		 layout.setHorizontal(false);
-		 layout.setMinorAlignment(FlowLayout.ALIGN_CENTER);*/
-		
-		//layout.setMinorSpacing(20);
-		
-		
-		/*layout.setHorizontalSpacing(4);
-		 layout.setVerticalSpacing(4);*/
 		setLayoutManager(layout);
+		
+		// Add the label
 		label.setForegroundColor(ColorConstants.black);
-		add(label, posText);
+		add(label, textPos);
 		
 		// Initialisation
 		initColor();
 		label.setText(text);
-		setBounds(new Rectangle(xPos, yPos, this.defaultWidth, this.defaultHeight));
+		this.setSize(width, height);
 		
 		/* Add child to his parent */
-		/*this.parent = parent;
-		 if(this.parent != null){
-		 this.parent.addFigureChild(this);
-		 }*/
-	}
-	
-	protected AbstractFigure(String text, Integer textPos, int width, int height){
-		this(text, textPos, 0, 0, width, height);
-	}
-	
-	//
-	// -- PROTECTED METHODS --------------------------------------------
-	//
-	
-	protected abstract void initColor();
-	
-	protected abstract void paintIC2DFigure(Graphics graphics);
-	
-	/* Update his size according to his child */
-	protected void updateSize(AbstractFigure child){
-		Dimension childDimension = child.getSize();
-		childDimension.expand(2*this.horizontalShift,2*this.verticalShift);
-		this.setSize(childDimension);
-		
+		this.parent = parent;
 		if(this.parent != null)
-			this.parent.updateSize(this);
+			this.parent.addFigureChild(this);
 	}
 	
-	protected void addFigureChild(AbstractFigure child){
-		if(this != child){
-			children.add(child);
-			this.updateSize(child);
-			child.setLocation(this.getLocation().getTranslated(new Point(this.horizontalShift, this.verticalShift)));
-			
-			add(child);
-		}
+		
+	protected AbstractFigure(AbstractFigure parent, String text, Integer textPos, int x, int y, int width){
+		this(parent, text, textPos, width, topShift);
+		this.setLocation(new Point(x, y));
+	}
+	
+	protected AbstractFigure(AbstractFigure parent, String text, Integer textPos, int width){
+		this(parent, text, textPos, width, topShift);
 	}
 	
 	//
 	// -- PUBLIC METHODS ---------------------------------------------
 	//
-	
-	public abstract String toString();
 	
 	public abstract ConnectionAnchor getAnchor();
 	
@@ -125,8 +110,34 @@ public abstract class AbstractFigure extends Figure{
 		this.label.setText(title);
 	}
 	
-	public IFigure getContentPane(){
-		//TODO
-		return null;
+	//
+	// -- PROTECTED METHODS --------------------------------------------
+	//
+	
+	protected abstract void initColor();
+	
+	protected abstract void paintIC2DFigure(Graphics graphics);
+	
+	/* Update his size according to his child */
+	protected void updateSize(){
+		int height = topShift;
+		for(int i=0 ; i<children.size() ; i++) {
+			height += ((AbstractFigure)children.get(i)).getSize().height + shift;
+		}
+		this.setSize(this.getSize().width, height);
+		if(this.parent != null)
+			this.parent.updateSize();
 	}
+	
+	protected void addFigureChild(AbstractFigure child){
+		if(this != child){
+			children.add(child);
+			child.setLocation(this.getLocation().getTranslated(shift, this.bounds.height));
+			this.setSize(this.getSize().width, this.getSize().height + child.getSize().height + shift);
+			if(this.parent != null)
+				this.parent.updateSize();
+			add(child);
+		}
+	}
+
 }
--- org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/figures/AbstractRectangleFigure.java
+++ org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/figures/AbstractRectangleFigure.java
@@ -1,5 +1,36 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2005 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.ic2d.monitoring.figures;
 
+import org.eclipse.draw2d.BorderLayout;
 import org.eclipse.draw2d.ChopboxAnchor;
 import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.Graphics;
@@ -10,12 +41,12 @@ public abstract class AbstractRectangleFigure extends AbstractFigure{
 	//
 	// -- CONSTRUCTORS -----------------------------------------------
 	//
-	protected AbstractRectangleFigure(String text, Integer posText, int posX, int posY, int width, int height){
-		super(text,posText, posX, posY, width, height);
+	protected AbstractRectangleFigure(AbstractRectangleFigure parent, String text, int posX, int posY, int width){
+		super(parent, text, BorderLayout.TOP, posX, posY, width);
 	}
 	
-	protected AbstractRectangleFigure(String text, Integer textPos, int width, int height){
-		this(text, textPos,0, 0, width, height);
+	protected AbstractRectangleFigure(AbstractRectangleFigure parent, String text, int width){
+		super(parent,text, BorderLayout.TOP, width);
 	}
 	
 	//
