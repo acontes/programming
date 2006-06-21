07d924691d65f127110f8954e31acd33a6859bfd
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-06-21 11:24:27 +0000

    removed collective binding checkings in ADL loader
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/java5@3551 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/adl/bindings/ProActiveTypeBindingLoader.java
+++ src/org/objectweb/proactive/core/component/adl/bindings/ProActiveTypeBindingLoader.java
@@ -7,6 +7,7 @@ import org.objectweb.fractal.adl.bindings.BindingContainer;
 import org.objectweb.fractal.adl.bindings.TypeBindingLoader;
 import org.objectweb.fractal.adl.interfaces.Interface;
 import org.objectweb.fractal.adl.types.TypeInterface;
+import org.objectweb.proactive.core.component.ProActiveInterface;
 import org.objectweb.proactive.core.component.adl.types.ProActiveTypeInterface;
 import org.objectweb.proactive.core.component.controller.MulticastBindingChecker;
 import org.objectweb.proactive.core.component.exceptions.ParameterDispatchException;
@@ -70,26 +71,28 @@ public class ProActiveTypeBindingLoader extends TypeBindingLoader {
                                     "in both interfaces !", (Node) binding);
                         }
 
-                        Map<Method, Method> matchingMethodsForThisItf = new HashMap<Method, Method>(clientSideItfMethods.length);
-
-                        for (Method method : clientSideItfMethods) {
-                            try {
-                                matchingMethodsForThisItf
-                                        .put(method, MulticastBindingChecker.searchMatchingMethod(method, serverSideItfMethods));
-                            } catch (ParameterDispatchException e1) {
-                                throw new ADLException("incompatible binding between multicast client interface " + cItf
-                                        .getName() + " (" + cItf.getSignature()
-                                        + ")  and server interface " + sItf.getName() + " (" + sItf
-                                        .getSignature() + ") : incompatible dispatch " +
-                                        e1.getMessage(), (Node) binding);
-                            } catch (NoSuchMethodException e1) {
-                                throw new ADLException("incompatible binding between multicast client interface " + cItf
-                                        .getName() + " (" + cItf.getSignature()
-                                        + ")  and server interface " + sItf.getName() + " (" + sItf
-                                        .getSignature() + ") : cannot find corresponding method " +
-                                        e1.getMessage(), (Node) binding);
-                            }
-                        }
+//                        Map<Method, Method> matchingMethodsForThisItf = new HashMap<Method, Method>(clientSideItfMethods.length);
+//
+//                        for (Method method : clientSideItfMethods) {
+//                            try {
+////                                matchingMethodsForThisItf
+////                                        .put(method, MulticastBindingChecker.searchMatchingMethod(method, serverSideItfMethods, ProActiveTypeInterface.GATHER_CARDINALITY.equals(sItf.getCardinality()), (ProActiveInterface)toItf));
+//                                matchingMethodsForThisItf
+//                                .put(method, MulticastBindingChecker.searchMatchingMethod(method, serverSideItfMethods, ProActiveTypeInterface.GATHER_CARDINALITY.equals(sItf.getCardinality())));
+//                            } catch (ParameterDispatchException e1) {
+//                                throw new ADLException("incompatible binding between multicast client interface " + cItf
+//                                        .getName() + " (" + cItf.getSignature()
+//                                        + ")  and server interface " + sItf.getName() + " (" + sItf
+//                                        .getSignature() + ") : incompatible dispatch " +
+//                                        e1.getMessage(), (Node) binding);
+//                            } catch (NoSuchMethodException e1) {
+//                                throw new ADLException("incompatible binding between multicast client interface " + cItf
+//                                        .getName() + " (" + cItf.getSignature()
+//                                        + ")  and server interface " + sItf.getName() + " (" + sItf
+//                                        .getSignature() + ") : cannot find corresponding method " +
+//                                        e1.getMessage(), (Node) binding);
+//                            }
+//                        }
                     }
                 }
 
