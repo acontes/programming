d1c2ec1aeaaddaecc9ebceb9c73546dfcaa68ae6
jmlegait <jmlegait@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-07-04 07:39:40 +0000

    Uses a state.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/proactive_newIC2D@3659 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/data/AOObject.java
+++ org.objectweb.proactive.ic2d.monitoring/src/org/objectweb/proactive/ic2d/monitoring/data/AOObject.java
@@ -33,7 +33,12 @@ package org.objectweb.proactive.ic2d.monitoring.data;
 import org.objectweb.proactive.core.UniqueID;
 
 public class AOObject extends AbstractDataObject{
-
+	
+	/**
+	 * State of the object (ex: WAITING_BY_NECESSITY)
+	 */
+	private int state;
+	
 	private static int counter = 0;
 	
 	private UniqueID id;
@@ -43,10 +48,16 @@ public class AOObject extends AbstractDataObject{
     
 	public AOObject(NodeObject parent, String className, UniqueID id){
 		super(parent, className + "#" + counter());
-		System.out.println("AOObject : constructor");
+		System.out.println("AOObject : constructor id="+id.toString());
 		//this.parent.putChild(this);
 		this.id = id;
+		/*
+		if(((NodeObject)this.parent).spy == null)
+			System.err.println("AOObject : constructor => WARNING spy is null");
+		((NodeObject)this.parent).spy.addMessageEventListener(this.id);
+		
 		System.out.println("Constructor AOObject : className = "+className+", id = "+counter);
+		*/
 	}
 	
     //
@@ -59,6 +70,19 @@ public class AOObject extends AbstractDataObject{
 
 	public String getFullName() {
 		return abstractDataObjectName;
+	}
+	
+	/**
+	 * Change the current state
+	 */
+	public void setState(int newState) {
+		this.state = newState;
+		setChanged();
+		notifyObservers(new Integer(this.state));
+	}
+	
+	public int getState(){
+		return this.state;
 	}
 	
 	//
