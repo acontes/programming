9302c017cbe28f3201232fd2737a10fcf24511c5
mreynier <mreynier@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-07-12 12:48:21 +0000

    Logs exceptions
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/proactive_newIC2D@3715 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- org.objectweb.proactive.ic2d.console/src/org/objectweb/proactive/ic2d/console/Console.java
+++ org.objectweb.proactive.ic2d.console/src/org/objectweb/proactive/ic2d/console/Console.java
@@ -46,17 +46,17 @@ import org.eclipse.ui.console.MessageConsoleStream;
  * Used to log informations in a console view.
  */
 public class Console extends MessageConsole {
-	
+
 	/**
 	 * Contains all consoles.
 	 */
 	private static HashMap consoles = new HashMap();
-	
+
 	/**
 	 * To know the date's format.
 	 */
 	private DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-	
+
 	/**
 	 * Some useful colors.
 	 */
@@ -65,7 +65,7 @@ public class Console extends MessageConsole {
 	public static final Color GREEN;
 	public static final Color GRAY;
 	public static final Color BLACK;
-	
+
 	static {
 		Display device = Display.getCurrent();
 		RED = new Color(device, 255, 0, 0);
@@ -78,7 +78,7 @@ public class Console extends MessageConsole {
 	//
 	// -- CONSTRUCTORS -----------------------------------------------
 	//
-	
+
 	/**
 	 * Creates a new Console
 	 * @param title Title of the frame console
@@ -89,11 +89,11 @@ public class Console extends MessageConsole {
 		ConsolePlugin.getDefault().getConsoleManager().addConsoles(
 				new IConsole[]{ this });
 	}
-	
+
 	//
 	// -- PUBLICS METHODS -----------------------------------------------
 	//
-	
+
 	/**
 	 * Returns the console having for title 'title'
 	 * @param title The console's title
@@ -106,17 +106,17 @@ public class Console extends MessageConsole {
 		}
 		return console;
 	}
-	
-	
+
+
 	/**
 	 * Logs a message to the console
 	 * @param message
 	 */
 	public void log(String message){
 		final String text = message;
-		
+
 		printTime();
-		
+
 		// Print the message in the UI Thread in async mode
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
@@ -125,17 +125,17 @@ public class Console extends MessageConsole {
 				stream.println(text);
 			}});
 	}
-	
-	
+
+
 	/**
 	 * Logs an warning message to the console.
 	 * @param message
 	 */
 	public void warn(String message){
 		final String text = message;
-		
+
 		printTime();
-		
+
 		// Print the message in the UI Thread in async mode
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
@@ -144,17 +144,17 @@ public class Console extends MessageConsole {
 				stream.println(text);
 			}});
 	}
-	
-	
+
+
 	/**
 	 * Logs an error message to the console.
 	 * @param message
 	 */
 	public void err(String message){
 		final String text = message;
-		
+
 		printTime();
-		
+
 		// Print the message in the UI Thread in async mode
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
@@ -163,11 +163,25 @@ public class Console extends MessageConsole {
 				stream.println(text);
 			}});
 	}
-	
+
+	/**
+	 * Logs an exception in the console
+	 * @param e the exception to log
+	 */
+	public synchronized void logException(Throwable e) {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				MessageConsoleStream stream = newMessageStream();
+				stream.print("\n");
+			}});
+		printTime();
+		logExceptionWhithoutTime(e, false);
+	}
+
 	//
 	// -- PRIVATE METHODS -----------------------------------------------
 	//
-	
+
 	/**
 	 * Prints the current time in the console.
 	 */
@@ -179,5 +193,29 @@ public class Console extends MessageConsole {
 				stream.setColor(Console.BLACK);
 				stream.print(dateFormat.format(new java.util.Date())+" => ");
 			}});
+	}
+
+	private synchronized void logExceptionWhithoutTime(Throwable e, boolean cause) {
+		StringBuilder builder = new StringBuilder();
+		if(cause)
+			builder.append("Caused by: ");
+
+		builder.append(e.getMessage()+"\n");
+		StackTraceElement[] traces = e.getStackTrace();
+		for(int i=0 ; i<traces.length ; i++)
+			builder.append("\t"+traces[i]+"\n");
+
+		final String log = builder.toString();
+
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				MessageConsoleStream stream = newMessageStream();
+				stream.setColor(Console.GRAY);
+				stream.print(log);
+			}});
+
+		if(e.getCause() != null)
+			logExceptionWhithoutTime(e.getCause(), true);
+
 	}
 }
