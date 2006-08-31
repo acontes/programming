3fe150a8972b9ab1af5d3f5d4b7347414d28b547
mreynier <mreynier@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-08-31 12:08:46 +0000

    Use GEF and build a tree like this VN->Host->JVM->Node->AO
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/proactive_newIC2D@3851 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- org.objectweb.proactive.ic2d.jobmonitoring/src/org/objectweb/proactive/ic2d/jobmonitoring/views/JobMonitoringView.java
+++ org.objectweb.proactive.ic2d.jobmonitoring/src/org/objectweb/proactive/ic2d/jobmonitoring/views/JobMonitoringView.java
@@ -1,34 +1,106 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2005 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.ic2d.jobmonitoring.views;
 
-import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.gef.editparts.RootTreeEditPart;
+import org.eclipse.gef.ui.parts.TreeViewer;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.part.ViewPart;
-import org.objectweb.proactive.ic2d.jobmonitoring.gui.JobMonitoringContentProvider;
-import org.objectweb.proactive.ic2d.jobmonitoring.gui.JobMonitoringLabelProvider;
+import org.objectweb.proactive.ic2d.jobmonitoring.actions.CollapseAllAction;
+import org.objectweb.proactive.ic2d.jobmonitoring.actions.ExpandAllAction;
+import org.objectweb.proactive.ic2d.jobmonitoring.editparts.JobMonitoringTreePartFactory;
 import org.objectweb.proactive.ic2d.monitoring.data.WorldObject;
 
+/**
+ * 
+ * @author Jean-Michael Legait and Michèle Reynier
+ *
+ */
 public class JobMonitoringView extends ViewPart {
 
-	public JobMonitoringView() {
-		// TODO Auto-generated constructor stub
-	}
+	
+	private TreeViewer treeViewer;
 
+	
+	/**
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
 	@Override
 	public void createPartControl(Composite parent) {
-		TreeViewer treeViewer = new TreeViewer(parent);
+
+		FormLayout layout = new FormLayout();
+		parent.setLayout(layout);
 		
-		treeViewer.setContentProvider(new JobMonitoringContentProvider());
-		treeViewer.setLabelProvider(new JobMonitoringLabelProvider());
-		treeViewer.setInput(WorldObject.getInstance());
-		
-		/*
+		// create graphical viewer
+		treeViewer = new TreeViewer();
+		treeViewer.createControl(parent);
+
+		// configure the viewer
 		Tree tree = new Tree(parent, SWT.SINGLE);
+		((RootTreeEditPart)treeViewer.getRootEditPart()).setWidget(tree);
+
+		// initialize the viewer with input
+		treeViewer.setEditPartFactory(new JobMonitoringTreePartFactory());
+		treeViewer.setContents(WorldObject.getInstance());
+
+		// hide an empty part in the view
+		FormData formData1 = new FormData();
+		formData1.top = new FormAttachment(0, 0);
+		formData1.bottom = new FormAttachment(0, 0);
+		formData1.left = new FormAttachment(0, 0);
+		formData1.right = new FormAttachment(0, 0);
+		parent.getChildren()[0].setLayoutData(formData1);
 		
-		TreeItem treeItem = new TreeItem(tree, SWT.NONE);
-		treeItem.setText("My first TreeItem");
-		Image icon = new Image(Display.getCurrent(), "icon/alt_window_16.gif");
-		treeItem.setImage(icon);
-		*/
+		FormData formData = new FormData();
+		formData.top = new FormAttachment(0, 0);
+		formData.bottom = new FormAttachment(100, 0);
+		formData.left = new FormAttachment(0, 0);
+		formData.right = new FormAttachment(100, 0);
+		parent.getChildren()[1].setLayoutData(formData);
+
+
+//		treeViewer.setContentProvider(new JobMonitoringContentProvider());
+//		treeViewer.setLabelProvider(new JobMonitoringLabelProvider());
+//		treeViewer.setInput(WorldObject.getInstance());
+
+		IToolBarManager toolBarManager = getViewSite().getActionBars().getToolBarManager();
+		
+		toolBarManager.add(new ExpandAllAction(treeViewer));
+		toolBarManager.add(new CollapseAllAction(treeViewer));
 	}
 
 	@Override
