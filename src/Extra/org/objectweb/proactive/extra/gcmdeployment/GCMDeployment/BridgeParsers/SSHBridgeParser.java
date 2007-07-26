99da7cb2ec3a26c8107dd8c1b707502d21e26bc3
glaurent <glaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-07-26 16:02:41 +0000

    parse a couple of attributes
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/GCMDeployment@5676 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/gcmdeployment/GCMDeployment/BridgeParsers/SSHBridgeParser.java
+++ src/Extra/org/objectweb/proactive/extra/gcmdeployment/GCMDeployment/BridgeParsers/SSHBridgeParser.java
@@ -2,6 +2,7 @@ package org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.BridgeParsers;
 
 import javax.xml.xpath.XPath;
 
+import org.objectweb.proactive.extra.gcmdeployment.GCMParserHelper;
 import org.objectweb.proactive.extra.gcmdeployment.process.bridge.AbstractBridge;
 import org.objectweb.proactive.extra.gcmdeployment.process.bridge.BridgeSSH;
 import org.w3c.dom.Node;
@@ -11,6 +12,13 @@ public class SSHBridgeParser extends AbstractBridgeParser {
     @Override
     public void parseBridgeNode(Node bridgeNode, XPath xpath) {
         super.parseBridgeNode(bridgeNode, xpath);
+        
+        String hostname = GCMParserHelper.getAttributeValue(bridgeNode, "hostname");
+        String username = GCMParserHelper.getAttributeValue(bridgeNode, "username");
+        
+        BridgeSSH bridgeSSH = ((BridgeSSH)bridge);
+        bridgeSSH.setHostname(hostname);
+        bridgeSSH.setUsername(username);
     }
 
     @Override
