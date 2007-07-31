1e22d8a7a199039c2bef24a6b94729df6cf1f368
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-07-31 08:20:38 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActiveScheduler@5687 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/scheduler/userAPI/State.java
+++ src/Extra/org/objectweb/proactive/extra/scheduler/userAPI/State.java
@@ -19,37 +19,47 @@ public enum State implements java.io.Serializable {
 	 * Get the jobs results is possible.
 	 * It can be paused, stopped or shutdown.
 	 */
-	STARTED,
+	STARTED ("Started"),
 	/**
 	 * The scheduler is stopped. Jobs cannot be sumitted anymore.
 	 * It will terminate every submitted jobs.
 	 * Get the jobs results is possible.
 	 * It can be started or shutdown.
 	 */
-	STOPPED,
+	STOPPED ("Stopped"),
 	/**
 	 * The scheduler is in immediate pause.
 	 * It means that every running tasks will be terminated,
 	 * but the running jobs will wait for the scheduler to resume.
 	 * It can be resumed, stopped, paused or shutdown.
 	 */
-	PAUSED_IMMEDIATE,
+	PAUSED_IMMEDIATE ("Paused"),
 	/**
 	 * The scheduler is paused.
 	 * It means that every running jobs will be terminated.
 	 * It can be resumed, stopped, paused immediate or shutdown.
 	 */
-	PAUSED,
+	PAUSED ("Paused"),
 	/**
 	 * The scheduler is shutting down,
 	 * It will terminate all running jobs (during this time, get jobs results is possible),
 	 * then it will serialize every remaining jobs results that still are in the finished queue.
 	 * Finally, it will shutdown the scheduler.
 	 */
-	SHUTTING_DOWN,
+	SHUTTING_DOWN ("Shutting down"),
 	/**
 	 * The scheduler has been killed, nothing can be done anymore.
 	 */
-	KILLED
+	KILLED ("Killed");
+	
+	private String definition;
+	
+	State (String def){
+		definition = def;
+	}
+	
+	public String getDefinition(){
+		return definition;
+	}
 	
 }
