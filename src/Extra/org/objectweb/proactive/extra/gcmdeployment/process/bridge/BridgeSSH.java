7521a9826c87065233c551e951824713ccf957fa
glaurent <glaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-01 09:34:40 +0000

    implemented BridgeSSH
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/GCMDeployment@5703 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/gcmdeployment/process/bridge/BridgeSSH.java
+++ src/Extra/org/objectweb/proactive/extra/gcmdeployment/process/bridge/BridgeSSH.java
@@ -1,6 +1,9 @@
 package org.objectweb.proactive.extra.gcmdeployment.process.bridge;
 
 public class BridgeSSH extends AbstractBridge {
+    
+    public final static String DEFAULT_SSHPATH = "ssh";
+    
     @Override
     public Object clone() throws CloneNotSupportedException {
         // TODO Auto-generated method stub
@@ -9,7 +12,19 @@ public class BridgeSSH extends AbstractBridge {
 
     @Override
     public String internalBuildCommand() {
-        // TODO Implements internalBuildCommand for BridgeSSH
-        return null;
+        StringBuilder command = new StringBuilder();
+        command.append(getCommandPath());
+        // append username
+        if (getUsername() != null) {
+            command.append(" -l ");
+            command.append(getUsername());
+        }
+
+        // append host
+        command.append(" ");
+        command.append(getHostname());
+        command.append(" ");
+
+        return command.toString();
     }
 }
