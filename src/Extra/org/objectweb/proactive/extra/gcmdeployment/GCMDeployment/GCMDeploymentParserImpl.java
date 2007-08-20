13c7393a9b00b3c0afec9d9094c4a5e3efb1f34c
glaurent <glaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-20 15:15:37 +0000

    Added MPI group
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/GCMDeployment@5943 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/gcmdeployment/GCMDeployment/GCMDeploymentParserImpl.java
+++ src/Extra/org/objectweb/proactive/extra/gcmdeployment/GCMDeployment/GCMDeploymentParserImpl.java
@@ -28,6 +28,7 @@ import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.Gr
 import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupGLiteParser;
 import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupGlobusParser;
 import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupGridEngineParser;
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupMPIParser;
 import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupOARGridParser;
 import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupOARParser;
 import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupPBSParser;
@@ -100,6 +101,7 @@ public class GCMDeploymentParserImpl implements GCMDeploymentParser {
         registerGroupParser(new GroupGLiteParser());
         registerGroupParser(new GroupGlobusParser());
         registerGroupParser(new GroupGridEngineParser());
+        registerGroupParser(new GroupMPIParser());
         // TODO add other group parsers here 
     }
 
