0271dc2058dbc50dd75f1bdb7a3c8056b288b75e
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-20 11:45:42 +0000

    Commit ProActive branche in ProActiveScheduler branche
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActiveScheduler@5920 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/Body.java
+++ src/Core/org/objectweb/proactive/Body.java
@@ -8,16 +8,16 @@
  * Contact: proactive@objectweb.org
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or any later version.
+ * version 2.1 of the License, or any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
@@ -209,4 +209,6 @@ public interface Body extends LocalBodyStrategy, UniversalBody,
      * @return true if the method exists, false otherwise
      */
     public boolean checkMethod(String methodName);
+
+    public void registerIncomingFutures();
 }
