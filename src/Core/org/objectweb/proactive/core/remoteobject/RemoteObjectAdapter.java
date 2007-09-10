cf9ef6eb6a6315ed4859144fe45abe2750e534cf
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-09-10 13:41:05 +0000

    TMP : print stack strace for debug purpose...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActiveScheduler@6177 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/remoteobject/RemoteObjectAdapter.java
+++ src/Core/org/objectweb/proactive/core/remoteobject/RemoteObjectAdapter.java
@@ -80,6 +80,7 @@ public class RemoteObjectAdapter implements RemoteObject {
         } catch (ProActiveException e) {
             throw new IOException(e.getMessage());
         } catch (IOException e) {
+            e.printStackTrace();
             ProActiveLogger.getLogger(Loggers.REMOTEOBJECT)
                            .warn("unable to contact remote object at " +
                 this.uri.toString() + " when calling " +
