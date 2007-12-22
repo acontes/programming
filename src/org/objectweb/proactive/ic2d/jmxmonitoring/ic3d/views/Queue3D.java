785312947fe51a984ed71875760f51817811f7e5
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-12-22 19:40:35 +0000

    Queue implementation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d@7529 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/Queue3D.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Geometry;
+import javax.media.j3d.TransformGroup;
+
+/**
+ * An implementation of an active object queue.
+ * @author vasile
+ *
+ */
+public class Queue3D extends AbstractQueue3D {
+
+	/**
+	 * @param name
+	 */
+	public Queue3D(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.view3d.AbstractFigure3D#arrangeSubFigures()
+	 */
+	@Override
+	public void arrangeSubFigures() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.view3d.AbstractFigure3D#createAppearance()
+	 */
+	@Override
+	protected Appearance createAppearance() {
+		return AppearanceBasket.queueAppearance;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.view3d.AbstractFigure3D#createGeometry()
+	 */
+	@Override
+	protected Geometry createGeometry() {
+		return GeometryBasket.getDefaultQueueGeometry();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.view3d.AbstractFigure3D#createTextBranch()
+	 */
+	@Override
+	protected TransformGroup createTextBranch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.view3d.AbstractFigure3D#animateCreation()
+	 */
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.view3d.AbstractFigure3D#animateCreation()
+	 */
+	@Override
+	protected void animateCreation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
