20c9757a220412248b968f38ca1b30d4dc4b9b08
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-12-22 19:44:52 +0000

    Earth view of grids
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d@7531 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/proearth/EarthGrid3D.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.proearth;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Geometry;
+import javax.media.j3d.TransformGroup;
+
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractGrid3D;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AppearanceBasket;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.GeometryBasket;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.PlacementBasket;
+
+
+
+/**
+ * An implementation of a grid figure. It uses a
+ * Earth texture and places the hosts on a sphere.
+ * The arrows used to show communications are curved.
+ * <br/>
+ * WORK IN PROGRESS
+ * 
+ * @author vasile
+ *
+ */
+public class EarthGrid3D extends AbstractGrid3D {
+
+	/**
+	 * @param name name of the grid
+	 */
+	public EarthGrid3D(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#arrangeSubFigures()
+	 */
+	@Override
+	public void arrangeSubFigures() {
+	    int i = 2;
+        for (AbstractFigure3D host : this.getSubFigures().values()) {
+            PlacementBasket.sphereArrangement(Math.random()*4*Math.PI,
+            			Math.random()*4*Math.PI, host);
+     
+            i++;
+            host.arrangeSubFigures();
+        }
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createAppearance()
+	 */
+	@Override
+	protected Appearance createAppearance() {
+		// TODO Auto-generated method stub
+		return AppearanceBasket.earthGridAppearance;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createGeometry()
+	 */
+	@Override
+	protected Geometry createGeometry() {
+		// TODO Auto-generated method stub
+		return GeometryBasket.getEarthGridGeometry();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createTextBranch()
+	 */
+	@Override
+	protected TransformGroup createTextBranch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#animateCreation()
+	 */
+	@Override
+	protected void animateCreation() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+   
+
+}
---
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/proearth/SphereArrow3D.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.proearth;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Geometry;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Tuple2d;
+
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractArrow3D;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AppearanceBasket;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.GeometryBasket;
+
+
+/**
+ * Implements a curved arrow.
+ * @author vasile
+ *
+ */
+public class SphereArrow3D extends AbstractArrow3D {
+
+    /**
+     * @param name  name of the arrow
+     * @param start the start angles on the sphere
+     * @param stop  the stop angles on the sphere
+     */
+    public SphereArrow3D(String name, Tuple2d start, Tuple2d stop) {
+        super(name);
+        // TODO Auto-generated constructor stub
+        this.setGeometry(createGeometry(start, stop));
+    }
+
+	
+	/**
+     * @param start the start angles on the sphere
+     * @param stop  the stop angles on the sphere
+	 * @return the arrow geometry
+	 */
+	private Geometry createGeometry(Tuple2d start, Tuple2d stop) {
+		//TODO remove constant segments and radius
+		return GeometryBasket.sphereArrowGeometry(start, stop, 50, 17);
+	}
+
+
+	/**
+	 * @param name
+	 */
+	public SphereArrow3D(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#arrangeSubFigures()
+	 */
+	@Override
+	public void arrangeSubFigures() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createAppearance()
+	 */
+	@Override
+	protected Appearance createAppearance() {
+		return AppearanceBasket.defaultLineAppearance;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createGeometry()
+	 */
+	@Override
+	protected Geometry createGeometry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createTextBranch()
+	 */
+	@Override
+	protected TransformGroup createTextBranch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#animateCreation()
+	 */
+	@Override
+	protected void animateCreation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
