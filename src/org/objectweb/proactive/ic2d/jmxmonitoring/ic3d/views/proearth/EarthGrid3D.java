34af4c58e15e4cf3f6ef63b5f840b43f871daf7a
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-03 18:38:00 +0000

    Organized source and added setArrow method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d@7573 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/proearth/EarthGrid3D.java
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/proearth/EarthGrid3D.java
@@ -6,6 +6,7 @@ package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.proearth;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.Geometry;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.Vector3f;
 
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D;
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractGrid3D;
@@ -13,44 +14,46 @@ import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AppearanceBasket;
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.GeometryBasket;
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.PlacementBasket;
 
-
-
 /**
- * An implementation of a grid figure. It uses a
- * Earth texture and places the hosts on a sphere.
- * The arrows used to show communications are curved.
- * <br/>
+ * An implementation of a grid figure. It uses a Earth texture and places the
+ * hosts on a sphere. The arrows used to show communications are curved. <br/>
  * WORK IN PROGRESS
  * 
  * @author vasile
- *
+ * 
  */
 public class EarthGrid3D extends AbstractGrid3D {
 
 	/**
-	 * @param name name of the grid
+	 * @param name
+	 *            name of the grid
 	 */
 	public EarthGrid3D(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#arrangeSubFigures()
 	 */
 	@Override
 	public void arrangeSubFigures() {
-	    int i = 2;
-        for (AbstractFigure3D host : this.getSubFigures().values()) {
-            PlacementBasket.sphereArrangement(Math.random()*4*Math.PI,
-            			Math.random()*4*Math.PI, host);
-     
-            i++;
-            host.arrangeSubFigures();
-        }
+		int i = 2;
+		for (AbstractFigure3D host : this.getSubFigures().values()) {
+			PlacementBasket.sphereArrangement(Math.random() * 4 * Math.PI, Math
+					.random()
+					* 4 * Math.PI, host);
+
+			i++;
+			host.arrangeSubFigures();
+		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createAppearance()
 	 */
 	@Override
@@ -59,7 +62,9 @@ public class EarthGrid3D extends AbstractGrid3D {
 		return AppearanceBasket.earthGridAppearance;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createGeometry()
 	 */
 	@Override
@@ -68,7 +73,9 @@ public class EarthGrid3D extends AbstractGrid3D {
 		return GeometryBasket.getEarthGridGeometry();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createTextBranch()
 	 */
 	@Override
@@ -77,17 +84,27 @@ public class EarthGrid3D extends AbstractGrid3D {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#animateCreation()
 	 */
 	@Override
 	protected void animateCreation() {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
-   
 
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#setArrow(java.lang.String,
+	 *      javax.vecmath.Vector3f, javax.vecmath.Vector3f)
+	 */
+	@Override
+	protected AbstractFigure3D setArrow(String name, Vector3f start,
+			Vector3f stop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
