b6a7691b8365ea91d4ea907cfbdb645245dc398d
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-03 19:02:57 +0000

    Organized source
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d@7588 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/Monitoring3DView.java
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/Monitoring3DView.java
@@ -1,6 +1,5 @@
 package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
 
-
 /*
  * ################################################################
  *
@@ -32,11 +31,14 @@ package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
  * ################################################################
  */
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Frame;
-import java.util.Set;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import javax.media.j3d.Canvas3D;
-
+import javax.swing.JPanel;
 
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.Separator;
@@ -51,163 +53,154 @@ import org.objectweb.proactive.ic2d.jmxmonitoring.action.NewHostAction;
 
 import org.objectweb.proactive.ic2d.jmxmonitoring.action.SetDepthAction;
 import org.objectweb.proactive.ic2d.jmxmonitoring.action.SetTTRAction;
-import org.objectweb.proactive.ic2d.jmxmonitoring.data.ModelRecorder;
 import org.objectweb.proactive.ic2d.jmxmonitoring.data.WorldObject;
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.controller.Grid3DController;
 
-
 public class Monitoring3DView extends ViewPart {
-    public static final String ID = "org.objectweb.proactive.ic2d.jmxmonitoring.view.Legend";
-    /** The World */
-    private WorldObject world;
-    private String title;
+	public static final String ID = "org.objectweb.proactive.ic2d.jmxmonitoring.view.Legend";
+	/** The World */
+	private WorldObject world;
+	private String title;
 
-    
-    public Monitoring3DView() {
-        super();
-        world = new WorldObject();
-        title = world.getName();
-    }
+	public Monitoring3DView() {
+		super();
+		world = new WorldObject();
+		title = world.getName();
+	}
 
-    @Override
-    public void createPartControl(Composite parent) {
-        parent.setLayout(new FillLayout());
-        
-        //**********ADD  TOOLBAR BUTTONS********************
-        IToolBarManager toolBarManager = getViewSite().getActionBars().getToolBarManager();
+	@Override
+	public void createPartControl(Composite parent) {
+		parent.setLayout(new FillLayout());
 
-        // Adds "Monitor a new Host" action to the view's toolbar
-        NewHostAction toolBarNewHost = new NewHostAction(parent.getDisplay(), world);
-        toolBarManager.add(toolBarNewHost);
+		// **********ADD TOOLBAR BUTTONS********************
+		IToolBarManager toolBarManager = getViewSite().getActionBars()
+				.getToolBarManager();
 
-        // Adds "Set depth" action to the view's toolbar
-        SetDepthAction toolBarSetDepth = new SetDepthAction(parent.getDisplay(), world);
-        toolBarManager.add(toolBarSetDepth);
+		// Adds "Monitor a new Host" action to the view's toolbar
+		NewHostAction toolBarNewHost = new NewHostAction(parent.getDisplay(),
+				world);
+		toolBarManager.add(toolBarNewHost);
 
-        toolBarManager.add(new Separator());
+		// Adds "Set depth" action to the view's toolbar
+		SetDepthAction toolBarSetDepth = new SetDepthAction(
+				parent.getDisplay(), world);
+		toolBarManager.add(toolBarSetDepth);
 
-        // Adds "Set Time to refresh" action to the view's toolbar
-        SetTTRAction toolBarTTR = new SetTTRAction(parent.getDisplay(), world.getMonitorThread());
-        toolBarManager.add(toolBarTTR);
+		toolBarManager.add(new Separator());
 
+		// Adds "Set Time to refresh" action to the view's toolbar
+		SetTTRAction toolBarTTR = new SetTTRAction(parent.getDisplay(), world
+				.getMonitorThread());
+		toolBarManager.add(toolBarTTR);
 
-        // Adds enable/disable monitoring action to the view's toolbar
-        EnableDisableMonitoringAction toolBarEnableDisableMonitoring = new EnableDisableMonitoringAction(
-            world);
-        toolBarManager.add(toolBarEnableDisableMonitoring);
+		// Adds enable/disable monitoring action to the view's toolbar
+		EnableDisableMonitoringAction toolBarEnableDisableMonitoring = new EnableDisableMonitoringAction(
+				world);
+		toolBarManager.add(toolBarEnableDisableMonitoring);
 
+		toolBarManager.add(new Separator());
 
-        toolBarManager.add(new Separator());
+		// ******************************
 
-        //******************************
-        
-        
-        // create swt container:
-        Composite SWT_AWT_container = new Composite(parent, SWT.EMBEDDED);
+		// create swt container:
+		Composite SWT_AWT_container = new Composite(parent, SWT.EMBEDDED);
 
-        // set bounds (here showing bounds to cover parent's area)
-        final Rectangle sh3dbnds = parent.getBounds();
-        sh3dbnds.x = sh3dbnds.y = 0;
-        SWT_AWT_container.setBounds(sh3dbnds);
+		// set bounds (here showing bounds to cover parent's area)
+		final Rectangle sh3dbnds = parent.getBounds();
+		sh3dbnds.x = sh3dbnds.y = 0;
+		SWT_AWT_container.setBounds(sh3dbnds);
 
-        // ----------------
-        Frame awt = SWT_AWT.new_Frame(SWT_AWT_container);
+		// ----------------
+		Frame awt = SWT_AWT.new_Frame(SWT_AWT_container);
 
-        // -----------------
-        final Rectangle bounds = SWT_AWT_container.getBounds();
-        awt.setBounds(0, 0, bounds.width, bounds.height);
+		// -----------------
+		final Rectangle bounds = SWT_AWT_container.getBounds();
+		awt.setBounds(0, 0, bounds.width, bounds.height);
 
-        awt.setLayout(new BorderLayout());
+		awt.setLayout(new BorderLayout());
 
-        // create your scene:
-        awt.add(newGrid());
-        // //TODO hacky
-        // //*********** ADDED FOR MULTIPLE VIEWS *********
-        // //uncomment above to go back to the old view
-        //        
-        // GridUniverse universe = new GridUniverse();
-        // Grid3D g = new Grid3D();
-        // universe.addGrid(g);
-        //
-        // //get the world object,
-        // Set<String> modelNames = ModelRecorder.getInstance().getNames();
-        // WorldObject root = ModelRecorder.getInstance()
-        // .getModel(modelNames.iterator().next());
-        //
-        // //create the grid controller,
-        // //the grid controller will be responsible with creating/removing
-        // //the host controllers and so on
-        // Grid3DController controller = new Grid3DController(root, g, null);
-        //
-        //        
-        // //three views
-        // Canvas3D viewOne = universe.newView();
-        // Canvas3D viewTwo = universe.newView();
-        // // Canvas3D viewThree = universe.newView();
-        //
-        // //create layout 65/35
-        // GridBagConstraints constr = new GridBagConstraints();
-        // GridBagLayout mainLay = new GridBagLayout();
-        // JPanel main = new JPanel(mainLay);
-        //
-        // //create layout in the 35 side with three rows
-        // JPanel side = new JPanel(new GridLayout(1, 1, 1, 1));
-        // JPanel left = new JPanel(new GridLayout(1, 1, 1, 1));
-        //
-        // left.setBackground(new Color(0, 0, 0));
-        // side.setBackground(new Color(0, 0, 0));
-        // main.setBackground(new Color(0, 0, 0));
-        //
-        // //add the first view on the left and all three on the right
-        // constr.insets = new Insets(1, 1, 1, 1);
-        //
-        // constr.fill = GridBagConstraints.BOTH;
-        // constr.gridwidth = 2;
-        // constr.weightx = 1;
-        // constr.weighty = 1;
-        // main.add(left, constr);
-        //
-        // constr.gridwidth = 2;
-        // constr.weightx = 0.3;
-        // constr.weighty = 1;
-        //
-        // main.add(side, constr);
-        //
-        // //add views in the side pane
-        // left.add(viewOne);
-        //
-        // side.add(viewTwo);
-        // // side.add(viewThree);
-        // awt.add(main);
-        //
-        // awt.pack();
-        // awt.setVisible(true);
-        // // awt.setSize(800, 600);
-        //
-        // viewOne.addMouseListener(new TemporaryMouseListener(side, left));
-        //
-        // viewTwo.addMouseListener(new TemporaryMouseListener(side, left));
-        // // viewThree.addMouseListener(new TemporaryMouseListener(side,
-        // left));
-    }
+		// create your scene:
+		// awt.add(newGrid());
+		// TODO hacky
+		// *********** ADDED FOR MULTIPLE VIEWS *********
+		// uncomment above to go back to the old view
 
-    private Canvas3D newGrid() {
-        GridUniverse grid = new GridUniverse();
-        Grid3D g = new Grid3D();
-        grid.addGrid(g);
+		GridUniverse universe = new GridUniverse();
+		Grid3D g = new Grid3D();
+		universe.addGrid(g);
 
-        // get the world object,
-//        Set<String> modelNames = ModelRecorder.getInstance().getNames();
-//        WorldObject root = ModelRecorder.getInstance()
-//                                        .getModel(modelNames.iterator().next());
-        Grid3DController controller = new Grid3DController(world, g, null);
+		// create the grid controller,
+		// the grid controller will be responsible with creating/removing
+		// the host controllers and so on
+		Grid3DController controller = new Grid3DController(world, g, null);
 
-        // Root3dController c = new Root3dController(g);
-        return grid.newView();
-    }
+		// three views
+		Canvas3D viewOne = universe.newView();
+		Canvas3D viewTwo = universe.newView();
+		Canvas3D viewThree = universe.newView();
 
-    @Override
-    public void setFocus() {
-        // TODO Auto-generated method stub
-    }
+		// create layout 65/35
+		GridBagConstraints constr = new GridBagConstraints();
+		GridBagLayout mainLay = new GridBagLayout();
+		JPanel main = new JPanel(mainLay);
+
+		// create layout in the 35 side with three rows
+		JPanel side = new JPanel(new GridLayout(2, 1, 1, 1));
+		JPanel left = new JPanel(new GridLayout(1, 1, 1, 1));
+
+		left.setBackground(new Color(0, 0, 0));
+		side.setBackground(new Color(0, 0, 0));
+		main.setBackground(new Color(0, 0, 0));
+
+		// add the first view on the left and all three on the right
+		constr.insets = new Insets(1, 1, 1, 1);
+
+		constr.fill = GridBagConstraints.BOTH;
+		constr.gridwidth = 2;
+		constr.weightx = 1;
+		constr.weighty = 1;
+		main.add(left, constr);
+
+		constr.gridwidth = 2;
+		constr.weightx = 0.3;
+		constr.weighty = 1;
+
+		main.add(side, constr);
+
+		// add views in the side pane
+		left.add(viewOne);
+
+		side.add(viewTwo);
+		side.add(viewThree);
+		awt.add(main);
+
+		awt.pack();
+		awt.setVisible(true);
+		// awt.setSize(800, 600);
+
+		viewOne.addMouseListener(new TemporaryMouseListener(side, left));
+
+		viewTwo.addMouseListener(new TemporaryMouseListener(side, left));
+		viewThree.addMouseListener(new TemporaryMouseListener(side, left));
+	}
+
+	private Canvas3D newGrid() {
+		GridUniverse grid = new GridUniverse();
+		Grid3D g = new Grid3D();
+		grid.addGrid(g);
+
+		// get the world object,
+		// Set<String> modelNames = ModelRecorder.getInstance().getNames();
+		// WorldObject root = ModelRecorder.getInstance()
+		// .getModel(modelNames.iterator().next());
+		Grid3DController controller = new Grid3DController(world, g, null);
+
+		// Root3dController c = new Root3dController(g);
+		return grid.newView();
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+	}
 }
