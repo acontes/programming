0e7a7e2da768462e6731deb6d8b6afa6d94e39dd
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-03 18:56:05 +0000

    Organized source and added setArrow method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d@7583 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/Node3D.java
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/Node3D.java
@@ -5,75 +5,98 @@ package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
 
 import javax.media.j3d.Appearance;
 import javax.media.j3d.Geometry;
-import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
-import javax.vecmath.Vector3d;
+import javax.vecmath.Vector3f;
 
 /**
- * An implementation for a node figure. The figure
- * is a cube 1x1x1  that gets resized when placed on the runtime. 
+ * An implementation for a node figure. The figure is a cube 1x1x1 that gets
+ * resized when placed on the runtime.
+ * 
  * @author vjuresch
- *
+ * 
  */
 public class Node3D extends AbstractNode3D {
-    public Node3D(String name) {
-        super(name);
-        // TODO Auto-generated constructor stub
-    }
+	public Node3D(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#arrangeSubFigures()
+	 */
+	@Override
+	public void arrangeSubFigures() {
+		int i = 1; // to count the nodes
+		for (AbstractFigure3D ao : this.getSubFigures().values()) {
+			PlacementBasket.sphericalAOVerticalArrangement(0.05, 1.5, i, this
+					.getSubFigures().size(), ao, this);
+			i++;
+			ao.arrangeSubFigures();
+		}
+		// //change the height of the node to fit the number of active objects
+		// TransformGroup scaleHeight =
+		// (TransformGroup)this.getParent().getParent();
+		// Transform3D height = new Transform3D();
+		// scaleHeight.getTransform(height);
+		// height.setScale(new Vector3d(1,1,i * 1.5 + 0.1));
 
-    /* (non-Javadoc)
-     * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#arrangeSubFigures()
-     */
-    @Override
-    public void arrangeSubFigures() {
-    	   int i = 1; //to count the nodes
-           for (AbstractFigure3D ao : this.getSubFigures().values()) {
-           		PlacementBasket.sphericalAOVerticalArrangement(0.05,1.5, i, this.getSubFigures().size(),
-           			ao, this);
-           		i++;
-           		ao.arrangeSubFigures();
-           }
-//    		//change the height of the node to fit the number of active objects
-//    		TransformGroup scaleHeight = (TransformGroup)this.getParent().getParent();
-//    		Transform3D  height = new Transform3D();
-//    		scaleHeight.getTransform(height);
-//    		height.setScale(new Vector3d(1,1,i * 1.5 + 0.1));
-//    		
-//    		scaleHeight.setTransform(height);
-    }
+		// scaleHeight.setTransform(height);
+	}
 
-    /* (non-Javadoc)
-     * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#createGeometry()
-     */
-    @Override
-    protected Geometry createGeometry() {
-        return GeometryBasket.getDefaultNodeGeometry();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#createGeometry()
+	 */
+	@Override
+	protected Geometry createGeometry() {
+		return GeometryBasket.getDefaultNodeGeometry();
+	}
 
-    // code to create default appearance of visual object	
-    /* (non-Javadoc)
-     * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#createAppearance()
-     */
-    @Override
-    protected Appearance createAppearance() {
-        return AppearanceBasket.defaultNodeAppearance;
-    }
+	// code to create default appearance of visual object
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#createAppearance()
+	 */
+	@Override
+	protected Appearance createAppearance() {
+		return AppearanceBasket.defaultNodeAppearance;
+	}
 
-    /* (non-Javadoc)
-     * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#createTextBranch()
-     */
-    @Override
-    protected TransformGroup createTextBranch() {
-    	return TextStylesBasket.nodeText(getShortenedName(10));
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#createTextBranch()
+	 */
+	@Override
+	protected TransformGroup createTextBranch() {
+		return TextStylesBasket.nodeText(getShortenedName(10));
+	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#animateCreation()
 	 */
 	@Override
 	protected void animateCreation() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#setArrow(javax.vecmath.Vector3f,
+	 *      javax.vecmath.Vector3f)
+	 */
+	@Override
+	protected AbstractFigure3D setArrow(String name, Vector3f start,
+			Vector3f stop) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
