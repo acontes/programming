22754ddb4b77a2674e2bbdea74187775c04f709d
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-03 18:39:20 +0000

    Organized source and added setArrow method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d@7574 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/proearth/SphereArrow3D.java
+++ src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/proearth/SphereArrow3D.java
@@ -7,11 +7,12 @@ import javax.media.j3d.Appearance;
 import javax.media.j3d.Geometry;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Tuple2d;
+import javax.vecmath.Vector3f;
 
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractArrow3D;
+import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D;
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AppearanceBasket;
 import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.GeometryBasket;
-
 
 /**
  * Implements a curved arrow.
@@ -20,28 +21,26 @@ import org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.GeometryBasket;
  */
 public class SphereArrow3D extends AbstractArrow3D {
 
-    /**
-     * @param name  name of the arrow
-     * @param start the start angles on the sphere
-     * @param stop  the stop angles on the sphere
-     */
-    public SphereArrow3D(String name, Tuple2d start, Tuple2d stop) {
-        super(name);
-        // TODO Auto-generated constructor stub
-        this.setGeometry(createGeometry(start, stop));
-    }
-
-	
 	/**
-     * @param start the start angles on the sphere
-     * @param stop  the stop angles on the sphere
+	 * @param name  name of the arrow
+	 * @param start the start angles on the sphere
+	 * @param stop  the stop angles on the sphere
+	 */
+	public SphereArrow3D(String name, Tuple2d start, Tuple2d stop) {
+		super(name);
+		// TODO Auto-generated constructor stub
+		this.setGeometry(createGeometry(start, stop));
+	}
+
+	/**
+	 * @param start the start angles on the sphere
+	 * @param stop  the stop angles on the sphere
 	 * @return the arrow geometry
 	 */
 	private Geometry createGeometry(Tuple2d start, Tuple2d stop) {
 		//TODO remove constant segments and radius
 		return GeometryBasket.sphereArrowGeometry(start, stop, 50, 17);
 	}
-
 
 	/**
 	 * @param name
@@ -77,7 +76,6 @@ public class SphereArrow3D extends AbstractArrow3D {
 		return null;
 	}
 
-
 	/* (non-Javadoc)
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#createTextBranch()
 	 */
@@ -87,15 +85,23 @@ public class SphereArrow3D extends AbstractArrow3D {
 		return null;
 	}
 
-
 	/* (non-Javadoc)
 	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.AbstractFigure3D#animateCreation()
 	 */
 	@Override
 	protected void animateCreation() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+	/* (non-Javadoc)
+	 * @see org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views.AbstractFigure3D#setArrow(java.lang.String, javax.vecmath.Vector3f, javax.vecmath.Vector3f)
+	 */
+	@Override
+	protected AbstractFigure3D setArrow(String name, Vector3f start,
+			Vector3f stop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
