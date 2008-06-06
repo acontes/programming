129c9aad564e0db6cfeb5a00a1b268dfbf0de001
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-06-06 13:57:55 +0000

    to be replaced with a newer version
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ic3d@9091 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d/src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/AbstractRuntime3D.java
+++
@@ -1,8 +0,0 @@
-package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
-
-public abstract class AbstractRuntime3D extends AbstractFigure3D {
-    public AbstractRuntime3D(String name) {
-        super(name);
-        // TODO Auto-generated constructor stub
-    }
-}
