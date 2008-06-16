ff40d8573fb118e8ff07dcd98650297322e33b22
sbauer <sbauer@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-06-16 12:16:03 +0000

    Added Cluster3D classes (useless yet)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ic3d@9188 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d/src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/AbstractCluster3D.java
@@ -0,0 +1,10 @@
+package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
+
+public abstract class AbstractCluster3D extends AbstractFigure3D {
+
+	public AbstractCluster3D(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d/src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/Cluster3D.java
@@ -0,0 +1,51 @@
+package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Geometry;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Vector3f;
+
+public class Cluster3D extends AbstractCluster3D {
+
+	public Cluster3D(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void animateCreation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void arrangeSubFigures() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Appearance createAppearance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Geometry createGeometry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected TransformGroup createTextBranch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected AbstractFigure3D setArrow(String name, Vector3f start, Vector3f stop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
