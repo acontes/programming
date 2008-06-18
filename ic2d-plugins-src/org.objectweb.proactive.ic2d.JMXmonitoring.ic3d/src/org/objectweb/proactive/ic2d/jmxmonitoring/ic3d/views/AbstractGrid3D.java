c32523dfe932fa9f537a08d3cff22574944e13e6
sbauer <sbauer@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-06-18 17:54:02 +0000

    moved translate from monitor  grid
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ic3d@9259 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d/src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/AbstractGrid3D.java
+++ ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring.ic3d/src/org/objectweb/proactive/ic2d/jmxmonitoring/ic3d/views/AbstractGrid3D.java
@@ -1,5 +1,10 @@
 package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
 
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3d;
+
 /**
  * All representation of a grid should extend this class. Usually the grid is an
  * invisible figure that contains the hosts. However there may be cases where a
@@ -9,12 +14,19 @@ package org.objectweb.proactive.ic2d.jmxmonitoring.ic3d.views;
  * 
  */
 public abstract class AbstractGrid3D extends AbstractFigure3D {
-	/**
-	 * @param name -
-	 *            name of the figure, for a grid is usually empty
-	 */
-	public AbstractGrid3D(final String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param name - name of the figure, for a grid is usually empty 
+     */
+    public AbstractGrid3D(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void setTranslation(Vector3d translation) {
+    	TransformGroup translate = this.getTranslateScaleTransform();
+    	Transform3D transform3d = new Transform3D();
+    	translate.getTransform(transform3d);
+    	transform3d.setTranslation(translation);
+    	translate.setTransform(transform3d);
+    }
 }
