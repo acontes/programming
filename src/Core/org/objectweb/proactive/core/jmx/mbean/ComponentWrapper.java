50318617ad4efa347fb96cad09bcf2f001c78fb5
yyuan <yyuan@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-01 15:37:33 +0000

    ComponentWrapper extends BodyWrapper and implements ComponentWrapperMBean
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_IC2D_COMPONENT@9478 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Core/org/objectweb/proactive/core/jmx/mbean/ComponentWrapper.java
@@ -0,0 +1,78 @@
+package org.objectweb.proactive.core.jmx.mbean;
+
+import javax.management.ObjectName;
+
+import org.objectweb.fractal.api.Component;
+import org.objectweb.fractal.api.NoSuchInterfaceException;
+import org.objectweb.fractal.api.control.ContentController;
+import org.objectweb.proactive.core.UniqueID;
+import org.objectweb.proactive.core.body.AbstractBody;
+import org.objectweb.proactive.core.component.Constants;
+import org.objectweb.proactive.core.component.body.ComponentBody;
+import org.objectweb.proactive.core.component.identity.ProActiveComponentImpl;
+
+public class ComponentWrapper extends BodyWrapper implements ComponentWrapperMBean {
+
+	ComponentBody cbody;
+	boolean iscomponent;
+
+	/**
+	 * Empty constructor required by JMX
+	 */
+	public ComponentWrapper() {
+		/* Empty Constructor required by JMX */
+	}
+
+	/**
+	 * Creates a new BodyWrapper MBean, representing an active object.
+	 * 
+	 * @param oname
+	 *            The JMX name of this wrapper
+	 * @param body
+	 *            The wrapped active object's body
+	 */
+	public ComponentWrapper(ObjectName oname, AbstractBody body) {
+		super(oname, body);
+		iscomponent = false;
+		if (body instanceof ComponentBody) {
+			this.cbody = (ComponentBody) body;
+			if (this.cbody.isComponent())
+				this.iscomponent = true;
+		}
+	}
+
+	public String getComponentName() {
+		return super.getName();
+	}
+
+	public boolean isComponent() {
+		return this.iscomponent;
+	}
+
+	public Component[] getSubComponents() {
+		if (!isComponent())
+			return null;
+
+		ProActiveComponentImpl componentImpl = this.cbody.getProActiveComponentImpl();
+		try
+		{
+			ContentController CC = (ContentController) componentImpl.getFcInterface(Constants.CONTENT_CONTROLLER);
+			return CC.getFcSubComponents();
+		}
+		catch(NoSuchInterfaceException e)
+		{
+			return null;
+		}
+		
+	}
+
+	public UniqueID getID() {
+		return super.getID();
+	}
+	
+	public ObjectName getObjectName()
+	{
+		return super.getObjectName();
+	}
+
+}
