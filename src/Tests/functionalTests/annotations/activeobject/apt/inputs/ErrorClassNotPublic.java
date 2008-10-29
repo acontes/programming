f88832483214f7e13c30d96b3424042be4a97171
fabratu <fabratu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-10-29 17:18:11 +0000

    PROACTIVE-499 "the class should be public" condition: * added an unit test, which helped me to: * detected & fixed a bug
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Compile_Time_Annotations@10388 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/annotations/activeobject/apt/inputs/ErrorClassNotPublic.java
@@ -0,0 +1,13 @@
+package functionalTests.annotations.activeobject.apt.inputs;
+
+import org.objectweb.proactive.extra.annotation.activeobject.ActiveObject;
+
+public class ErrorClassNotPublic {
+
+}
+
+@ActiveObject
+class ThisIsWrong {
+		
+}
+
