01364829b27a6cc75d01c825c92f83f131c701f2
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-11-27 13:10:40 +0000

    Merge branch 'SVN_trunk' into SVN_STORY_MESSAGE_ROUTING
    * SVN_trunk: (67 commits)
      Encoding charset typo + Javadoc translation (french->english)
      Removing some unuseful XXX comments
      Issue for PROACTIVE-415
      Typo
      Added a functional test for final fields in activeObject
      Fix for PROACTIVE-545
      PROACTIVE-535 Added equals to NodeImpl and NodeInformation
      Commiting the first release(sort of) of the toolchain doc 
      removed the svn:ignore property for doc/toolchain in order to prepare for doc toolchain commit  
      Fix PROACTIVE-277
      related to DOC-2 : updated link
      related to DOC-2 : updated link
      related to DOC-2 : broken link
      fix PROACTIVE-529 aka misconfigured helloWS.bat
      PROACTIVE-527  there is .class under doc/src/guided_tour/examples/... -- should not be under revision control
      there is some .class file in doc/src/guided_tour/examples, they should not be there
      Updated the NAS benchmarks to use the GCM Deployment
      Modified xsd schema for groupCCS
      Updated GroupCCS, that use again /classpath argument in command to launch
      added missing ant target dependency
      ...
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Message_Routing@10658 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/config/ProActiveConfiguration.java
+++ src/Core/org/objectweb/proactive/core/config/ProActiveConfiguration.java
@@ -78,7 +78,7 @@ public class ProActiveConfiguration {
      */
     public static final String PROACTIVE_LOG_PROPERTIES_FILE = "ProActiveLoggers.properties";
 
-    protected static final String FILE_PROTOCOL_PREFIX = "file:///";
+    protected static final String FILE_PROTOCOL_PREFIX = "file:";
 
     /** User configuration directory */
     protected static final String PROACTIVE_USER_CONFIG_FILENAME = FILE_PROTOCOL_PREFIX +
@@ -248,7 +248,7 @@ public class ProActiveConfiguration {
             fname = PROACTIVE_USER_CONFIG_FILENAME;
         }
 
-        if (!fname.matches("^\\w+://.*$")) {
+        if (!fname.matches("^\\w+:.*$")) {
             // protocol prefix was not specified
             // using "file" protocol by default
             fname = FILE_PROTOCOL_PREFIX + fname;
@@ -261,7 +261,9 @@ public class ProActiveConfiguration {
             logger.debug("User Config File is: " + u.toExternalForm());
             userProps = ProActiveConfigurationParser.parse(u.toString(), userProps);
         } catch (Exception e) {
-            logger.warn("Configuration file " + u.toExternalForm() + " not found");
+            if (!fname.equals(PROACTIVE_CONFIG_FILENAME)) {
+                logger.warn("Configuration file " + u.toExternalForm() + " not found");
+            }
         }
 
         return userProps;
