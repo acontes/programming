f7043499b9b0d33f9cef6cb17161313269514d32
afawaz <afawaz@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-12-10 15:50:54 +0000

    Modified Message.java and TypeHelper.java. Still need to add some methods to Message.java to act directly on a byte array upon routing (inside the registry for example)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Message_Routing@10784 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/Message.java
+++ src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/Message.java
@@ -5,9 +5,9 @@ package org.objectweb.proactive.extra.forwardingv2.protocol;
  *
  * A message MUST have the following structure
  * 
- * MSG = LENGHT PROTO-ID  MSG_TYPE SRC_AGENT-ID SRC_ENDPOINT-ID DST_AGENT-ID DST_ENDPOINT-ID ID DATA
+ * MSG = LENGHT | PROTO-ID | MSG_TYPE | SRC_AGENT-ID | SRC_ENDPOINT-ID | DST_AGENT-ID | DST_ENDPOINT-ID | ID | DATA
  * 
- * LENGTH = long
+ * LENGTH = int
  * PROTO-ID = int
  * MSG_TYPE = int
  * SRC_AGENT-ID = long
@@ -19,84 +19,136 @@ package org.objectweb.proactive.extra.forwardingv2.protocol;
  * 
  */
 public class Message {
-	
-	// 1 long for the length, 2 int for protoID and MsgType, 2 long for SrcAgent and SrcEndpoint IDs, 2 long for DstAgent and DstEndpoint IDs, 1 long for MSG_ID 
-	public static final int HEADER_LENGTH = 8 + 2 * 4 + 2 * 8 + 2 * 8 + 8;
-	
-	public static final int PROTOV1 = 1;
-	
+
+    // 1 long for the length, 2 int for protoID and MsgType, 2 long for SrcAgent and SrcEndpoint IDs, 2 long for DstAgent and DstEndpoint IDs, 1 long for MSG_ID 
+    public static final int HEADER_LENGTH = 3 * 4 + 2 * 8 + 2 * 8 + 8; // = 52
+
+    public static final int LENGTH_OFFSET = 0;
+    public static final int PROTO_ID_OFFSET = 4;
+    public static final int MSG_TYPE_OFFSET = 8;
+    public static final int SRC_AGENT_ID_OFFSET = 12;
+    public static final int SRC_ENDPOINT_ID_OFFSET = 20;
+    public static final int DST_AGENT_ID_OFFSET = 28;
+    public static final int DST_ENDPOINT_ID_OFFSET = 36;
+    public static final int MSG_ID_OFFSET = 44;
+
+    public static final int PROTOV1 = 1;
+
     protected int type;
     protected AgentID srcAgentID, dstAgentID;
     protected EndpointID srcEndpointID, dstEndpointID;
-    protected long ID;
+    protected long msgID;
     protected byte[] data;
 
-    Message() {}
-
-    public int getLength() {
-    	return HEADER_LENGTH + data.length;
+    Message() {
     }
-    
+
+    Message(byte[] byteArray) {
+        int datalength = TypeHelper.byteArrayToInt(byteArray, LENGTH_OFFSET) - Message.HEADER_LENGTH;
+        data = new byte[datalength];
+
+        type = TypeHelper.byteArrayToInt(byteArray, MSG_TYPE_OFFSET);
+        srcAgentID = new AgentID(TypeHelper.byteArrayToLong(byteArray, SRC_AGENT_ID_OFFSET));
+        srcEndpointID = new EndpointID(TypeHelper.byteArrayToLong(byteArray, SRC_ENDPOINT_ID_OFFSET));
+        dstAgentID = new AgentID(TypeHelper.byteArrayToLong(byteArray, DST_AGENT_ID_OFFSET));
+        dstEndpointID = new EndpointID(TypeHelper.byteArrayToLong(byteArray, DST_ENDPOINT_ID_OFFSET));
+        msgID = TypeHelper.byteArrayToLong(byteArray, MSG_ID_OFFSET);
+
+        for (int i = 0; i < datalength; i++) {
+            data[i] = byteArray[Message.HEADER_LENGTH + i];
+        }
+    }
+
+    /**
+     * serializes the Message in a byte array
+     * @return the array representing the formatted message
+     */
+    public byte[] toByteArray() {
+        int length = getLength();
+        byte[] byteArray = new byte[length];
+
+        TypeHelper.intToByteArray(length, byteArray, LENGTH_OFFSET);
+        TypeHelper.intToByteArray(getProtoID(), byteArray, PROTO_ID_OFFSET);
+        TypeHelper.intToByteArray(type, byteArray, MSG_TYPE_OFFSET);
+        TypeHelper.longToByteArray(srcAgentID.getId(), byteArray, SRC_AGENT_ID_OFFSET);
+        TypeHelper.longToByteArray(srcEndpointID.getId(), byteArray, SRC_ENDPOINT_ID_OFFSET);
+        TypeHelper.longToByteArray(dstAgentID.getId(), byteArray, DST_AGENT_ID_OFFSET);
+        TypeHelper.longToByteArray(dstEndpointID.getId(), byteArray, DST_ENDPOINT_ID_OFFSET);
+        TypeHelper.longToByteArray(msgID, byteArray, MSG_ID_OFFSET);
+
+        for (int i = 0; i < data.length; i++) {
+            byteArray[Message.HEADER_LENGTH + i] = data[i];
+        }
+
+        return byteArray;
+    }
+
+    /**
+     * @return the total length of the formatted message
+     */
+    public int getLength() {
+        return HEADER_LENGTH + data.length;
+    }
+
     public int getProtoID() {
-    	return PROTOV1;
+        return PROTOV1;
     }
 
     //traditional getters and setters
-	public int getType() {
-		return type;
-	}
+    public int getType() {
+        return type;
+    }
 
-	public void setType(int type) {
-		this.type = type;
-	}
+    public void setType(int type) {
+        this.type = type;
+    }
 
-	public AgentID getSrcAgentID() {
-		return srcAgentID;
-	}
+    public AgentID getSrcAgentID() {
+        return srcAgentID;
+    }
 
-	public void setSrcAgentID(AgentID srcAgentID) {
-		this.srcAgentID = srcAgentID;
-	}
+    public void setSrcAgentID(AgentID srcAgentID) {
+        this.srcAgentID = srcAgentID;
+    }
 
-	public AgentID getDstAgentID() {
-		return dstAgentID;
-	}
+    public AgentID getDstAgentID() {
+        return dstAgentID;
+    }
 
-	public void setDstAgentID(AgentID dstAgentID) {
-		this.dstAgentID = dstAgentID;
-	}
+    public void setDstAgentID(AgentID dstAgentID) {
+        this.dstAgentID = dstAgentID;
+    }
 
-	public EndpointID getSrcEndpointID() {
-		return srcEndpointID;
-	}
+    public EndpointID getSrcEndpointID() {
+        return srcEndpointID;
+    }
 
-	public void setSrcEndpointID(EndpointID srcEndpointID) {
-		this.srcEndpointID = srcEndpointID;
-	}
+    public void setSrcEndpointID(EndpointID srcEndpointID) {
+        this.srcEndpointID = srcEndpointID;
+    }
 
-	public EndpointID getDstEndpointID() {
-		return dstEndpointID;
-	}
+    public EndpointID getDstEndpointID() {
+        return dstEndpointID;
+    }
 
-	public void setDstEndpointID(EndpointID dstEndpointID) {
-		this.dstEndpointID = dstEndpointID;
-	}
+    public void setDstEndpointID(EndpointID dstEndpointID) {
+        this.dstEndpointID = dstEndpointID;
+    }
 
-	public long getID() {
-		return ID;
-	}
+    public long getMsgID() {
+        return msgID;
+    }
 
-	public void setID(long id) {
-		ID = id;
-	}
+    public void setMsgID(long msgID) {
+        this.msgID = msgID;
+    }
 
-	public byte[] getData() {
-		return data;
-	}
+    public byte[] getData() {
+        return data;
+    }
 
-	public void setData(byte[] data) {
-		this.data = data;
-	}
-	
-    
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
 }
--- src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/TypeHelper.java
+++ src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/TypeHelper.java
@@ -1,27 +1,124 @@
 package org.objectweb.proactive.extra.forwardingv2.protocol;
 
+/**
+ * 
+ * TypeHelper allows formatting a Message, that is putting it under the form of a Byte array, or recovering it from a byte Array
+ *
+ */
 public class TypeHelper {
-	
-	public static int byteArrayToInt(byte[] a, int offset) {
-		int l = 0;
-		
-	    l |= a[offset++] & 0x000000FF;
-	    l |= a[offset++] & 0x0000FF00;
-	    l |= a[offset++] & 0x00FF0000;
-	    l |= a[offset++] & 0xFF000000;
-	    
-	    return l;
-	}
-	
-	public static byte[] intToByteArray(int val) {
-		byte[] b = new byte[4];
-		
-	    b[0] |= val & 0x000000FF;
-	    b[1] |= val & 0x0000FF00;
-	    b[2] |= val & 0x00FF0000;
-	    b[3] |= val & 0xFF000000;
-	    
-	    return b;
-	}
 
+    /**
+     * just to test
+     * @param args
+     */
+    public static void main(String[] args) {
+        long l = 2345667;
+        byte[] a = new byte[8];
+        longToByteArray(l, a, 0);
+
+        System.out.println("the decrypted value of l is : " + byteArrayToLong(a, 0));
+
+        int i = 234;
+        byte[] b = new byte[8];
+        longToByteArray(i, b, 0);
+
+        System.out.println("the decrypted value of i is : " + byteArrayToLong(b, 0));
+    }
+
+    /**
+     * 
+     * converts the byte representation of a long into its value.
+     * 
+     * @param a the byte array in which to find the byte representation
+     * @param offset the offset in the byte array at which to find the byte representation
+     * @return the represented long value
+     */
+    public static long byteArrayToLong(byte[] a, int offset) {
+        long l = 0;
+
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset] & 0xFF;
+
+        return l;
+    }
+
+    /**
+     * Copies the byte representation of a long into a byte array starting at the given offset
+     * 
+     * @param val the long to convert
+     * @param a the byte array in which to copy the byte representation
+     * @param offset the index of the array at which to start copying
+     */
+    public static void longToByteArray(long val, byte[] a, int offset) {
+
+        a[offset + 7] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 6] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 5] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 4] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 3] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 2] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 1] |= val & 0xFF;
+        val >>= 8;
+        a[offset] |= val & 0xFF;
+    }
+
+    /**
+     * 
+     * converts the byte representation of an int into its value as an integer.
+     * 
+     * @param a the byte array in which to find the byte representation
+     * @param offset the offset in the byte array at which to find the byte representation
+     * @return the represented int value
+     */
+
+    public static int byteArrayToInt(byte[] a, int offset) {
+        int l = 0;
+
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset++] & 0xFF;
+        l <<= 8;
+        l |= a[offset] & 0xFF;
+
+        return l;
+    }
+
+    /**
+     * Copies the byte representation of an int into a byte array starting at the given offset
+     * 
+     * @param val the int to convert
+     * @param a the byte array in which to copy the byte representation
+     * @param offset the index of the array at which to start copying
+     */
+    public static void intToByteArray(int val, byte[] a, int offset) {
+
+        a[offset + 3] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 2] |= val & 0xFF;
+        val >>= 8;
+        a[offset + 1] |= val & 0xFF;
+        val >>= 8;
+        a[offset] |= val & 0xFF;
+    }
 }
