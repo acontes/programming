9b0b8dfcf8b2fcf685d3c9f05c67d32f2079053b
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-12-10 10:27:57 +0000

    Message format
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Message_Routing@10771 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/Message.java
+++ src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/Message.java
@@ -3,7 +3,19 @@ package org.objectweb.proactive.extra.forwardingv2.protocol;
 /**
  * An object representing a message 
  *
+ * A message MUST have the following structure
+ * 
+ * MSG = LENGHT PROTO-ID  MSG_TYPE SRC_AGENT-ID SRC_ENDPOINT-ID DST_AGENT-ID DST_ENDPOINT-ID ID DATA
+ * LENGTH = long
+ * PROTO-ID = int
+ * MSG_TYPE = int
+ * SRC_AGENT-ID = long
+ * SRC_ENDPOINT-ID = long
+ * DST_AGENT-ID = long
+ * DST_ENDOPINT-ID = long
+ * ID = long
+ * DATA = byte[]
+ * 
  */
 public class Message {
-
 }
