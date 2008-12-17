bb17310b0a16070341b4d863fd74e407a4e4a840
jmartin <jmartin@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-12-17 14:58:24 +0000

    Adding constructor without parameter
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Message_Routing@10898 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/RegistrationRequestMessage.java
+++ src/Extra/org/objectweb/proactive/extra/forwardingv2/protocol/RegistrationRequestMessage.java
@@ -4,8 +4,12 @@ public class RegistrationRequestMessage extends RegistrationMessage {
 
     //TODO: ajouter ce qu'il faut pour pouvoir fabriquer des requests sans agentID
 
-    public RegistrationRequest(MessageType type, AgentID agentID) {
-        super(type, agentID);
+    public RegistrationRequestMessage(AgentID agentID) {
+        super(MessageType.REGISTRATION_REQUEST, agentID);
+    }
+    
+    public RegistrationRequestMessage() {
+        super(MessageType.REGISTRATION_REQUEST, null);
     }
 
     /**
@@ -13,7 +17,7 @@ public class RegistrationRequestMessage extends RegistrationMessage {
      * @param byteArray the byte array from which to read
      * @param offset the offset at which to find the message in the byte array
      */
-    public RegistrationRequest(byte[] byteArray, int offset) {
+    public RegistrationRequestMessage(byte[] byteArray, int offset) {
         super(byteArray, offset);
     }
 
