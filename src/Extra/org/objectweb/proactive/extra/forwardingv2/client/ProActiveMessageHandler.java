04b643d048f4ec86000d8ce2227ccc2238e7859e
jmartin <jmartin@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-12-17 09:16:30 +0000

    formatting
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Message_Routing@10875 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/forwardingv2/client/ProActiveMessageHandler.java
+++ src/Extra/org/objectweb/proactive/extra/forwardingv2/client/ProActiveMessageHandler.java
@@ -32,7 +32,7 @@ public class ProActiveMessageHandler implements MessageHandler {
     private AgentV2 agent;
 
     public ProActiveMessageHandler(AgentV2 agent) {
-    	this.agent = agent;
+        this.agent = agent;
         tpe = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS,
             new LinkedBlockingQueue<Runnable>());
     }
