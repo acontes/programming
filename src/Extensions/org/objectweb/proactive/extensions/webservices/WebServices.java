458ae3f14cd17665103b50eb5232f1ddf6e843b6
ffonteno <ffonteno@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-03-19 16:28:58 +0000

    Removed axis2 directory in examples/helloWebServices in order to use only one configuration repository of axis2.
    Added examples of a composite component exposed as a web service. This example will
    be soon moved to functional tests.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ffonteno_axis2_exportation@11624 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/webservices/WebServices.java
+++ src/Extensions/org/objectweb/proactive/extensions/webservices/WebServices.java
@@ -31,12 +31,21 @@
  */
 package org.objectweb.proactive.extensions.webservices;
 
+import java.io.File;
+
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.deployer.ContextDeployer;
+import org.mortbay.jetty.deployer.WebAppDeployer;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.HandlerCollection;
 import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.webapp.WebAppContext;
 
 import org.apache.axis2.transport.http.AxisServlet;
 
 import org.objectweb.fractal.api.Component;
 import org.objectweb.proactive.annotation.PublicAPI;
+import org.objectweb.proactive.core.config.PAProperties;
 import org.objectweb.proactive.core.httpserver.HTTPServer;
 import org.objectweb.proactive.core.util.log.Loggers;
 import org.objectweb.proactive.core.util.log.ProActiveLogger;
@@ -96,7 +105,7 @@ public final class WebServices extends WSConstants {
     public static void exposeAsWebService(Object o, String url, String urn) {
         PADeployer.deploy(o, url, urn, null, false);
     }
-    
+
     /**
      * Delete the service on a web server
       *
@@ -139,7 +148,7 @@ public final class WebServices extends WSConstants {
     public static void exposeComponentAsWebService(Component component, String url, String componentName) {
         PADeployer.deployComponent(component, url, componentName, null);
     }
-    
+
     /**
      * Undeploy all the interfaces of a component deployed on a web server
      *
