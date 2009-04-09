a08d6add97fd3d9a50898e49e09e9d3b7371006f
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-09 15:10:05 +0000

    succ method added
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@11885 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/SpaceType.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/SpaceType.java
@@ -6,13 +6,22 @@ package org.objectweb.proactive.extra.dataspaces;
 import java.io.Serializable;
 
 /**
- * 
- *
+ * Data space type with defined succ method.
  */
 public enum SpaceType implements Serializable, Comparable<SpaceType> {
-    INPUT, OUTPUT, SCRATCH;
-    
-    public String getDirectoryName() {
+	INPUT, OUTPUT, SCRATCH;
+
+	public String getDirectoryName() {
 		return name().toLowerCase();
+	}
+
+	public SpaceType succ() {
+		final SpaceType[] v = SpaceType.values();
+		final int n = this.ordinal() + 1;
+
+		if (n < v.length)
+			return v[n];
+		else
+			return null;
 	}
 }
