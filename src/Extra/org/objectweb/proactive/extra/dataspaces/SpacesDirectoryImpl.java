985a4cc5509b674be05b58cb24c9d2a8d2e72d62
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-09 15:10:45 +0000

    generating NextSpaceURIKey moved to SpaceURI class as a static method NextSpaceURI
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@11889 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/SpacesDirectoryImpl.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/SpacesDirectoryImpl.java
@@ -24,7 +24,7 @@ public class SpacesDirectoryImpl implements SpacesDirectory {
 	 * .objectweb.proactive.extra.dataspaces.SpaceURI)
 	 */
 	public Collection<SpaceInstanceInfo> lookupAll(SpaceURI uri) {
-		final SpaceURI nextKey = new NextSpaceURIKey(uri);
+		final SpaceURI nextKey = SpaceURI.nextSpaceURI(uri);
 
 		synchronized (data) {
 			final SortedMap<SpaceURI, SpaceInstanceInfo> sub = data.subMap(uri, nextKey);
@@ -75,41 +75,6 @@ public class SpacesDirectoryImpl implements SpacesDirectory {
 	public void unregister(SpaceURI uri) {
 		synchronized (data) {
 			data.remove(uri);
-		}
-	}
-
-	/**
-	 * Helper class for generating next space uri key in the same path level.
-	 */
-	private class NextSpaceURIKey extends SpaceURI {
-
-		public NextSpaceURIKey(SpaceURI key) {
-			super(key);
-
-			// case: appid/type/name/
-			// case: appid/type/rt/node/
-			if (key.isComplete())
-				throw new IllegalArgumentException(
-						"Source key uri is complete. Doesn't make sens, giving up.");
-
-			// case: appid/ - just ++
-			if (dsType == null) {
-				appId++;
-				return;
-			}
-
-			// case: appid/SCRATCH/rt/ - just build next rt string
-			if (dsType == SpaceType.SCRATCH && runtimeId != null) {
-				runtimeId = runtimeId + '\0';
-				return;
-			}
-
-			// case: appid/type/ - paste a next type
-			dsType = dsType.succ();
-
-			// case: appid/last_type/ - there was no next type?
-			if (dsType == null)
-				appId++;
 		}
 	}
 }
