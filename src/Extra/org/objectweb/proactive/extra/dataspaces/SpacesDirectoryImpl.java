bd85ff7c8ae9845d9bbf6e144464995cc946f7c8
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-09 08:50:48 +0000

    SpaceURI not needed in register method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@11874 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/SpacesDirectoryImpl.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/SpacesDirectoryImpl.java
@@ -5,31 +5,30 @@ package org.objectweb.proactive.extra.dataspaces;
 
 import java.util.Set;
 
-
 /**
  * 
  *
  */
 public class SpacesDirectoryImpl implements SpacesDirectory {
 
-    public Set<SpaceInstanceInfo> lookupAll(SpaceURI uri) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public Set<SpaceInstanceInfo> lookupAll(SpaceURI uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public SpaceInstanceInfo lookupFirst(SpaceURI uri) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public SpaceInstanceInfo lookupFirst(SpaceURI uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public void register(SpaceURI uri, SpaceInstanceInfo spaceInstanceInfo) {
-        // TODO Auto-generated method stub
+	public void register(SpaceInstanceInfo spaceInstanceInfo) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    public void unregister(SpaceURI uri) {
-        // TODO Auto-generated method stub
+	public void unregister(SpaceURI uri) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
 }
