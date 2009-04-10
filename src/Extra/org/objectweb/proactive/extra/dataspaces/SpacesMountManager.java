1a162211406b39959e5ce6378ba12582866547e8
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-10 14:01:10 +0000

    close method added
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@11928 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/SpacesMountManager.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/SpacesMountManager.java
@@ -5,23 +5,27 @@ package org.objectweb.proactive.extra.dataspaces;
 
 import java.util.Set;
 
-
 /**
- * resp:
-	- (internally) manages VFS junctions adding/deleting
-	- (externally) manages mounting / unmounting
-	- creates VirtualFileSystem from instance of VFSManager
-	- contacts CachingSpacesDirectory (or trough SpaceDirectory interface)
-col:
-	- CachingSpacesDirectory (or SpaceDirectory interface)
+ * resp: - (internally) manages VFS junctions adding/deleting - (externally)
+ * manages mounting / unmounting - creates VirtualFileSystem from instance of
+ * VFSManager - contacts CachingSpacesDirectory (or trough SpaceDirectory
+ * interface) col: - CachingSpacesDirectory (or SpaceDirectory interface)
  */
 public class SpacesMountManager {
 
-    public Object resolveFile(SpaceURI spaceURI) {
-        return null;
-    }
+	public Object resolveFile(SpaceURI spaceURI) {
+		return null;
+	}
 
-    public Set<Object> resolveSpaces(SpaceURI spaceURI) {
-        return null;
-    }
+	public Set<Object> resolveSpaces(SpaceURI spaceURI) {
+		return null;
+	}
+
+	/**
+	 * Removes all junctions, mounted file systems. Forgets about the
+	 * VirtualFileSystem instance.
+	 */
+	public void close() {
+		// TODO auto generated
+	}
 }
