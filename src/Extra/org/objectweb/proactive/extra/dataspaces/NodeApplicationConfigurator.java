664c3d817e9c0953e2fc810da0995b1de3f755bf
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-10 14:01:13 +0000

    implementation added and javadoc cleaned
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@11929 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/NodeApplicationConfigurator.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/NodeApplicationConfigurator.java
@@ -6,25 +6,107 @@ package org.objectweb.proactive.extra.dataspaces;
 import org.apache.commons.vfs.impl.DefaultFileSystemManager;
 
 /**
- * resp: - maintaines configuration for application - creation logic: creates
- * scratch SpaceInstanceInfo from SpaceConfiguration and app id connects to
- * NamingService and creates CachingSpacesDirectory for that NamingService.
- * registers scratch SpaceInstanceInfo through CachingSpacesDirectory creates
- * SpacesMountManager with CachingSpacesDirectory and VFS manager - cleaning up:
- * - closes SpacesMountManager (removes all junctions, mounted file systems,
- * forget about the VirtualFileSystem instance) - unregisters scratch from
- * NamingService (and closes connection?) - removes scratch data space directory
- * content - creates and returns DataSpacesImpl col: DataSpacesImpl
- * SpacesMountManager CachingSpacesDirectory AppScratchSpace
- * 
+ * Maintains configuration for application and its life cycle. Each instance can
+ * be configured only once.
  */
 public class NodeApplicationConfigurator {
 
-	public DataSpacesImpl configureApplication(long appid, String namingServiceURL,
-			DefaultFileSystemManager manager, NodeScratchSpace scratch) {
-		return null;
+	private SpacesMountManager spacesMountManager;
+
+	private ApplicationScratchSpace applicationScratchSpace;
+
+	private SpacesDirectory cachingDirectory;
+
+	private NamingService namingService;
+
+	private DataSpacesImpl impl;
+
+	private SpaceInstanceInfo scratchInfo;
+
+	private boolean configured = false;
+
+	public static NamingService createNamingServiceStub(String url) {
+		// TODO
+		return new NamingService();
 	}
 
-	public void close() {
+	public static void closeNamingServiceStub(NamingService stub) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Builds application-related objects. Can be called only once per instance.
+	 * Scenario:
+	 * <ol>
+	 * <li>Obtains NamingService stub and creates CachingSpacesDirectory for it</li>
+	 * <li>Obtains ApplicationScratchSpace for a given <code>appid</code> from
+	 * existing NodeScratchSpace.</li>
+	 * <li>Obtains scratch SpaceInstanceInfo from ApplicationScratchSpace and
+	 * registers it through CachingSpacesDirectory</li>
+	 * <li>Creates SpacesMountManager with CachingSpacesDirectory and VFS
+	 * manager</li>
+	 * <li>Creates DataSpacesImpl for built configuration objects.</li>
+	 * </ol>
+	 * 
+	 * @param appid
+	 * @param namingServiceURL
+	 * @param manager
+	 * @param nodeScratchSpace
+	 * @return
+	 * @throws IllegalStateException
+	 *             when trying to reconfigure the instance.
+	 */
+	public DataSpacesImpl configureApplication(long appid, String namingServiceURL,
+			DefaultFileSystemManager manager, NodeScratchSpace nodeScratchSpace) throws IllegalStateException {
+
+		if (configured)
+			throw new IllegalStateException("This instance has been already configured");
+
+		// create naming service stub with URL and decorate it with local cache
+		namingService = NodeApplicationConfigurator.createNamingServiceStub(namingServiceURL);
+		cachingDirectory = new CachingSpacesDirectory(namingService);
+
+		// create scratch data space for this application and register it
+		applicationScratchSpace = nodeScratchSpace.initForApplication(appid);
+		scratchInfo = applicationScratchSpace.getSpaceInstanceInfo();
+		cachingDirectory.register(scratchInfo);
+
+		// create SpacesMountManager
+		// TODO
+		spacesMountManager = new SpacesMountManager();
+
+		// create implementation object connected to the application's
+		// configuration
+		// TODO
+		impl = new DataSpacesImpl();
+
+		configured = true;
+		return impl;
+	}
+
+	/**
+	 * Cleans up application-related objects if the have been configured:
+	 * <ol>
+	 * <li>Closes SpacesMountManager.</li>
+	 * <li>Unregisters scratch data space from the cache (and NamingService).</li>
+	 * <li>Closes NamingService stub.</li>
+	 * <li>Closes ApplicationScratchSpace.</li>
+	 * </ol>
+	 * 
+	 * @throws IllegalStateException
+	 *             when instance has not been configured.
+	 */
+	public void close() throws IllegalStateException {
+		if (!configured)
+			throw new IllegalStateException("This instance has not been configured");
+
+		spacesMountManager.close();
+		cachingDirectory.unregister(scratchInfo.getMountingPoint());
+		NodeApplicationConfigurator.closeNamingServiceStub(namingService);
+		applicationScratchSpace.close();
+	}
+
+	public DataSpacesImpl getDataSpaceImpl() {
+		return impl;
 	}
 }
