3b6e0477727d8b5517853a32b6b0ead617397233
fannykilanga <fannykilanga@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-10 11:59:13 +0000

    odre d'ajout des voisins nord , est , sud ,ouest
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@11902 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/ActiveGrid2D.java
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/ActiveGrid2D.java
@@ -8,6 +8,7 @@ import org.objectweb.proactive.core.node.NodeException;
 import org.objectweb.proactive.extensions.structuredp2p.util.Deployment;
 import org.objectweb.proactive.gcmdeployment.GCMVirtualNode;
 
+
 /**
  * 
  * @author Kilanga Fanny
@@ -17,104 +18,111 @@ import org.objectweb.proactive.gcmdeployment.GCMVirtualNode;
  * @version 0.1
  */
 public class ActiveGrid2D {
-	private int nbRows;
-	private int nbCols;
+    private int nbRows;
+    private int nbCols;
 
-	private ArrayList<AwareObject> elements;
+    private ArrayList<AwareObject> elements;
 
-	public ActiveGrid2D(int row, int col) {
-		this.nbRows = row;
-		this.nbCols = col;
-		this.elements = new ArrayList<AwareObject>(this.nbRows * this.nbCols);
-	}
+    public ActiveGrid2D(int row, int col) {
+        this.nbRows = row;
+        this.nbCols = col;
+        this.elements = new ArrayList<AwareObject>(this.nbRows * this.nbCols);
+    }
 
-	/**
-	 * 
-	 * @param descriptor
-	 */
-	public void createCoordinates(String descriptor) {
-		int nbElem = this.nbCols * this.nbRows;
+    /*
+     * *
+     * 
+     * @param descriptor
+     */
+    public void createCoordinates(String descriptor) {
+        int nbElem = this.nbCols * this.nbRows;
 
-		try {
-			ArrayList<GCMVirtualNode> listVn = Deployment.getAllVirtualNodes();
+        try {
+            ArrayList<GCMVirtualNode> listVn = Deployment.getAllVirtualNodes();
 
-			for (int i = 0; i < nbElem; i++) {
+            for (int i = 0; i < nbElem; i++) {
 
-				AwareObject e = (AwareObject) PAActiveObject.newActive(
-						AwareObject.class.getName(), new Object[] {}, listVn
-								.get(i % listVn.size()).getANode());
+                AwareObject e = (AwareObject) PAActiveObject.newActive(AwareObject.class.getName(),
+                        new Object[] {}, listVn.get(i % listVn.size()).getANode());
 
-				this.elements.add(e);
-			}
-			// initialisation des cooordonnees
-			initCordinates();
-			// initialisation de la liste des voisins de chaque element
-			for (int i = 0; i < elements.size(); i++) {
-				this.initNeighbors(elements.get(i));
-			}
+                this.elements.add(e);
+            }
+            // initialisation des cooordonnees
+            initCordinates();
+            // initialisation de la liste des voisins de chaque element
+            for (int i = 0; i < elements.size(); i++) {
+                this.initNeighbors(elements.get(i));
+            }
 
-		} catch (NodeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ProActiveException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        } catch (NodeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ProActiveException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void initCordinates() {
-		int index = 0;
+    public void initCordinates() {
+        int index = 0;
 
-		for (int i = 0; i < this.nbRows; i++) { // Y
-			for (int j = 0; j < nbCols; j++) { // X
-				this.elements.get(index).setX(j);
-				this.elements.get(index++).setY(i);
-			}
-		}
-	}
+        for (int i = 0; i < this.nbRows; i++) { // Y
+            for (int j = 0; j < nbCols; j++) { // X
+                this.elements.get(index).setX(j);
+                this.elements.get(index++).setY(i);
+            }
+        }
+    }
 
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public AwareObject getByCoordinates(int x, int y) {
-		for (int i = 0; i < elements.size(); i++) {
-			if (elements.get(i).getX() == x && elements.get(i).getY() == y) {
-				return elements.get(i);
-			}
-		}
-		return null;
-	}
+    /*
+     * *
+     * 
+     * @param x
+     * 
+     * @param y
+     * 
+     * @return
+     */
+    public AwareObject getByCoordinates(int x, int y) {
+        for (int i = 0; i < elements.size(); i++) {
+            if (elements.get(i).getX() == x && elements.get(i).getY() == y) {
+                return elements.get(i);
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * 
-	 * @param e
-	 */
-	public void initNeighbors(AwareObject e) {
+    /*
+     * *
+     * 
+     * @param e
+     */
+    public void initNeighbors(AwareObject e) {
 
-		int myX = e.getX();
-		int myY = e.getY();
+        int myX = e.getX();
+        int myY = e.getY();
+        //North
+        if (!this.getByCoordinates(myX, myY + 1).equals(null)) {
 
-		if (!this.getByCoordinates(myX + 1, myY).equals(null)) {
-			e.add(this.getByCoordinates(myX + 1, myY));
-		}
+            e.add(this.getByCoordinates(myX, myY + 1));
+        }
+        //East
+        if (!this.getByCoordinates(myX + 1, myY).equals(null)) {
+            e.add(this.getByCoordinates(myX + 1, myY));
+        }
 
-		if (!this.getByCoordinates(myX, myY + 1).equals(null)) {
+        //South
+        if (!this.getByCoordinates(myX, myY - 1).equals(null)) {
 
-			e.add(this.getByCoordinates(myX, myY + 1));
-		}
+            e.add(this.getByCoordinates(myX, myY - 1));
+        }
+        //West
+        if (!this.getByCoordinates(myX - 1, myY).equals(null)) {
 
-		if (!this.getByCoordinates(myX - 1, myY).equals(null)) {
+            e.add(this.getByCoordinates(myX - 1, myY));
+        }
 
-			e.add(this.getByCoordinates(myX - 1, myY));
-		}
-		if (!this.getByCoordinates(myX, myY - 1).equals(null)) {
-
-			e.add(this.getByCoordinates(myX, myY - 1));
-		}
-	}
+    }
 
 }
--- src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/AwareObject.java
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/AwareObject.java
@@ -2,6 +2,7 @@ package org.objectweb.proactive.extensions.structuredp2p.grid2D;
 
 import java.io.Serializable;
 
+
 /**
  * An aware object is an object which is aware of his neighbors.
  * 
@@ -13,73 +14,74 @@ import java.io.Serializable;
  */
 @SuppressWarnings("serial")
 public class AwareObject implements Serializable {
-	private int x;
-	private int y;
+    private int x;
+    private int y;
 
-	private Object[] neighbors = new Object[4];
-	private int index = 1;
+    private Object[] neighbors = new Object[4];
+    private int index = 0;
 
-	/**
-	 * The no-argument constructor as commanded by ProActive.
-	 */
-	public AwareObject() {
+    /*
+     * * The no-argument constructor as commanded by ProActive.
+     */
+    public AwareObject() {
 
-	}
+    }
 
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 */
-	public AwareObject(int x, int y) {
+    /*
+     * *
+     * 
+     * @param x
+     * 
+     * @param y
+     */
+    public AwareObject(int x, int y) {
+        this.x = x;
+        this.y = y;
 
-	}
+    }
 
-	public void add(AwareObject e) {
-		this.neighbors[this.index] = e;
-		this.index++;
-	}
+    public void add(AwareObject e) {
+        this.neighbors[this.index] = e;
+        this.index++;
+    }
 
-	/**
-	 * Returns the x-coordinate of the current object for the grid to which it
-	 * belongs.
-	 * 
-	 * @return the x-coordinate of the current object for the grid to which it
-	 *         belongs.
-	 */
-	public int getX() {
-		return x;
-	}
+    /*
+     * * Returns the x-coordinate of the current object for the grid to which it belongs.
+     * 
+     * @return the x-coordinate of the current object for the grid to which it belongs.
+     */
+    public int getX() {
+        return x;
+    }
 
-	/**
-	 * Returns the y-coordinate of the current object for the grid to which it
-	 * belongs.
-	 * 
-	 * @return the y-coordinate of the current object for the grid to which it
-	 *         belongs.
-	 */
-	public int getY() {
-		return y;
-	}
+    /*
+     * * Returns the y-coordinate of the current object for the grid to which it belongs.
+     * 
+     * @return the y-coordinate of the current object for the grid to which it belongs.
+     */
+    public int getY() {
+        return y;
+    }
 
-	/**
-	 * 
-	 * @return
-	 */
-	public Object[] getNeighbors() {
-		return neighbors;
-	}
+    /*
+     * *
+     * 
+     * @return
+     */
+    public Object[] getNeighbors() {
+        return neighbors;
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public void setNeighbors(Object[] neighbors) {
-		this.neighbors = neighbors;
-	}
+    public void setNeighbors(Object[] neighbors) {
+        this.neighbors = neighbors;
+    }
 
 }
