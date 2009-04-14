023117d33c3b14d8271f8f5ba56bb9529e5d2c85
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-14 09:02:25 +0000

    Implementation and javadoc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@11941 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/NodeConfigurator.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/NodeConfigurator.java
@@ -3,41 +3,94 @@
  */
 package org.objectweb.proactive.extra.dataspaces;
 
-import org.objectweb.proactive.core.node.Node;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.impl.DefaultFileSystemManager;
 
 /**
- * resp: - maintaines node immutable configuration (scratch configuration): 1.
- * configures VFS manager 2. stores scratch SpaceConfiguration 3. initializes &
- * tests scratch data space - for node configuration for application (app id,
- * naming service) creates and initializes NodeApplicationConfigurator - obtains
- * DataSpacesImpl from NodeApplicationConfigurator
- * 
+ * Represents immutable configuration for a node. Produces configuration for
+ * application by creating NodeApplicationConfigurator. Objects life cycle:
+ * <ol>
+ * <li>Instance initialization by default constructor.</li>
+ * <li><code>configureNode</code> method call for passing node-specific and
+ * immutable settings. This can be called only once for each instance.</li>
+ * <li><code>configureApplication</code> method call for configuring application
+ * on a node. This involves creation of {@link NodeApplicationConfigurator}.</li>
+ * <li>Obtaining {@link NodeApplicationConfigurator} if needed.</li>
+ * <li>Closing all composite objects by <code>close</code> method call.</li>
+ * </ol>
  */
 public class NodeConfigurator {
 
+	private SpaceConfiguration scratchConfiguration;
+
+	private boolean configured = false;
+
+	private DefaultFileSystemManager manager = null;
+
+	private NodeScratchSpace nodeScratchSpace;
+
+	private NodeApplicationConfigurator appConfigurator = null;
+
 	/**
-	 * init?
+	 * <ol>
+	 * <li>Stores VFS manager</li>
+	 * <li>Stores scratch SpaceConfiguration</li>
+	 * <li>Initializes & tests scratch data space</li>
+	 * </ol>
 	 * 
 	 * @param config
 	 *            scratch data space configuration
+	 * @throws IllegalStateException
+	 *             when trying to reconfigure the instance
 	 */
-	public void configureNode(SpaceConfiguration config) {
+	synchronized public void configureNode(SpaceConfiguration config) throws IllegalStateException {
+		if (configured)
+			throw new IllegalStateException("This instance has been already configured");
 
+		try {
+			manager = VFSFactory.createDefaultFileSystemManager();
+		} catch (FileSystemException e) {
+			// TODO node cannot be used!
+			e.printStackTrace();
+		}
+
+		scratchConfiguration = config;
+		nodeScratchSpace = new NodeScratchSpace(config, manager);
+		nodeScratchSpace.init();
+
+		configured = true;
 	}
 
-	public DataSpacesImpl configureApplication(long appid, String namingServiceURL) {
-		return null;
+	/**
+	 * @param appid
+	 * @param namingServiceURL
+	 * @return
+	 */
+	synchronized public DataSpacesImpl configureApplication(long appid, String namingServiceURL) {
+		tryCloseAppConfigurator();
+
+		appConfigurator = new NodeApplicationConfigurator();
+		return appConfigurator.configureApplication(appid, namingServiceURL, manager, nodeScratchSpace);
 	}
 
 	public NodeApplicationConfigurator getNodeApplicationConfigurator() {
-		return null;
+		return appConfigurator;
 	}
 
-	public DataSpacesImpl getDataSpacesImpl(Node n) {
-		return null;
+	/**
+	 * Closes all components owned by this configurator.
+	 */
+	synchronized public void close() {
+		tryCloseAppConfigurator();
 	}
 
-	public void close() {
-
+	/**
+	 * Helper method for closing when needed.
+	 */
+	private void tryCloseAppConfigurator() {
+		if (appConfigurator != null) {
+			appConfigurator.close();
+			appConfigurator = null;
+		}
 	}
 }
