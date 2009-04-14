e4dd139df3e733112a3cddb89aa5ff4b9bd77e75
plaurent <plaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-14 20:01:53 +0000

    Code refactor.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@11970 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/AwareObject.java
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/AwareObject.java
@@ -3,6 +3,7 @@ package org.objectweb.proactive.extensions.structuredp2p.grid2D;
 import java.io.Serializable;
 
 import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.api.PAFuture;
 
 /**
  * An aware object is an object which is aware of his neighbors.
@@ -15,8 +16,8 @@ import org.objectweb.proactive.api.PAActiveObject;
  */
 @SuppressWarnings("serial")
 public class AwareObject implements Serializable {
-	private int x;
-	private int y;
+	private int x = 0;
+	private int y = 0;
 	private AwareObject stub;
 
 	private AwareObject northNeighbor = null;
@@ -39,10 +40,96 @@ public class AwareObject implements Serializable {
 	 * @param y
 	 *            the y-coordinate in the grid2d.
 	 */
-	public AwareObject(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public AwareObject(Integer x, Integer y) {
+		this.x = x.intValue();
+		this.y = y.intValue();
 		this.stub = (AwareObject) PAActiveObject.getStubOnThis();
+	}
+
+	/**
+	 * Lookup a {@link AwareObject} by it coordinates from the current object
+	 * and returns it.
+	 * 
+	 * @param x
+	 *            the x-coordinate to lookup.
+	 * @param y
+	 *            the y-coordinate to lookup.
+	 * @return the AwareObject find or null.
+	 */
+	public AwareObject find(int x, int y) {
+		System.out.println("this.x = " + this.x + ", x = " + x);
+		System.out.println("this.y = " + this.y + ", y = " + y + "\n");
+
+		if (this.x == x && this.y == y)
+			return this;
+
+		if (this.x != x) {
+			if (this.eastNeighbor == null) {
+				return null;
+			}
+
+			return this.eastNeighbor.find(x, y);
+		} else if (this.y != y) {
+			if (this.southNeighbor == null) {
+				return null;
+			}
+
+			return this.southNeighbor.find(x, y);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Register a new peer as a neighbor.
+	 * 
+	 * @param peerStub
+	 *            the stub of the peer to register.
+	 * 
+	 * @throws Exception
+	 *             if the position is already token.
+	 */
+	public void registerNewPeer(AwareObject peerStub) throws Exception {
+		int peerX = peerStub.getX();
+		int peerY = peerStub.getY();
+
+		System.out.println("REGISTER START FOR x=" + peerX + ", y=" + peerY);
+
+		if (PAFuture.getFutureValue(this.find(peerX, peerY)) != null)
+			throw new Exception("This position is already used (x="
+					+ this.getX() + ", y=" + this.getY() + ")");
+
+		AwareObject northObj = this.find(peerX, peerY - 1);
+		AwareObject eastObj = this.find(peerX + 1, peerY);
+		AwareObject southObj = this.find(peerX, peerY + 1);
+		AwareObject westObj = this.find(peerX - 1, peerY);
+
+		System.out.println("North --------->");
+		if (PAFuture.getFutureValue(northObj) != null) {
+			peerStub.setNorthNeighbor(northObj);
+			northObj.setSouthNeighbor(peerStub);
+		}
+
+		System.out.println("East --------->");
+		if (PAFuture.getFutureValue(eastObj) != null) {
+			peerStub.setEastNeighbor(eastObj);
+			eastObj.setWestNeighbor(peerStub);
+		}
+
+		System.out.println("South --------->");
+		if (PAFuture.getFutureValue(southObj) != null) {
+			peerStub.setSouthNeighbor(southObj);
+			southObj.setNorthNeighbor(peerStub);
+		}
+
+		System.out.println("West --------->");
+		if (PAFuture.getFutureValue(westObj) != null) {
+			peerStub.setWestNeighbor(westObj);
+			westObj.setEastNeighbor(peerStub);
+		}
+
+		System.out.println("--------- NEW AWARE-OBJECT REGISTERED ---------");
+		System.out.println(peerStub);
 	}
 
 	/**
@@ -77,60 +164,76 @@ public class AwareObject implements Serializable {
 	}
 
 	/**
-	 * @return the northNeighbor
+	 * Returns the north neighbor of the current object.
+	 * 
+	 * @return the northNeighbor of the current object.
 	 */
 	public AwareObject getNorthNeighbor() {
 		return northNeighbor;
 	}
 
 	/**
-	 * @return the eastNeighbor
+	 * Returns the east neighbor of the current object.
+	 * 
+	 * @return the eastNeighbor of the current object.
 	 */
 	public AwareObject getEastNeighbor() {
 		return eastNeighbor;
 	}
 
 	/**
-	 * @return the southNeighbor
+	 * Returns the south neighbor of the current object.
+	 * 
+	 * @return the southNeighbor of the current object.
 	 */
 	public AwareObject getSouthNeighbor() {
 		return southNeighbor;
 	}
 
 	/**
-	 * @return the westNeighbor
+	 * Returns the west neighbor of the current object.
+	 * 
+	 * @return the westNeighbor of the current object.
 	 */
 	public AwareObject getWestNeighbor() {
 		return westNeighbor;
 	}
 
 	/**
+	 * Sets the north neighbor of the current object.
+	 * 
 	 * @param northNeighbor
-	 *            the northNeighbor to set
+	 *            the new northNeighbor to set.
 	 */
 	public void setNorthNeighbor(AwareObject northNeighbor) {
 		this.northNeighbor = northNeighbor;
 	}
 
 	/**
+	 * Sets the east neighbor of the current object.
+	 * 
 	 * @param eastNeighbor
-	 *            the eastNeighbor to set
+	 *            the new eastNeighbor to set.
 	 */
 	public void setEastNeighbor(AwareObject eastNeighbor) {
 		this.eastNeighbor = eastNeighbor;
 	}
 
 	/**
+	 * Sets the south neighbor of the current object.
+	 * 
 	 * @param southNeighbor
-	 *            the southNeighbor to set
+	 *            the new southNeighbor to set.
 	 */
 	public void setSouthNeighbor(AwareObject southNeighbor) {
 		this.southNeighbor = southNeighbor;
 	}
 
 	/**
+	 * Sets the west neighbor of the current object.
+	 * 
 	 * @param westNeighbor
-	 *            the westNeighbor to set
+	 *            the new westNeighbor to set.
 	 */
 	public void setWestNeighbor(AwareObject westNeighbor) {
 		this.westNeighbor = westNeighbor;
@@ -156,13 +259,47 @@ public class AwareObject implements Serializable {
 		this.y = y;
 	}
 
+	/**
+	 * Returns a description for a given neighbor.
+	 * 
+	 * @param neighbor
+	 *            the neighbor we want to get description.
+	 * @return a description for a given neighbor.
+	 */
+	private String getNeighborDescription(AwareObject neighbor) {
+		StringBuffer buf = new StringBuffer();
+
+		if (neighbor == null) {
+			buf.append("null");
+		} else {
+			buf.append("x=" + neighbor.getX() + ",");
+			buf.append("y=" + neighbor.getY());
+		}
+
+		return buf.toString();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public String toString() {
 		StringBuffer buf = new StringBuffer("AwareObject x=" + this.x + ", y="
 				+ this.y + "\n");
-		buf.append("  north = " + this.northNeighbor + "\n");
-		buf.append("  east = " + this.eastNeighbor + "\n");
-		buf.append("  south = " + this.southNeighbor + "\n");
-		buf.append("  west = " + this.westNeighbor + "\n");
+		buf.append("\n");
+		buf.append("\t\t");
+		buf.append(getNeighborDescription(this.northNeighbor));
+		buf.append("\n");
+		buf.append("\t\t ^ \t\t\n");
+		buf.append("\t\t | \n");
+		buf.append(getNeighborDescription(this.westNeighbor));
+		buf.append(" <- \t x \t -> ");
+		buf.append(getNeighborDescription(this.eastNeighbor));
+		buf.append("\n");
+		buf.append("\t\t |\n");
+		buf.append("\t\t |\n");
+		buf.append("\t\t");
+		buf.append(getNeighborDescription(this.southNeighbor));
+		buf.append("\n");
 
 		return buf.toString();
 	}
--- src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/Launcher.java
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/grid2D/Launcher.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import org.objectweb.proactive.ActiveObjectCreationException;
 import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.node.Node;
 import org.objectweb.proactive.core.node.NodeException;
 import org.objectweb.proactive.extensions.structuredp2p.util.Deployment;
 
@@ -21,6 +22,8 @@ import org.objectweb.proactive.extensions.structuredp2p.util.Deployment;
 public class Launcher {
 	private int nbCols;
 	private int nbRows;
+	private static Node node;
+	private static AwareObject entryPoint;
 
 	/**
 	 * Constructor.
@@ -33,9 +36,102 @@ public class Launcher {
 	public Launcher(int rows, int cols) {
 		this.nbRows = rows;
 		this.nbCols = cols;
+		System.out.println("Grid " + cols + "x" + rows);
 	}
 
+	/**
+	 * Creates the entry point. It's the required {@link AwareObject} in order
+	 * to enter in the AwareObject grid network.
+	 */
+	public void createEntryPoint() {
+		node = Deployment.getVirtualNode("Grid2D").getANode();
+		try {
+			Launcher.entryPoint = (AwareObject) PAActiveObject.newActive(
+					AwareObject.class.getName(), new Object[] { new Integer(0),
+							new Integer(0) }, node);
+
+			// Binds the entry point to a specific URL on the RMI registry
+			PAActiveObject.registerByName(Launcher.entryPoint,
+					"Grid2DEntryPoint");
+		} catch (ActiveObjectCreationException e) {
+			e.printStackTrace();
+		} catch (NodeException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Print app menu option on the standard output.
+	 */
+	private static void printOptions() {
+		System.out.println("Select one option :");
+		System.out
+				.println("  * Enter a coordinate like '0 1' in order to see neighbor of this peer");
+		System.out.println("  * Enter 'quit' in order to quit the application");
+	}
+
+	/**
+	 * Creates <code>nbRows</code> <code>nbCols</code> AwareObjects by forming a
+	 * grid2D. Each object is inserted while using the <code>entryPoint</code>.
+	 */
+	private void createsAwareObjects() {
+		int nbAwareObjects = this.nbCols * this.nbRows;
+		int x = 0;
+		int y = 0;
+
+		AwareObject entryPoint = null;
+		AwareObject newAwareObject = null;
+
+		// Retrieve entryPoint
+		try {
+			entryPoint = (AwareObject) PAActiveObject.lookupActive(
+					AwareObject.class.getName(), "Grid2DEntryPoint");
+			System.out.println("EntryPoint retrieved !");
+		} catch (ActiveObjectCreationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 1; i < nbAwareObjects; i++) {
+			if (i % this.nbCols == 0) {
+				x = 0;
+				y++;
+			} else {
+				x++;
+			}
+
+			try {
+				newAwareObject = (AwareObject) PAActiveObject
+						.newActive(AwareObject.class.getName(), new Object[] {
+								new Integer(x), new Integer(y) }, Launcher.node);
+				entryPoint.registerNewPeer(newAwareObject);
+			} catch (ActiveObjectCreationException e) {
+				e.printStackTrace();
+			} catch (NodeException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Entry point of the application.
+	 * 
+	 * @param args
+	 *            parameters given to the application when launched.
+	 */
 	public static void main(String args[]) {
+		if (args.length != 4) {
+			System.err.println("Usage : java "
+					+ Launcher.class.getCanonicalName() + " " + args[0]
+					+ " descriptor nbRows nbCols");
+		}
+
 		try {
 			Deployment.deploy(args[1]);
 		} catch (NodeException e) {
@@ -44,101 +140,46 @@ public class Launcher {
 			e.printStackTrace();
 		}
 
-		/**
-		 * Creates the entry point, the required AwareObject in order to enter
-		 * in the AwareObject grid network.
-		 */
-		AwareObject entryPoint = null;
-		try {
-			entryPoint = (AwareObject) PAActiveObject.newActive(
-					AwareObject.class.getName(), new Object[] {}, Deployment
-							.getVirtualNode("Grid2D").getANode());
-		} catch (ActiveObjectCreationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NodeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		final int nbRows = Integer.parseInt(args[2]);
+		final int nbCols = Integer.parseInt(args[3]);
 
-		// Binds the entry point to a specific URL on the RMI Registry
-		try {
-			PAActiveObject.registerByName(entryPoint, "Grid2DEntryPoint");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		Launcher launcher = new Launcher(Integer.parseInt(args[2]), Integer
-				.parseInt(args[3]));
+		Launcher launcher = new Launcher(nbRows, nbCols);
+		launcher.createEntryPoint();
 		launcher.createsAwareObjects();
 
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()) {
-			if (scanner.next().equals("quit")) {
-				// Terminates the deployment on the grid.
-				Deployment.kill();
-				break;
-			}
-			Thread.yield();
-		}
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Scanner scanner = new Scanner(System.in);
+				String next;
 
-	}
-
-	private void createsAwareObjects() {
-		int nbAwareObjects = this.nbCols * this.nbRows;
-
-		// Retrieve entryPoint
-		AwareObject entryPoint = null;
-		try {
-			entryPoint = (AwareObject) PAActiveObject.lookupActive(
-					AwareObject.class.getName(), "Grid2DEntryPoint");
-		} catch (ActiveObjectCreationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		for (int i = 0; i < nbAwareObjects; i++) {
-			AwareObject newAwareObject = null;
-			AwareObject current = entryPoint;
-			try {
-				newAwareObject = (AwareObject) PAActiveObject.newActive(
-						AwareObject.class.getName(), new Object[] {},
-						Deployment.getVirtualNode("Grid2D").getANode());
-			} catch (ActiveObjectCreationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NodeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			AwareObject lastY = entryPoint;
-			for (int y = 0; y < this.nbRows; y++) {
-				for (int x = 0; x < this.nbCols; x++) {
-					AwareObject eastObject = current.getEastNeighbor();
-					if (eastObject == null) {
-						current.setEastNeighbor(newAwareObject);
-						newAwareObject.setWestNeighbor(current);
+				Launcher.printOptions();
+				while (true) {
+					next = scanner.nextLine();
+					if (next.equals("quit")) {
+						Deployment.kill();
 						break;
+					} else if (next.matches("[0-9]+ [0-9]+")) {
+						String[] coordinates = next.split("\\s");
+						AwareObject founded = null;
+
+						int x = Integer.parseInt(coordinates[0]);
+						int y = Integer.parseInt(coordinates[1]);
+
+						if (x >= nbCols || x < 0 || y >= nbRows || y < 0) {
+							System.err.println("Error, x must be in [0,"
+									+ (int) (nbCols - 1) + "] and y in [0,"
+									+ (int) (nbRows - 1) + "]");
+						} else {
+							founded = Launcher.entryPoint.find(x, y);
+							System.out.println(founded);
+						}
 					}
 
-					current = eastObject;
+					Launcher.printOptions();
 				}
-
-				current = lastY;
-
-				if (current.getSouthNeighbor() == null) {
-					current.setSouthNeighbor(newAwareObject);
-					newAwareObject.setNorthNeighbor(current);
-					break;
-				}
-
-				current = current.getSouthNeighbor();
-				lastY = current;
 			}
-		}
+
+		}).start();
 	}
 }
