35bcb7fe09a43dd5facba8b9a2cacfb30b4a39d8
plaurent <plaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-15 08:07:44 +0000

    Classpath update.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@11972 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- examples/structuredp2p/structuredp2p.sh
+++ examples/structuredp2p/structuredp2p.sh
@@ -24,6 +24,15 @@ echo --- STRUCTURED P2P - 2D Active Grid ---------------------
 
 args="-descriptor $default_descriptor $1 $2"
 
+export CLASSPATH=../../lib/*:$CLASSPATH
+export CLASSPATH=../../classes/Core:$CLASSPATH
+export CLASSPATH=../../classes/Benchmarks:$CLASSPATH
+export CLASSPATH=../../classes/Examples:$CLASSPATH
+export CLASSPATH=../../classes/Extensions:$CLASSPATH
+export CLASSPATH=../../classes/Extra:$CLASSPATH
+export CLASSPATH=../../classes/Tests:$CLASSPATH
+export CLASSPATH=../../classes/Utils:$CLASSPATH
+
 $JAVACMD org.objectweb.proactive.extensions.structuredp2p.grid2D.Launcher $args
 
 echo
