33d27e65d38e350e6c6888365ae3abc89694c516
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-16 15:10:38 +0000

    javadoc and parts of implementation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12013 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/DataSpacesImpl.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/DataSpacesImpl.java
@@ -3,16 +3,19 @@
  */
 package org.objectweb.proactive.extra.dataspaces;
 
-import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
-import javax.tools.FileObject;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemException;
+import org.objectweb.proactive.extra.dataspaces.exceptions.MalformedURIException;
 
+// TODO what about IO exceptions?
 /**
- * resp: - implements PADataSpaces API within a node (and for app) col: -
- * SpacesMountManager - CachingSpacesDirectory (or SpaceDirectory interface)
- * 
+ * Implements PADataSpaces API within a node (and an application).
  */
 public class DataSpacesImpl {
 
@@ -31,71 +34,207 @@ public class DataSpacesImpl {
 		this.appId = appId;
 	}
 
-	public FileObject resolveDefaultInput() {
-		return null;
+	/**
+	 * Implementation (more generic) method for resolveDefaultInput and
+	 * resolveDefaultOutput.
+	 * 
+	 * @see {@link PADataSpaces#resolveDefaultInput()}
+	 * @see {@link PADataSpaces#resolveDefaultOutput()}
+	 * @return FileObject received from SpacesMountManager instance
+	 * @throws IllegalArgumentException
+	 *             when specified space type is neither input nor output
+	 * @throws FileSystemException
+	 */
+	public FileObject resolveDefaultInputOutput(SpaceType type) throws IllegalArgumentException,
+			FileSystemException {
+		assertIsInputOrOutput(type);
+
+		final DataSpacesURI defaultInputURI = DataSpacesURI.createInOutSpaceURI(appId, type, type
+				.getDefaultName());
+
+		final FileObject fo = spacesMountManager.resolveFile(defaultInputURI);
+		return fo;
 	}
 
-	public FileObject resolveDefaultOutput() {
-		return null;
+	/**
+	 * @see {@link PADataSpaces#resolveScratchForAO()}
+	 * @return FileObject received from SpacesMountManager instance
+	 * @throws FileSystemException
+	 */
+	public FileObject resolveScratchForAO() throws FileSystemException {
+		final String aoid = Utils.extractAOId();
+		final DataSpacesURI scratchURI = appScratchSpace.getScratchForAO(aoid);
+		final FileObject fo = spacesMountManager.resolveFile(scratchURI);
+
+		return fo;
 	}
 
-	public FileObject resolveScratchForAO() {
-		return null;
+	/**
+	 * Implementation (more generic) method for getAllKnownInputNames and
+	 * getAllKnownInputNames.
+	 * 
+	 * @see {@link PADataSpaces#getAllKnownInputNames()}
+	 * @see {@link PADataSpaces#getAllKnownOutputNames()}
+	 * @see {@link SpacesDirectory#lookupAll(DataSpacesURI)}
+	 * 
+	 * @param type
+	 *            of data spaces (input or output)
+	 * @return set of known names
+	 * @throws IllegalArgumentException
+	 *             when specified space type is neither input nor output
+	 */
+	public Set<String> getAllKnownInputOutputNames(SpaceType type) throws IllegalArgumentException {
+		assertIsInputOrOutput(type);
+
+		final DataSpacesURI aURI = DataSpacesURI.createURI(appId, type);
+		final Set<SpaceInstanceInfo> infos = spacesDirectory.lookupAll(aURI);
+		final Set<String> names = new HashSet<String>();
+
+		for (SpaceInstanceInfo sii : infos) {
+			names.add(sii.getName());
+		}
+		return names;
 	}
 
-	public Set<String> getAllKnownInputNames() {
-		return null;
+	/**
+	 * Implementation (more generic) method for resolveAllKnownInputs and
+	 * resolveAllKnownOutputs.
+	 * 
+	 * @see {@link PADataSpaces#resolveAllKnownInputs()}
+	 * @see {@link PADataSpaces#resolveAllKnownOutputs()}
+	 * @param type
+	 * @return
+	 * @throws FileSystemException
+	 * @throws IllegalArgumentException
+	 *             when specified space type is neither input nor output
+	 */
+	public Map<String, FileObject> resolveAllKnownInputsOutputs(SpaceType type) throws FileSystemException,
+			IllegalArgumentException {
+
+		assertIsInputOrOutput(type);
+		final DataSpacesURI uri = DataSpacesURI.createURI(appId, type);
+		final Map<SpaceInstanceInfo, FileObject> spaces = spacesMountManager.resolveSpaces(uri);
+		final Map<String, FileObject> ret = new HashMap<String, FileObject>(spaces.size());
+
+		for (Entry<SpaceInstanceInfo, FileObject> entry : spaces.entrySet()) {
+			final String name = entry.getKey().getName();
+			ret.put(name, entry.getValue());
+		}
+		return ret;
 	}
 
-	public Set<String> getAllKnownOutputNames() {
-		return null;
+	/**
+	 * Implementation (more generic) method for resolveDefaultInputBlocking and
+	 * resolveDefaultOutputBlocking.
+	 * 
+	 * @see {@link PADataSpaces#resolveDefaultInputBlocking(long))}
+	 * @see {@link PADataSpaces#resolveDefaultOutputBlocking(long))}
+	 * @param timeoutMillis
+	 * @param type
+	 * @return
+	 * @throws FileSystemException
+	 * @throws IllegalArgumentException
+	 */
+	public FileObject resolveDefaultInputOutputBlocking(long timeoutMillis, SpaceType type)
+			throws IllegalArgumentException, FileSystemException {
+
+		assertIsInputOrOutput(type);
+		// TODO FIXME it is not blocking ;-)
+		return resolveDefaultInputOutput(type);
 	}
 
-	public Map<String, FileObject> resolveAllKnownInputs() {
-		return null;
+	/**
+	 * @see {@link PADataSpaces#resolveFile(String)}
+	 * @param uri
+	 * @return
+	 * @throws MalformedURIException
+	 * @throws FileSystemException
+	 */
+	public FileObject resolveFile(String uri) throws MalformedURIException, FileSystemException {
+		final DataSpacesURI spaceURI = DataSpacesURI.parseURI(uri);
+		return spacesMountManager.resolveFile(spaceURI);
 	}
 
-	public Map<String, FileObject> resolveAllKnownOutputs() {
-		return null;
-	}
-
-	public FileObject resolveDefaultInputBlocking(long timeoutMillis) {
-		return null;
-	}
-
-	public FileObject resolveDefaultOutputBlocking(long timeoutMillis) {
-		return null;
-	}
-
-	public FileObject resolveFile(String uri) {
-		return null;
-	}
-
+	/**
+	 * @see {@link PADataSpaces#getURI(FileObject)}
+	 * @param fileObject
+	 * @return
+	 */
 	public String getURI(FileObject fileObject) {
-		return null;
+		return fileObject.getName().getFriendlyURI();
 	}
 
-	public FileObject resolveInput(String name) {
-		return null;
+	/**
+	 * Implementation (more generic) method for resolveInput and resolveOutput.
+	 * 
+	 * @see {@link PADataSpaces#resolveInput(String)}
+	 * @see {@link PADataSpaces#resolveOutput(String)}
+	 * 
+	 * @param name
+	 * @param type
+	 * @return
+	 * @throws FileSystemException
+	 * @throws IllegalArgumentException
+	 *             when specified space type is neither input nor output
+	 */
+	public FileObject resolveInputOutput(String name, SpaceType type) throws FileSystemException,
+			IllegalArgumentException {
+		assertIsInputOrOutput(type);
+		final DataSpacesURI uri = DataSpacesURI.createInOutSpaceURI(appId, type, name);
+
+		return spacesMountManager.resolveFile(uri);
 	}
 
-	public FileObject resolveOutput(String name) {
-		return null;
+	/**
+	 * Implementation (more generic) method for resolveInputBlocking and
+	 * resolveOutputBlocking.
+	 * 
+	 * @see {@link PADataSpaces#resolveInputBlocking(String, long)}
+	 * @see {@link PADataSpaces#resolveOutputBlocking(String, long)}
+	 * @param name
+	 * @param timeoutMillis
+	 * @param type
+	 * @return
+	 * @throws FileSystemException
+	 * @throws IllegalArgumentException
+	 *             when specified space type is neither input nor output
+	 */
+	public FileObject resolveInputOutputBlocking(String name, long timeoutMillis, SpaceType type)
+			throws FileSystemException, IllegalArgumentException {
+
+		assertIsInputOrOutput(type);
+		// TODO FIXME not blocking yet
+		return resolveInputOutput(name, type);
 	}
 
-	public FileObject resolveInputBlocking(String name, long timeoutMillis) {
-		return null;
+	/**
+	 * Implementation (more generic) method for addInput and addOutput.
+	 * 
+	 * @see {@link PADataSpaces#addInput(String, String, String)}
+	 * @see {@link PADataSpaces#addOutput(String, String, String)}
+	 * @param name
+	 * @param path
+	 * @param url
+	 * @param type
+	 * @return
+	 * @throws IllegalArgumentException
+	 *             when specified space type is neither input nor output
+	 */
+	public String addInputOutput(String name, String path, String url, SpaceType type)
+			throws IllegalArgumentException {
+
+		assertIsInputOrOutput(type);
+
+		final String hostname = Utils.getHostnameForThis();
+		final SpaceConfiguration config = new SpaceConfiguration(url, path, hostname, type, name);
+		final SpaceInstanceInfo spaceInstanceInfo = new SpaceInstanceInfo(appId, config);
+
+		spacesDirectory.register(spaceInstanceInfo);
+		return DataSpacesURI.createInOutSpaceURI(appId, type, name).toString();
 	}
 
-	public FileObject resolveOutputBlocking(String name, long timeoutMillis) {
-		return null;
-	}
-
-	public String addInput(String name, String path, URL url) {
-		return null;
-	}
-
-	public String addOutput(String name, String path, URL url) {
-		return null;
+	private void assertIsInputOrOutput(SpaceType type) {
+		if (type == SpaceType.SCRATCH)
+			throw new IllegalArgumentException("This method can be only used with input or output data space");
 	}
 }
--- src/Extra/org/objectweb/proactive/extra/dataspaces/PADataSpaces.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/PADataSpaces.java
@@ -3,88 +3,237 @@
  */
 package org.objectweb.proactive.extra.dataspaces;
 
-import java.net.URL;
 import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.vfs.FileObject;
 
 /**
- * resp: - contains all API methods - finds out node-specific objects -
- * delegates method calls col: - DataSpacesImpl
+ * The ProActive Data Spaces API. (delegates method calls to DataSpacesImpl)
  */
 public class PADataSpaces {
 
 	private PADataSpaces() {
 	}
 
+	/**
+	 * FileObject Returns ﬁle handle for the default input data space, as deﬁned
+	 * in applica- tion descriptor or (optionally) dynamically set through API
+	 * during appli- cation execution. This call might block for a while if
+	 * there is a need to wait for start up of input provider or input needs to
+	 * be mounted (timeout excep- tion is being thrown after some conﬁgurable
+	 * period). Returned ﬁle object handle can be directly used to perform
+	 * operations on the ﬁle/directory, regardless of the underlying protocol.
+	 * Input is expected to be readable. In case of no (default) input deﬁned,
+	 * it throws an exception (compare to resolveDefaultInputBlocking()).
+	 * 
+	 * @return
+	 */
 	public static FileObject resolveDefaultInput() {
 		return null;
 	}
 
+	/**
+	 * Analogous method for accessing default output. Output is expected to be
+	 * writable from any node. Writes synchronization is a developer’s responsi-
+	 * bility.
+	 * 
+	 * @return
+	 */
 	public static FileObject resolveDefaultOutput() {
 		return null;
 	}
 
+	/**
+	 * Similar to resolveDefaultInput(), except that in case of no default in-
+	 * put deﬁned, method would block until such an input is deﬁned or timeout
+	 * expires.
+	 * 
+	 * @return
+	 */
 	public static FileObject resolveScratchForAO() {
 		return null;
 	}
 
+	/**
+	 * Returns names of every deﬁned inputs known at this time. It does not
+	 * necessarily mean that they are already mountable (nodes and ProActive-
+	 * Providers may not have started yet) nor mounted. If inputs are available
+	 * at static application descriptor, every name is guaranteed to be
+	 * returned. If some another addInput() method returned before this call was
+	 * made (locally precede this call or precede it in global real-time), name
+	 * of this input is also returned. This method doesn’t cause inputs to be
+	 * mounted, i.e. it doesn’t cause local VFS view to be refreshed.
+	 * 
+	 * @return
+	 */
 	public static Set<String> getAllKnownInputNames() {
 		return null;
 	}
 
+	/**
+	 * Analogous method for outputs.
+	 * 
+	 * @return
+	 */
 	public static Set<String> getAllKnownOutputNames() {
 		return null;
 	}
 
+	/**
+	 * Returns map of input names to ﬁle handles for every input known at this
+	 * time (see getAllInputNames()). This call might block for a while if there
+	 * is a need to wait for start up of some input provider or some input need
+	 * to be mounted. Every input is mounted in result of this call, i.e. it
+	 * does refresh local VFS view. Returned input handles are expected to be
+	 * readable.
+	 * 
+	 * @return
+	 */
 	public static Map<String, FileObject> resolveAllKnownInputs() {
 		return null;
 	}
 
+	/**
+	 * Analogous method for outputs.
+	 * 
+	 * @return
+	 */
 	public static Map<String, FileObject> resolveAllKnownOutputs() {
 		return null;
 	}
 
+	/**
+	 * Similar to resolveInput(name), except that in case of no input deﬁned
+	 * with that name, method would block until such an input is deﬁned or
+	 * timeout expires.
+	 * 
+	 * @param timeoutMillis
+	 * @return
+	 */
 	public static FileObject resolveDefaultInputBlocking(long timeoutMillis) {
 		return null;
 	}
 
+	/**
+	 * Analogous blocking method for accessing output.
+	 * 
+	 * @param timeoutMillis
+	 * @return
+	 */
 	public static FileObject resolveDefaultOutputBlocking(long timeoutMillis) {
 		return null;
 	}
 
+	/**
+	 * Returns ﬁle handle for any valid URI within some existing dataspace in
+	 * application (another AO’s scratch, input or output). This call might
+	 * block for a while if there is a need to wait for start up of some remote
+	 * provider or it needs to be mounted. Returned ﬁle handle should be
+	 * readable, but not necessarily writable. This kind of capabilities
+	 * checking is caller’s respon- sibility or it can be implied from some
+	 * objects contract (e.g. you know that input URI is passed to you).
+	 * 
+	 * @param uri
+	 * @return
+	 */
 	public static FileObject resolveFile(String uri) {
 		return null;
 	}
 
+	/**
+	 * Returns URI for a given ﬁle object, which is valid in the whole
+	 * application. I.e. it can be passed to another AO and should be resolvable
+	 * there.
+	 * 
+	 * @param fileObject
+	 * @return
+	 */
 	public static String getURI(FileObject fileObject) {
 		return null;
 	}
 
+	/**
+	 * Returns ﬁle handle for an input with speciﬁc name. This call might block
+	 * for a while if there is a need to wait for start up of input provider or
+	 * input needs to be mounted (timeout exception is being thrown after some
+	 * conﬁgurable period). Returned input is expected to be readable. In case
+	 * of no input deﬁned with that name, it throws an exception.
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public static FileObject resolveInput(String name) {
 		return null;
 	}
 
+	/**
+	 * Analogous method for output.
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public static FileObject resolveOutput(String name) {
 		return null;
 	}
 
-	public static FileObject resolveInputBlocking(String name,
-			long timeoutMillis) {
+	/**
+	 * Similar to resolveInput(name), except that in case of no input deﬁned
+	 * with that name, method would block until such an input is deﬁned or
+	 * timeout expires.
+	 * 
+	 * @param name
+	 * @param timeoutMillis
+	 * @return
+	 */
+	public static FileObject resolveInputBlocking(String name, long timeoutMillis) {
 		return null;
 	}
 
-	public static FileObject resolveOutputBlocking(String name,
-			long timeoutMillis) {
+	/**
+	 * Analogous blocking method for accessing output.
+	 * 
+	 * @param name
+	 * @param timeoutMillis
+	 * @return
+	 */
+	public static FileObject resolveOutputBlocking(String name, long timeoutMillis) {
 		return null;
 	}
 
-	public static String addInput(String name, String path, URL url) {
+	/**
+	 * Add input with a provided name during application execution. Input with
+	 * empty (null — implicitly "default") or "default" name becomes an
+	 * application default input. Input name must be unique for the application,
+	 * i.e. if some input with provided name is already registered, this method
+	 * throws exception. Input must have local path and/or global access URL
+	 * deﬁned. In case of path-based local-only access deﬁned, space is exposed
+	 * for remote access through VFS ProActiveProvider; path given for local
+	 * access is resolved in context of caller’s node local ﬁle system. In case
+	 * of remote access deﬁned through URL, local access deﬁnition is optional,
+	 * it may be used internally only for performance reasons. Returned URI of
+	 * created input data space can be safely passed to another Active Objects.
+	 * Given input name (which might be constant in code) can also be safely
+	 * used by other Active Objects after this method returns.
+	 * 
+	 * @param name
+	 * @param path
+	 * @param url
+	 * @return
+	 */
+	public static String addInput(String name, String path, String url) {
 		return null;
 	}
 
-	public static String addOutput(String name, String path, URL url) {
+	/**
+	 * Analogous method for output.
+	 * 
+	 * @param name
+	 * @param path
+	 * @param url
+	 * @return
+	 */
+	public static String addOutput(String name, String path, String url) {
 		return null;
 	}
 
