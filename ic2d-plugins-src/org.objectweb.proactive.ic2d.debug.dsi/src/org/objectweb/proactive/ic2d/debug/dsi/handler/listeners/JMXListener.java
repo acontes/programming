961e4cbb27ccb266fbc699993e68dccb35b40758
mmenant <mmenant@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-17 13:19:11 +0000

    Add View for graphe rendering for communiction in services flow in IC2D Plugin for DSI
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_DSI_Monitoring@12031 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- ic2d-plugins-src/org.objectweb.proactive.ic2d.debug.dsi/src/org/objectweb/proactive/ic2d/debug/dsi/handler/listeners/JMXListener.java
+++ ic2d-plugins-src/org.objectweb.proactive.ic2d.debug.dsi/src/org/objectweb/proactive/ic2d/debug/dsi/handler/listeners/JMXListener.java
@@ -10,6 +10,7 @@ import org.objectweb.proactive.core.jmx.notification.NotificationType;
 import org.objectweb.proactive.core.jmx.notification.RequestNotificationData;
 import org.objectweb.proactive.ic2d.console.Console;
 import org.objectweb.proactive.ic2d.debug.dsi.Activator;
+import org.objectweb.proactive.ic2d.debug.dsi.handler.DSIHandler;
 import org.objectweb.proactive.ic2d.jmxmonitoring.extpoint.IJMXListenerExtPoint;
 import org.objectweb.proactive.ic2d.jmxmonitoring.util.IC2DThreadPool;
 
@@ -18,14 +19,12 @@ public class JMXListener implements Serializable, IJMXListenerExtPoint {
 
     private static final long serialVersionUID = -4438864290999520319L;
 
-    //private DSIHandler handler;
+    private DSIHandler handler;
 
     public JMXListener() {
-        Console.getInstance(Activator.CONSOLE_NAME).log("JMX DSI Listener ON");
-        //this.handler = DSIHandler.getInstance();
+        this.handler = DSIHandler.getInstance();
     }
 
-    @SuppressWarnings("unchecked")
     public void handleNotification(Notification notification, Object handback){
         // Get the type of the notification
         String type = notification.getType();
@@ -53,19 +52,19 @@ public class JMXListener implements Serializable, IJMXListenerExtPoint {
                             
                         RequestNotificationData data = (RequestNotificationData) notification.getUserData();
                         String method = data.getMethodName(); 
-                        if( method != null && !method.equals("handleNotification") ){
-                            setRequest(notification.getType(), (RequestNotificationData) notification.getUserData(), notification.getSequenceNumber());
+                        if( method != null ){
+                            setRequest(notification.getType(), (RequestNotificationData) notification.getUserData());
                         }
                 }
             }
         }
         
-        private void setRequest(String type, RequestNotificationData request, long seqNumber) {
+        private void setRequest(String type, RequestNotificationData request) {
             UniqueID idSrc = request.getSource();
             UniqueID idDst = request.getDestination();
             String method = request.getMethodName();
             long seq = request.getSequenceNumber();
-            //handler.addService(idSrc, idDst, type, method, seq, request.getTags());
+            handler.addService(idSrc, idDst, type, method, seq, request.getTags());
             Console.getInstance(Activator.CONSOLE_NAME).log("Service added :" + method);
         }
         
---
+++ ic2d-plugins-src/org.objectweb.proactive.ic2d.debug.dsi/src/org/objectweb/proactive/ic2d/debug/dsi/views/CommunicationGraphView.java
@@ -0,0 +1,140 @@
+package org.objectweb.proactive.ic2d.debug.dsi.views;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.ManagedForm;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.zest.core.viewers.AbstractZoomableViewer;
+import org.eclipse.zest.core.viewers.GraphViewer;
+import org.eclipse.zest.core.viewers.IZoomableWorkbenchPart;
+import org.eclipse.zest.core.widgets.Graph;
+import org.eclipse.zest.core.widgets.GraphConnection;
+import org.eclipse.zest.core.widgets.GraphNode;
+import org.eclipse.zest.core.widgets.ZestStyles;
+import org.eclipse.zest.layouts.LayoutAlgorithm;
+import org.eclipse.zest.layouts.LayoutStyles;
+import org.eclipse.zest.layouts.algorithms.CompositeLayoutAlgorithm;
+import org.eclipse.zest.layouts.algorithms.HorizontalShift;
+import org.eclipse.zest.layouts.algorithms.RadialLayoutAlgorithm;
+import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
+import org.objectweb.proactive.core.UniqueID;
+import org.objectweb.proactive.ic2d.console.Console;
+import org.objectweb.proactive.ic2d.debug.dsi.Activator;
+import org.objectweb.proactive.ic2d.debug.dsi.handler.DSIHandler;
+import org.objectweb.proactive.ic2d.debug.dsi.handler.RequestDSI;
+import org.objectweb.proactive.ic2d.jmxmonitoring.data.NamesFactory;
+
+public class CommunicationGraphView extends ViewPart implements IZoomableWorkbenchPart {
+
+    /* The view ID */
+    public static final String ID = "org.objectweb.proactive.ic2d.debug.dsi.views.CommunicationGraphView";
+
+    private Map<String, GraphNode> nodes = new HashMap<String, GraphNode>();
+    private Graph g = null;
+
+    private FormToolkit toolKit = null;
+    private ScrolledForm form = null;
+    private ManagedForm managedForm = null;
+    private GraphViewer viewer;
+    private VisualizationForm visualizationForm;
+
+    private Color colors[] = { new Color(Display.getDefault(), 216, 228, 248),
+            new Color(Display.getDefault(), 1, 70, 122), new Color(Display.getDefault(), 139, 150, 171),
+            new Color(Display.getDefault(), 213, 243, 255), new Color(Display.getDefault(), 255, 255, 206),
+            new Color(Display.getDefault(), 128, 128, 128), new Color(Display.getDefault(), 220, 220, 220),
+            new Color(Display.getDefault(), 0, 0, 0), new Color(Display.getDefault(), 255, 0, 0),
+            new Color(Display.getDefault(), 127, 0, 0), new Color(Display.getDefault(), 255, 196, 0),
+            new Color(Display.getDefault(), 255, 255, 0), new Color(Display.getDefault(), 0, 255, 0),
+            new Color(Display.getDefault(), 0, 127, 0), new Color(Display.getDefault(), 96, 255, 96),
+            new Color(Display.getDefault(), 0, 255, 255), new Color(Display.getDefault(), 0, 0, 255) };
+
+    @Override
+    public void createPartControl(Composite parent) {
+        //TODO : Trouver comment afficher le graphe par rapport a la vue du PDE
+        //        g = new Graph(Display.getCurrent().getActiveShell(), SWT.NONE);
+
+
+        toolKit = new FormToolkit(parent.getDisplay());
+        visualizationForm = new VisualizationForm(parent, toolKit, this);
+        viewer = visualizationForm.getGraphViewer();
+        form = visualizationForm.getForm();
+        managedForm = visualizationForm.getManagedForm();
+
+        g = viewer.getGraphControl();
+
+
+        g.setLayoutAlgorithm(new CompositeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING, new LayoutAlgorithm[] { new RadialLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), new HorizontalShift(LayoutStyles.NO_LAYOUT_NODE_RESIZING) }),true);
+
+
+
+        //        final Graph g = viewer.getGraphControl();
+        //
+        //        GraphNode a = new GraphNode(g, ZestStyles.CONNECTIONS_DIRECTED, "Root");
+        //        GraphConnection connection = new GraphConnection(g, SWT.NONE, a, a);
+        //        connection.setText("A to A");
+        //        a.setLocation(100, 100);
+
+        //      g.setLayoutAlgorithm(new TreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
+
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void refresh(){
+        Random rand = new Random();
+        Map<UniqueID, Set<RequestDSI>> dsi = DSIHandler.getInstance().getDSI();
+        
+        for (Map.Entry<UniqueID, Set<RequestDSI>> e : dsi.entrySet()) {
+
+            for (RequestDSI r : e.getValue()) {
+                if (nodes.get(r.getSender().shortString() + e.getKey().shortString()) == null) {
+                    createNode(r.getSender(), e.getKey());
+                }
+                if (nodes.get(r.getDestinator().shortString() + e.getKey().shortString()) == null) {
+                    createNode(r.getDestinator(), e.getKey());
+                }
+            }
+
+            Color color = colors[rand.nextInt(colors.length)];
+            for (RequestDSI r : e.getValue()) {
+                GraphConnection connection = new GraphConnection(g, ZestStyles.CONNECTIONS_DIRECTED, nodes
+                        .get(r.getSender().shortString() + e.getKey().shortString()), nodes.get(r
+                                .getDestinator().shortString() +
+                                e.getKey().shortString()));
+                connection.setLineColor(color);
+                connection.setLineWidth(2);
+                connection.setData(Boolean.FALSE);
+                connection.setText(e.getKey().shortString());
+            }
+        }
+        g.setLayoutAlgorithm(new TreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);        
+        Console.getInstance(Activator.CONSOLE_NAME).log("Rendering graph...done");
+        
+    }
+
+    private void createNode(UniqueID id, UniqueID dsi) {
+        String name = NamesFactory.getInstance().associateName(id, "AO");
+        GraphNode n = new GraphNode(g, SWT.NONE, name + " - " + id.shortString());
+        nodes.put(id.shortString() + dsi.shortString(), n);
+    }
+
+    public AbstractZoomableViewer getZoomableViewer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+}
