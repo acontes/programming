7da5e8b8cc5fcb83ea7cc612ef5ed5b08f38981d
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-17 14:36:34 +0000

    new exceptionWrongApplicationIdException introduced
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12051 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/exceptions/WrongApplicationIdException.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.objectweb.proactive.extra.dataspaces.exceptions;
+
+/**
+ * 
+ *
+ */
+public class WrongApplicationIdException extends ConfigurationException {
+
+	/**
+	 * 
+	 */
+	public WrongApplicationIdException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public WrongApplicationIdException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public WrongApplicationIdException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public WrongApplicationIdException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
