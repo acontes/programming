da256025115e90e52de2852c4594c106cdcea4f3
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-20 13:26:17 +0000

    test for CachingSpacesDirectory
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12077 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/dataspaces/CachingSpacesDirectoryTest.java
@@ -0,0 +1,13 @@
+package functionalTests.dataspaces;
+
+import org.objectweb.proactive.extra.dataspaces.CachingSpacesDirectory;
+import org.objectweb.proactive.extra.dataspaces.SpacesDirectory;
+import org.objectweb.proactive.extra.dataspaces.SpacesDirectoryImpl;
+
+public class CachingSpacesDirectoryTest extends SpacesDirectoryAbstractTest {
+
+	@Override
+	protected SpacesDirectory getSource() throws Exception {
+		return new CachingSpacesDirectory(new SpacesDirectoryImpl());
+	}
+}
