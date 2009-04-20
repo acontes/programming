385e51f6dcbb69728f8795f530d6c087b3c5d318
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-20 13:59:38 +0000

    SpaceType docs
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12089 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/SpaceType.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/SpaceType.java
@@ -6,15 +6,36 @@ package org.objectweb.proactive.extra.dataspaces;
 import java.io.Serializable;
 
 /**
- * Data space type with defined succ method.
+ * Data space type, defining its semantics.
+ * 
+ * This enum has defined order and it is possible to access it successor through
+ * {@link #succ()} method.
  */
 public enum SpaceType implements Serializable, Comparable<SpaceType> {
-	INPUT, OUTPUT, SCRATCH;
+	/**
+	 * Application input.
+	 */
+	INPUT,
+	/**
+	 * Application output.
+	 */
+	OUTPUT,
+	/**
+	 * Temporary storage, associated with node, but used by/partially associated
+	 * to an application.
+	 */
+	SCRATCH;
 
+	/**
+	 * @return directory name in URI for that data space type
+	 */
 	public String getDirectoryName() {
 		return name().toLowerCase();
 	}
 
+	/**
+	 * @return next enum in order, or null this is the last one
+	 */
 	public SpaceType succ() {
 		final SpaceType[] v = SpaceType.values();
 		final int n = this.ordinal() + 1;
