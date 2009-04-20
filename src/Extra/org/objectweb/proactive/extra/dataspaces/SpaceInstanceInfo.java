ed8da5941ead58c61f6d40121775d58305aacec6
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-20 13:57:49 +0000

    Complete SpaceInstanceInfo documentation and implementation
    hashCode() and equals() added
    documentation improved
    some sanity checks improved
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12082 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/SpaceInstanceInfo.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/SpaceInstanceInfo.java
@@ -7,10 +7,13 @@ import java.io.Serializable;
 
 import javax.naming.ConfigurationException;
 
-// FIXME: comparable or hashCode needed
 /**
- * Stores mapping from mounting point URI to access description (like URL, path
- * and hostname) along with mounting point information.
+ * Stores complete description of data space instance, i.e. mounting point URI
+ * with information contained there (type, application id...), and space access
+ * description (remote access URL, optional local path and hostname).
+ * 
+ * Instances of this class are immutable, therefore thread-safe.
+ * <code>hashCode</code> and <code>equals</code> methods are defined.
  */
 public final class SpaceInstanceInfo implements Serializable {
 
@@ -39,22 +42,18 @@ public final class SpaceInstanceInfo implements Serializable {
 	 * @param config
 	 *            scratch data space configuration
 	 * @throws ConfigurationException
-	 *             when cannot build complete mounting point SpaceURI
+	 *             when provided information is not enough to build a complete
+	 *             space definition (no hostname for path etc.)
 	 */
 	public SpaceInstanceInfo(long appid, String runtimeId, String nodeId, SpaceConfiguration config)
 			throws ConfigurationException {
-
-		if (runtimeId == null || nodeId == null || config == null)
-			throw new IllegalArgumentException("Configuration can not be null");
-
 		if (config.getType() != SpaceType.SCRATCH)
-			throw new IllegalArgumentException("This constructor must be used for scratch data space.");
+			throw new ConfigurationException("This constructor must be used for scratch data space.");
 
 		this.mountingPoint = DataSpacesURI.createScratchSpaceURI(appid, runtimeId, nodeId);
 		this.url = config.getUrl();
 		this.hostname = config.getHostname();
-		this.path = config.path;
-
+		this.path = config.getPath();
 		check();
 	}
 
@@ -62,73 +61,140 @@ public final class SpaceInstanceInfo implements Serializable {
 	 * Creates SpaceInstanceInfo for input/output data space.
 	 * 
 	 * @param appid
-	 *            application id
+	 *            application identifier
 	 * @param config
 	 *            input or output data space configuration
 	 * @throws ConfigurationException
-	 *             when cannot build complete mounting point SpaceURI
+	 *             when provided information is not enough to build a complete
+	 *             space definition (no hostname for path etc.)
 	 */
 	public SpaceInstanceInfo(long appid, SpaceConfiguration config) throws ConfigurationException {
-
-		if (config == null)
-			throw new IllegalArgumentException("Space configuration is null");
-
 		if (config.getType() == SpaceType.SCRATCH)
-			throw new IllegalArgumentException("This constructor cannot be used for scratch data space.");
+			throw new ConfigurationException("This constructor cannot be used for scratch data space.");
 
 		this.mountingPoint = DataSpacesURI.createInOutSpaceURI(appid, config.getType(), config.getName());
 		this.url = config.getUrl();
 		this.hostname = config.getHostname();
-		this.path = config.path;
+		this.path = config.getPath();
 		check();
 	}
 
+	/**
+	 * Remote access URL. Always defined.
+	 * 
+	 * @return remote access URL to this data space
+	 */
 	public String getUrl() {
 		return url;
 	}
 
+	/**
+	 * Returns local access path, that can be used for host with hostname as
+	 * returned by {@link #getHostname()}.
+	 * 
+	 * Local access path may not be defined.
+	 * 
+	 * @return local access path; <code>null</code> if local access is undefined
+	 */
 	public String getPath() {
 		return path;
 	}
 
+	/**
+	 * Returns hostname where local access path may be used. This hostname
+	 * should be comparable to {@link Utils#getHostnameForThis()}.
+	 * 
+	 * @return hostname where local access path may be used; <code>null</code>
+	 *         if local access is undefined
+	 */
 	public String getHostname() {
 		return hostname;
 	}
 
+	/**
+	 * Returns mounting point URI of this data space.
+	 * 
+	 * @return mounting point URI
+	 */
 	public DataSpacesURI getMountingPoint() {
 		return mountingPoint;
 	}
 
 	/**
-	 * Returns the name of a mounting point.
+	 * Returns the name of a space, if such makes sense for that type of data
+	 * space.
 	 * 
-	 * @return
+	 * @return name of a space; may be <code>null</code> for scratch data space
 	 */
 	public String getName() {
 		return mountingPoint.getName();
 	}
 
 	/**
-	 * Returns data space type of a mounting point.
-	 * 
-	 * @return
+	 * @return data space type
 	 */
 	public SpaceType getType() {
 		return mountingPoint.getSpaceType();
 	}
 
 	/**
-	 * Returns application id of a mounting point.
-	 * 
-	 * @return
+	 * @return application id of data space
 	 */
 	public long getAppId() {
 		return mountingPoint.getAppId();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mountingPoint.hashCode();
+		result = prime * result + url.hashCode();
+		result = prime * result + ((hostname == null) ? 0 : hostname.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof SpaceInstanceInfo))
+			return false;
+
+		final SpaceInstanceInfo other = (SpaceInstanceInfo) obj;
+		if (mountingPoint == null) {
+			if (other.mountingPoint != null)
+				return false;
+		} else if (!mountingPoint.equals(other.mountingPoint))
+			return false;
+
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+
+		if (hostname == null) {
+			if (other.hostname != null)
+				return false;
+		} else if (!hostname.equals(other.hostname))
+			return false;
+
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		return true;
+	}
+
 	private void check() throws ConfigurationException {
 		if (!mountingPoint.isComplete())
 			throw new ConfigurationException("Constructed mounting point URI must be complete");
+		if (url == null)
+			throw new ConfigurationException("No remote access URL provided");
+		if (path != null && hostname == null)
+			throw new ConfigurationException("Local path provided without hostname specified");
 	}
-
 }
