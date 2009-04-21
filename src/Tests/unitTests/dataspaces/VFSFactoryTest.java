fc5c7f2a4425db64b32555518f3360d33d83324f
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-21 13:28:20 +0000

    VFSFactory smoke tests
    This time placed in an appropriate package (unitTests?)
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12129 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/unitTests/dataspaces/VFSFactoryTest.java
@@ -0,0 +1,76 @@
+package unitTests.dataspaces;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.impl.DefaultFileSystemManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.proactive.extra.dataspaces.DataSpacesURI;
+import org.objectweb.proactive.extra.dataspaces.VFSFactory;
+
+public class VFSFactoryTest {
+
+	private DefaultFileSystemManager manager;
+
+	private File testFile;
+
+	@Before
+	public void setUp() throws Exception {
+		manager = VFSFactory.createDefaultFileSystemManager();
+		testFile = new File(System.getProperty("java.io.tmpdir"), "ProActive-VFSFactoryTest");
+		final OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(testFile));
+		osw.write("test");
+		osw.close();
+	}
+
+	@After
+	public void tearDown() {
+		manager.close();
+		testFile.delete();
+	}
+
+	@Test
+	public void testCreateVirtualFileSystem() throws FileSystemException {
+		manager.createVirtualFileSystem(DataSpacesURI.VFS_SCHEME);
+	}
+
+	@Test
+	public void testReplicator() throws Exception {
+		assertNotNull(manager.getReplicator());
+	}
+
+	@Test
+	public void testTempStorage() throws Exception {
+		assertNotNull(manager.getTemporaryFileStore());
+	}
+
+	@Test
+	public void testLocalFileProvider() throws Exception {
+		FileObject fo = null;
+		try {
+			// TODO does it work on windows machines?
+			final String fileUrl = "file:///" + testFile.getCanonicalPath().replaceFirst("^/", "");
+			fo = manager.resolveFile(fileUrl);
+			assertTrue(fo.exists());
+
+			final InputStream ios = fo.getContent().getInputStream();
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(ios));
+			assertEquals("test", reader.readLine());
+		} finally {
+			if (fo != null)
+				fo.close();
+		}
+	}
+}
