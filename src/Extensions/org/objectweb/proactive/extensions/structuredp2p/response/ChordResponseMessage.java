a2e9ae317cad10f98d0e2e89cb6157f016a1a032
atrovato <atrovato@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-21 08:24:38 +0000

    Begin of the ResponseMessage package. Not fixed.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@12101 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/response/ChordResponseMessage.java
@@ -0,0 +1,15 @@
+package org.objectweb.proactive.extensions.structuredp2p.response;
+
+/**
+ * A chord response message gives a CHORD peer for routing.
+ * 
+ * @author Kilanga Fanny
+ * @author Trovato Alexandre
+ * @author Pellegrino Laurent
+ * 
+ * @version 0.1
+ */
+@SuppressWarnings("serial")
+public class ChordResponseMessage extends ResponseMessage {
+
+}
