1644c5b33499d5ec4cf06de95d7bd0a8dcf45c4a
atrovato <atrovato@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-21 09:12:32 +0000

    DataStorage more complete but not fixed.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@12108 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/structuredp2p/data/DataStorage.java
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/data/DataStorage.java
@@ -1,5 +1,9 @@
 package org.objectweb.proactive.extensions.structuredp2p.data;
 
+import java.io.Serializable;
+import java.util.HashMap;
+
+
 /**
  * @author Kilanga Fanny
  * @author Trovato Alexandre
@@ -7,6 +11,64 @@ package org.objectweb.proactive.extensions.structuredp2p.data;
  * 
  * @version 0.1
  */
-public class DataStorage {
+@SuppressWarnings("serial")
+public class DataStorage implements Serializable {
+    private HashMap<String, Object> data;
 
+    public DataStorage() {
+        this.data = new HashMap<String, Object>();
+    }
+
+    /**
+     * Return all the data in the storage.
+     * 
+     * @return data
+     */
+    public HashMap<String, Object> getData() {
+        return this.data;
+    }
+
+    /**
+     * Return the data for a key.
+     * 
+     * @param key
+     *            id of the data
+     * @return the data
+     */
+    public Object getData(String key) {
+        return this.data.get(key);
+    }
+
+    /**
+     * Insert all the data from an other DataStorage.
+     * 
+     * @param other
+     *            the other DataStorage
+     */
+    public void addData(DataStorage other) {
+        this.data.putAll(other.getData());
+    }
+
+    /**
+     * Insert a new object in the storage.
+     * 
+     * @param key
+     *            id of the object
+     * @param value
+     *            the datas
+     */
+    // FIXME la key est un hash ou non ?
+    public void addData(String key, Object value) {
+        this.data.put(key, value);
+    }
+
+    /**
+     * Remove the object with the current key.
+     * 
+     * @param key
+     *            the current key.
+     */
+    public void removeData(String key) {
+        this.data.remove(key);
+    }
 }
