776c940ebbc19fc6ccfbcb48bd15a0b5be425996
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-21 13:27:55 +0000

    VFSFactory real implementation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12128 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/dataspaces/VFSFactory.java
+++ src/Extra/org/objectweb/proactive/extra/dataspaces/VFSFactory.java
@@ -1,16 +1,60 @@
 package org.objectweb.proactive.extra.dataspaces;
 
 import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.impl.DefaultFileReplicator;
 import org.apache.commons.vfs.impl.DefaultFileSystemManager;
+import org.apache.commons.vfs.impl.PrivilegedFileReplicator;
+import org.apache.commons.vfs.provider.ftp.FtpFileProvider;
+import org.apache.commons.vfs.provider.http.HttpFileProvider;
+import org.apache.commons.vfs.provider.https.HttpsFileProvider;
 import org.apache.commons.vfs.provider.local.DefaultLocalFileProvider;
+import org.apache.commons.vfs.provider.sftp.SftpFileProvider;
+import org.apache.commons.vfs.provider.url.UrlFileProvider;
 
-// TODO
+// TODO set logger?
+/**
+ * Factory class for creating configured VFS DefaultFileSystemManager instances.
+ * <p>
+ * Instances of managers created through this factory are guaranteed to have
+ * providers for following protocols:
+ * <ul>
+ * <li>local files, scheme <code>file:</code></li>
+ * <li>HTTP, scheme <code>http:</code></li>
+ * <li>HTTPS, scheme <code>https:</code></li>
+ * <li>FTP, scheme <code>ftp:</code></li>
+ * <li>SFTP, scheme <code>sftp:</code></li>
+ * <li>default URL provider handled by Java URL class</code>
+ * </ul>
+ * 
+ * Configured replicator and temporary storage are also guaranteed.
+ */
 public class VFSFactory {
-    public static DefaultFileSystemManager createDefaultFileSystemManager() throws FileSystemException {
-        final DefaultFileSystemManager manager = new DefaultFileSystemManager();
-        manager.addProvider("file", new DefaultLocalFileProvider());
-        // TODO
-        manager.init();
-        return manager;
-    }
+	/**
+	 * Creates new DefaultSystemManager instance with configured providers,
+	 * replicator and temporary storage - as described in class description.
+	 * <p>
+	 * Returned instance is initialized and it is a caller responsibility to
+	 * close it to release resources.
+	 * 
+	 * @return configured and initialized DefaultFileSystemManager instance
+	 * @throws FileSystemException
+	 *             when initialization or configuration process fails
+	 */
+	public static DefaultFileSystemManager createDefaultFileSystemManager() throws FileSystemException {
+		final DefaultFileSystemManager manager = new DefaultFileSystemManager();
+
+		final DefaultFileReplicator replicator = new DefaultFileReplicator();
+		manager.setReplicator(new PrivilegedFileReplicator(replicator));
+		manager.setTemporaryFileStore(replicator);
+
+		manager.addProvider("file", new DefaultLocalFileProvider());
+		manager.addProvider("http", new HttpFileProvider());
+		manager.addProvider("https", new HttpsFileProvider());
+		manager.addProvider("ftp", new FtpFileProvider());
+		manager.addProvider("sftp", new SftpFileProvider());
+		manager.setDefaultProvider(new UrlFileProvider());
+
+		manager.init();
+		return manager;
+	}
 }
