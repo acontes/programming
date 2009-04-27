5a79df64b58365683bcd7cdb1715887a7b7e98fd
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-27 13:02:07 +0000

    appendSubDirs() tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12367 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/unitTests/dataspaces/UtilsTest.java
+++ src/Tests/unitTests/dataspaces/UtilsTest.java
@@ -1,6 +1,7 @@
 package unitTests.dataspaces;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.objectweb.proactive.extra.dataspaces.Utils;
@@ -24,5 +25,40 @@ public class UtilsTest {
     @Test
     public void testGetLocalAccessURLNoLocalPath() throws ConfigurationException {
         assertEquals("http://remote/", Utils.getLocalAccessURL("http://remote/", null, null));
+    }
+
+    @Test
+    public void testAppendSubDirsNoBaseLocation() throws Exception {
+        assertNull(Utils.appendSubDirs(null, "abc"));
+    }
+
+    @Test
+    public void testAppendSubDirsNoBaseNoSubDir() throws Exception {
+        assertNull(Utils.appendSubDirs(null));
+    }
+
+    @Test
+    public void testAppendSubDirsBaseNoSlashNoSubDir() throws Exception {
+        assertEquals("/abc", Utils.appendSubDirs("/abc"));
+    }
+
+    @Test
+    public void testAppendSubDirsBaseSlash1SubDir() throws Exception {
+        assertEquals("/abc/1", Utils.appendSubDirs("/abc/", "1"));
+    }
+
+    @Test
+    public void testAppendSubDirsBaseNoSlash1SubDir() throws Exception {
+        assertEquals("/abc/1", Utils.appendSubDirs("/abc", "1"));
+    }
+
+    @Test
+    public void testAppendSubDirsBaseAsURL1SubDir() throws Exception {
+        assertEquals("http://test.com/1", Utils.appendSubDirs("http://test.com/", "1"));
+    }
+
+    @Test
+    public void testAppendSubDirsBaseNoSlash2SubDir() throws Exception {
+        assertEquals("/abc/1/2", Utils.appendSubDirs("/abc", "1", "2"));
     }
 }
