3e5f1261f10c20f66061e4fd276f6d888e5e4f1a
plaurent <plaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-27 09:41:01 +0000

    CanJoinResponseMessage
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@12349 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/message/response/CanJoinResponseMessage.java
@@ -0,0 +1,22 @@
+package org.objectweb.proactive.extensions.structuredp2p.message.response;
+
+/**
+ * A chord response message gives a CAN peer for routing.
+ * 
+ * @author Kilanga Fanny
+ * @author Pellegrino Laurent
+ * @author Trovato Alexandre
+ * 
+ * @version 0.1
+ */
+@SuppressWarnings("serial")
+public class CanJoinResponseMessage extends ResponseMessage {
+
+    /**
+     * 
+     * @param peer
+     */
+    public CanJoinResponseMessage() {
+        super();
+    }
+}
