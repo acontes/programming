475f421900b08cbfb1fd1a86b506bfeff14e22ae
plaurent <plaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-05-06 09:12:08 +0000

    Tracker implementation.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@12498 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/core/Tracker.java
@@ -0,0 +1,82 @@
+package org.objectweb.proactive.extensions.structuredp2p.core;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+/**
+ * @author Kilanga Fanny
+ * @author Pellegrino Laurent
+ * @author Trovato Alexandre
+ * 
+ * @version 0.1
+ */
+@SuppressWarnings("serial")
+public class Tracker implements Serializable {
+
+    /**
+     * The remote peers list that the tracker maintains.
+     */
+    private List<Peer> remotePeers = new ArrayList<Peer>();
+
+    /**
+     * The type of peers that can use this tracker in order to join the network.
+     */
+    private OverlayType type;
+
+    /**
+     * Constructor.
+     */
+    public Tracker() {
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param type
+     *            the type of tracker (ie. the kind of peers that can be add on the network).
+     */
+    public Tracker(OverlayType type) {
+        this.type = type;
+    }
+
+    /**
+     * Add on the network that the tracker know the given peer.
+     * 
+     * @param remotePeer
+     *            the peer to add on the network.
+     */
+    public void addOnNetwork(Peer remotePeer) {
+        if (remotePeer.getType() != this.type) {
+            throw new IllegalStateException("You can only add network of type " + this.type +
+                " by this tracker");
+        }
+
+        Peer peerToJoin = this.getRandomPeer();
+
+        try {
+            if (peerToJoin.join(remotePeer)) {
+                Random rand = new Random();
+                if (rand.nextInt(2) == 0) {
+                    this.remotePeers.add(remotePeer);
+                }
+            }
+        } catch (Exception e) {
+            // The remote peer we contact in order to join is died, so we retry with an anoter
+            this.remotePeers.remove(peerToJoin);
+            this.addOnNetwork(remotePeer);
+        }
+    }
+
+    /**
+     * Returns a random peer from the local list.
+     * 
+     * @return a random peer from the local list.
+     */
+    public Peer getRandomPeer() {
+        Random rand = new Random();
+        return this.remotePeers.get(rand.nextInt(this.remotePeers.size()));
+    }
+}
