ebedb944ab230974a392a1dffbeaaf5aafd69d79
ffonteno <ffonteno@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-05-07 14:51:34 +0000

    Changed two small modifications due to the merge. Compilation failed in the previous commit.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ffonteno_axis2_exportation@12545 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- examples/helloWebServices/helloWS.sh
+++ examples/helloWebServices/helloWS.sh
@@ -17,7 +17,8 @@ fi
 echo
 echo
 
-$JAVACMD $HTTP_OPT org.objectweb.proactive.examples.webservices.helloWorld.HelloWorld "$@"
+#$JAVACMD $HTTP_OPT org.objectweb.proactive.examples.webservices.helloWorld.HelloWorld "$@"
+$JAVACMD -Dproactive.http.port=8080 org.objectweb.proactive.examples.webservices.helloWorld.HelloWorld "$@"
 
 echo
 echo ------------------------------------------------------------
