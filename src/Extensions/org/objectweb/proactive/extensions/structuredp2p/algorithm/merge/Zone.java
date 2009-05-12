844eff0e0ebad236c19d8ced106a4e3e907a649d
atrovato <atrovato@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-05-12 08:27:09 +0000

    Zone for algorithm tests.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/StructuredP2P@12587 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/structuredp2p/algorithm/merge/Zone.java
+++ src/Extensions/org/objectweb/proactive/extensions/structuredp2p/algorithm/merge/Zone.java
@@ -5,27 +5,104 @@ import java.util.ArrayList;
 
 public class Zone {
 
-    public int xMin;
-    public int xMax;
-    public int yMin;
-    public int yMax;
+    public int xMin = 0;
+    public int xMax = 500;
+    public int yMin = 0;
+    public int yMax = 500;
 
-    private ArrayList<Zone>[][] neighbors;
-    private ArrayList<int[]> splitHistory;
+    public ArrayList<Zone>[][] neighbors;
+    public ArrayList<int[]> splitHistory;
 
-    public Zone(Zone zone) {
+    public Zone() {
         this.neighbors = new ArrayList[2][2];
         this.splitHistory = new ArrayList<int[]>();
     }
 
     public boolean join(Zone zone) {
-        // TODO
-        return false;
+        int dimension = 0;
+        int direction = 0;
+        int directionInv = (direction + 1) % 2;
+
+        // Get the next dimension to split onto
+        if (this.splitHistory != null && this.splitHistory.size() > 0) {
+            dimension = (this.splitHistory.get(this.splitHistory.size() - 1)[0] + 1) % 2;
+        }
+
+        // Nouvelle zone
+        // Modifications en x
+        if (dimension == 0) {
+            zone.xMin = (this.xMin + this.xMax) / 2;
+            this.xMax = (this.xMin + this.xMax) / 2;
+        } else if (dimension == 1) {
+            zone.yMin = (this.yMin + this.yMax) / 2;
+            this.yMax = (this.yMin + this.yMax) / 2;
+        } else {
+            return false;
+        }
+
+        zone.splitHistory = this.splitHistory;
+        zone.splitHistory.add(new int[] { dimension, directionInv });
+        zone.neighbors = this.neighbors;
+        zone.neighbors[dimension][directionInv].clear();
+        zone.neighbors[dimension][directionInv].add(this);
+
+        this.splitHistory.add(new int[] { dimension, directionInv });
+        this.neighbors[dimension][direction].clear();
+        this.neighbors[dimension][direction].add(zone);
+
+        return true;
+    }
+
+    public Zone leave() {
+        if (this.splitHistory.size() > 0) {
+            int[] lastOP = this.splitHistory.get(this.splitHistory.size() - 1);
+            int dimension = lastOP[0];
+            int direction = lastOP[1];
+
+            int nbNeigbors = this.neighbors[dimension][direction].size();
+
+            // If there is just one neighbor, easy
+            if (nbNeigbors == 1) {
+                this.neighbors[dimension][direction].get(0).merge(this);
+            }
+            // Else, do the same thing recursively (it's a little heavy with data transfer)
+            else if (nbNeigbors > 1) {
+                Zone zone = this.neighbors[dimension][direction].get(0).leave();
+                zone.xMin = this.xMin;
+                zone.yMin = this.yMin;
+                zone.xMax = this.xMax;
+                zone.yMax = this.yMax;
+                zone.splitHistory = this.splitHistory;
+                zone.neighbors = this.neighbors;
+            }
+
+        }
+
+        for (ArrayList<Zone>[] firstN : this.neighbors) {
+            for (ArrayList<Zone> secondN : firstN) {
+                for (Zone zone : secondN) {
+                    zone.removeNeighbor(this);
+                }
+            }
+        }
+
+        return this;
     }
 
     public boolean merge(Zone zone) {
-        // TODO
-        return false;
+        if (this.xMin > zone.xMin) {
+            this.xMin = zone.xMin;
+        } else if (this.xMin < zone.xMin) {
+            this.xMax = zone.xMax;
+        } else if (this.yMin > zone.yMin) {
+            this.yMin = zone.yMin;
+        } else if (this.yMin < zone.yMin) {
+            this.yMax = zone.yMax;
+        }
+
+        this.splitHistory.remove(this.splitHistory.size() - 1);
+
+        return true;
     }
 
     public boolean contains(int x, int y) {
