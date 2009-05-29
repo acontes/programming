3e2146e7151c0fcbe011107e8978f27717a05c69
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-05-29 18:12:38 +0000

    DataSpacesTest: extend tests for 2 nodes per JVM, use more than 1 scratch
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@12886 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/dataspaces/DataSpacesTest.java
+++ src/Tests/functionalTests/dataspaces/DataSpacesTest.java
@@ -34,22 +34,32 @@ import org.objectweb.proactive.extra.dataspaces.exceptions.SpaceNotFoundExceptio
 public class DataSpacesTest extends GCMFunctionalTestDataSpaces {
     private static final String ADDED_INPUT_NAME = "another_input";
     private static final String OUTPUT_FILE_NAME = "some_file.txt";
-    private static final String OUTPUT_FILE_CONTENT = "didum";
+    private static final String OUTPUT_FILE_CONTENT1 = "didum";
+    private static final String OUTPUT_FILE_CONTENT2 = "didum_toto";
     private Node node1;
     private Node node2;
+    private Node node3;
+    private Node node4;
     private TestActiveObject ao1;
     private TestActiveObject ao2;
+    private TestActiveObject ao3;
+    private TestActiveObject ao4;
 
     public DataSpacesTest() {
-        super(2, 1);
+        super(2, 2);
     }
 
     @Before
     public void createTestActiveObjects() throws ActiveObjectCreationException, NodeException {
         node1 = getANode();
         node2 = getANode();
+        node3 = getANode();
+        node4 = getANode();
+        // create AOs on hosts on the same and different runtimes
         ao1 = (TestActiveObject) PAActiveObject.newActive(TestActiveObject.class.getName(), null, node1);
         ao2 = (TestActiveObject) PAActiveObject.newActive(TestActiveObject.class.getName(), null, node2);
+        ao3 = (TestActiveObject) PAActiveObject.newActive(TestActiveObject.class.getName(), null, node3);
+        ao4 = (TestActiveObject) PAActiveObject.newActive(TestActiveObject.class.getName(), null, node4);
         // no need for @After, as whole GCMApp will be killed
     }
 
@@ -60,46 +70,66 @@ public class DataSpacesTest extends GCMFunctionalTestDataSpaces {
         // read inputs:
         assertEquals(INPUT_FILE_CONTENT, ao1.readDefaultInputFile(INPUT_FILE_NAME));
         assertEquals(INPUT_FILE_CONTENT, ao2.readDefaultInputFile(INPUT_FILE_NAME));
-        assertEquals(INPUT_FILE_CONTENT, ao1.readInputFile(INPUT_NAME, INPUT_FILE_NAME));
-        assertEquals(INPUT_FILE_CONTENT, ao2.readInputFile(INPUT_NAME, INPUT_FILE_NAME));
+        assertEquals(INPUT_FILE_CONTENT, ao3.readInputFile(INPUT_NAME, INPUT_FILE_NAME));
+        assertEquals(INPUT_FILE_CONTENT, ao4.readInputFile(INPUT_NAME, INPUT_FILE_NAME));
 
         // write to outputs:
-        final String defaultOutputFileUri = ao1.writeDefaultOutputFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT);
-        assertEquals(OUTPUT_FILE_CONTENT, ao2.readFile(defaultOutputFileUri));
-        assertEquals(OUTPUT_FILE_CONTENT, ao1.readFile(defaultOutputFileUri));
+        final String defaultOutputFileUri = ao1
+                .writeDefaultOutputFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT1);
+        assertEquals(OUTPUT_FILE_CONTENT1, ao1.readFile(defaultOutputFileUri));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao2.readFile(defaultOutputFileUri));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao3.readFile(defaultOutputFileUri));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao4.readFile(defaultOutputFileUri));
 
-        final String outputFileUri = ao1.writeOutputFile(OUTPUT_NAME, OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT);
-        assertEquals(OUTPUT_FILE_CONTENT, ao2.readFile(outputFileUri));
-        assertEquals(OUTPUT_FILE_CONTENT, ao1.readFile(outputFileUri));
+        final String outputFileUri = ao1.writeOutputFile(OUTPUT_NAME, OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT1);
+        assertEquals(OUTPUT_FILE_CONTENT1, ao1.readFile(outputFileUri));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao2.readFile(outputFileUri));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao3.readFile(outputFileUri));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao4.readFile(outputFileUri));
 
-        // write to scratch:
-        final String scratchFileUri = ao1.writeScratchFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT);
-        assertEquals(OUTPUT_FILE_CONTENT, ao2.readFile(scratchFileUri));
-        assertEquals(OUTPUT_FILE_CONTENT, ao1.readFile(scratchFileUri));
+        // write to scratches:
+        final String scratchFileUri1 = ao1.writeScratchFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT1);
+        final String scratchFileUri2 = ao2.writeScratchFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT2);
+        final String scratchFileUri3 = ao3.writeScratchFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT1);
+        final String scratchFileUri4 = ao4.writeScratchFile(OUTPUT_FILE_NAME, OUTPUT_FILE_CONTENT2);
+
+        assertEquals(OUTPUT_FILE_CONTENT1, ao1.readFile(scratchFileUri1));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao2.readFile(scratchFileUri1));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao3.readFile(scratchFileUri1));
+        assertEquals(OUTPUT_FILE_CONTENT1, ao4.readFile(scratchFileUri1));
+
+        assertEquals(OUTPUT_FILE_CONTENT2, ao1.readFile(scratchFileUri2));
+
+        assertEquals(OUTPUT_FILE_CONTENT1, ao1.readFile(scratchFileUri3));
+
+        assertEquals(OUTPUT_FILE_CONTENT2, ao3.readFile(scratchFileUri4));
 
         // read inputs names:
         final HashSet<String> expectedInputs = new HashSet<String>();
         expectedInputs.add(PADataSpaces.DEFAULT_IN_OUT_NAME);
         expectedInputs.add(INPUT_NAME);
         assertEquals(expectedInputs, ao1.getAllKnownInputsNames());
-        assertEquals(expectedInputs, ao2.getAllKnownInputsNames());
+        assertEquals(expectedInputs, ao3.getAllKnownInputsNames());
 
         // read outputs names:
         final HashSet<String> expectedOutputs = new HashSet<String>();
         expectedOutputs.add(PADataSpaces.DEFAULT_IN_OUT_NAME);
         expectedOutputs.add(OUTPUT_NAME);
         assertEquals(expectedInputs, ao1.getAllKnownOutputsNames());
-        assertEquals(expectedInputs, ao2.getAllKnownOutputsNames());
+        assertEquals(expectedInputs, ao3.getAllKnownOutputsNames());
 
         // blocking resolve input + add input:
         // (exceptions for readInputFileBlocking - to make it asynchronous)
         PAException.tryWithCatch(new Class[] { SpaceNotFoundException.class, NotConfiguredException.class,
                 IOException.class, ProActiveTimeoutException.class });
         try {
-            final StringWrapper contentWrapper = ao2.readInputFileBlocking(ADDED_INPUT_NAME, INPUT_FILE_NAME,
-                    30000);
-            ao1.addInputSpace(ADDED_INPUT_NAME, inputDir.getAbsolutePath());
-            assertEquals(INPUT_FILE_CONTENT, contentWrapper.stringValue());
+            final StringWrapper contentWrapper1 = ao1.readInputFileBlocking(ADDED_INPUT_NAME,
+                    INPUT_FILE_NAME, 30000);
+            final StringWrapper contentWrapper3 = ao3.readInputFileBlocking(ADDED_INPUT_NAME,
+                    INPUT_FILE_NAME, 30000);
+            ao4.addInputSpace(ADDED_INPUT_NAME, inputDir.getAbsolutePath());
+            assertEquals(INPUT_FILE_CONTENT, contentWrapper1.stringValue());
+            assertEquals(INPUT_FILE_CONTENT, contentWrapper3.stringValue());
             PAException.endTryWithCatch();
         } finally {
             PAException.removeTryWithCatch();
@@ -107,9 +137,10 @@ public class DataSpacesTest extends GCMFunctionalTestDataSpaces {
         expectedInputs.add(ADDED_INPUT_NAME);
         assertEquals(expectedInputs, ao1.getAllKnownInputsNames());
         assertEquals(expectedInputs, ao2.getAllKnownInputsNames());
+        assertEquals(expectedInputs, ao3.getAllKnownInputsNames());
+        assertEquals(expectedInputs, ao4.getAllKnownInputsNames());
 
         // TODO tests for deployer node?
-        // TODO tests for 2 nodes on the same JVM?
     }
 
     @ActiveObject
