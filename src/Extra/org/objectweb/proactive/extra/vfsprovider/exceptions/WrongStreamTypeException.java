71e5c12c96f13d8e67cc4a161de1b9b65175e19b
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-23 09:21:35 +0000

    WrongStreamTypeException serial id added
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13247 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/vfsprovider/exceptions/WrongStreamTypeException.java
+++ src/Extra/org/objectweb/proactive/extra/vfsprovider/exceptions/WrongStreamTypeException.java
@@ -2,4 +2,9 @@ package org.objectweb.proactive.extra.vfsprovider.exceptions;
 
 public class WrongStreamTypeException extends Exception {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -6361321020427385102L;
+
 }
