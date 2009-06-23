1b189566bcd3a63e89cc35a71353e6c0e67c4300
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-23 09:23:50 +0000

    javadoc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13251 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/vfsprovider/protocol/FileSystemServer.java
+++ src/Extra/org/objectweb/proactive/extra/vfsprovider/protocol/FileSystemServer.java
@@ -1,4 +1,9 @@
 package org.objectweb.proactive.extra.vfsprovider.protocol;
 
+/**
+ * Protocol definition made of two parts: {@link StreamOperations} and {@link FileOperations}.
+ * <p>
+ * Instances of this class are intended to work as remote objects and hence are thread-safe.
+ */
 public interface FileSystemServer extends StreamOperations, FileOperations {
 }
