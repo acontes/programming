e21db2152717157778f9ee5ad56c1ae6720f0cac
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-24 15:32:44 +0000

    javadoc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13279 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/vfsprovider/protocol/FileType.java
+++ src/Extra/org/objectweb/proactive/extra/vfsprovider/protocol/FileType.java
@@ -1,5 +1,16 @@
 package org.objectweb.proactive.extra.vfsprovider.protocol;
 
+/**
+ * Represents a type of a file.
+ */
 public enum FileType {
-    FILE, DIRECTORY
+    /**
+     * Indicates that file is an ordinary file.
+     */
+    FILE,
+
+    /**
+     * Indicates that file is a directory.
+     */
+    DIRECTORY
 }
