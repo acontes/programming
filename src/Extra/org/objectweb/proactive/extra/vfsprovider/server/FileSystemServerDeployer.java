fa811a8b0f0c1f23377cd4ad12a72341fc1b0c03
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-25 08:49:42 +0000

    file system server deployer added
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13291 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extra/org/objectweb/proactive/extra/vfsprovider/server/FileSystemServerDeployer.java
@@ -0,0 +1,75 @@
+package org.objectweb.proactive.extra.vfsprovider.server;
+
+import java.io.IOException;
+
+import org.objectweb.proactive.api.PARemoteObject;
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.remoteobject.RemoteObjectExposer;
+import org.objectweb.proactive.core.remoteobject.RemoteObjectHelper;
+import org.objectweb.proactive.extra.vfsprovider.protocol.FileSystemServer;
+
+
+/**
+ * Deploys {@link FileSystemServer} with instance of {@link FileSystemServerImpl} implementation on
+ * the local runtime.
+ */
+public class FileSystemServerDeployer {
+
+    private static final String FILE_SERVER_DEFAULT_NAME = "defaultFileSystemServer";
+
+    /** URL of the remote object */
+    final private String url;
+
+    final private FileSystemServer fileSystemServer;
+
+    RemoteObjectExposer<FileSystemServer> roe;
+
+    /**
+     * Deploys locally a FileSystemServer as a RemoteObject with a default name.
+     * 
+     * @param rootPath
+     * @throws IOException
+     */
+    public FileSystemServerDeployer(String rootPath) throws IOException {
+        this(FILE_SERVER_DEFAULT_NAME, rootPath);
+    }
+
+    /**
+     * Deploys locally a FileSystemServer as a RemoteObject with a given name.
+     * 
+     * @param name
+     *            of deployed RemoteObject
+     * @param rootPath
+     * @throws IOException
+     */
+    public FileSystemServerDeployer(String name, String rootPath) throws IOException {
+        fileSystemServer = new FileSystemServerImpl(rootPath);
+        roe = PARemoteObject.newRemoteObject(FileSystemServerImpl.class.getName(), this.fileSystemServer);
+        roe.createRemoteObject(name);
+        url = roe.getURL();
+    }
+
+    public FileSystemServer getLocalNamingService() {
+        return this.fileSystemServer;
+    }
+
+    public FileSystemServer getRemoteNamingService() throws ProActiveException {
+        return (FileSystemServer) RemoteObjectHelper.generatedObjectStub(this.roe.getRemoteObject());
+    }
+
+    public String getNamingServiceURL() {
+        return this.url;
+    }
+
+    /**
+     * Unexport the remote object.
+     * 
+     * @throws ProActiveException
+     */
+    public void terminate() throws ProActiveException {
+        if (roe != null) {
+            roe.unexportAll();
+            roe = null;
+        }
+    }
+}
