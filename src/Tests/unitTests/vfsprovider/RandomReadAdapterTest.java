0f0115175fe7c52c8923aa2be809fc37645a6af9
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-26 11:33:36 +0000

    Stream tests added
    Tests check the comatibility of different adapters with stream interface.
    Abstract test cases defined that are overriden if a operation is not
    supported. Some helper methods may be overriden to provide more
    sophisticated test in a specific adapter.
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13325 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/unitTests/vfsprovider/RandomReadAdapterTest.java
@@ -0,0 +1,42 @@
+package unitTests.vfsprovider;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.objectweb.proactive.extra.vfsprovider.exceptions.WrongStreamTypeException;
+import org.objectweb.proactive.extra.vfsprovider.server.RandomAccessStreamAdapter;
+import org.objectweb.proactive.extra.vfsprovider.server.Stream;
+
+public class RandomReadAdapterTest extends AbstractStreamTest {
+
+    @Override
+    protected Stream getInstance(File f) throws Exception {
+        return RandomAccessStreamAdapter.createRandomAccessRead(f);
+    }
+    
+    @Override
+    @Test(expected = WrongStreamTypeException.class)
+    public void writeTest() throws IOException, WrongStreamTypeException {
+        super.writeTest();
+    }
+
+    @Override
+    @Test(expected = WrongStreamTypeException.class)
+    public void flushTest() throws IOException, WrongStreamTypeException {
+        super.flushTest();
+    }
+    
+    @Override
+    @Test(expected = FileNotFoundException.class)
+    public void createFromNotExistingFileTest() throws Exception {
+        super.createFromNotExistingFileTest();
+    }
+
+    @Override
+    protected long changePosition(Stream s) throws Exception {
+        s.seek(10);
+        return 10;
+    }
+}
---
+++ src/Tests/unitTests/vfsprovider/RandomReadWriteAdapterTest.java
@@ -0,0 +1,29 @@
+package unitTests.vfsprovider;
+
+import java.io.File;
+
+import org.objectweb.proactive.extra.vfsprovider.server.RandomAccessStreamAdapter;
+import org.objectweb.proactive.extra.vfsprovider.server.Stream;
+
+public class RandomReadWriteAdapterTest extends AbstractStreamTest {
+
+    @Override
+    protected Stream getInstance(File f) throws Exception {
+        return RandomAccessStreamAdapter.createRandomAccessReadWrite(f);
+    }
+    
+    @Override
+    protected long changePosition(Stream s) throws Exception {
+        s.seek(10);
+        return 10;
+    }
+    
+    @Override
+    protected long changeFileLength(Stream s) throws Exception {
+        s.seek(1);
+        s.write(TEST_FILE_CONTENT.getBytes());
+        s.write(TEST_FILE_CONTENT.getBytes());
+        return 2 * TEST_FILE_CONTENT_LEN + 1; 
+    }
+
+}
