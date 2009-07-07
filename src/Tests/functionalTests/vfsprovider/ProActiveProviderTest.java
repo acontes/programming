23e40785568fabddeaf886a9fc20f3e9b9c8f603
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-07-07 16:41:45 +0000

    Added real ProActiveProviderTest based on VFS generic provider test
    After few patches, it's working!
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13428 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/vfsprovider/ProActiveProviderTest.java
+++ src/Tests/functionalTests/vfsprovider/ProActiveProviderTest.java
@@ -2,32 +2,69 @@ package functionalTests.vfsprovider;
 
 import junit.framework.Test;
 
+import org.apache.commons.AbstractVfsTestCase;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.impl.DefaultFileSystemManager;
 import org.apache.commons.vfs.test.AbstractProviderTestConfig;
 import org.apache.commons.vfs.test.ProviderTestSuite;
+import org.objectweb.proactive.extra.vfsprovider.client.ProActiveFileName;
 import org.objectweb.proactive.extra.vfsprovider.client.ProActiveFileProvider;
+import org.objectweb.proactive.extra.vfsprovider.client.ProActiveFileName.ProActiveProviderScheme;
+import org.objectweb.proactive.extra.vfsprovider.server.FileSystemServerDeployer;
 
 
+/**
+ * Test suite for VFS ProActiveProvider basing on VFS generic provider tests.
+ * <p>
+ * Run with junit3 with java property test.basedir set to VFS test directory (can be found as
+ * test-data directory in VFS tests jar).
+ */
 public class ProActiveProviderTest extends AbstractProviderTestConfig {
+    private FileSystemServerDeployer deployer;
 
     public static Test suite() throws Exception {
-        return new ProviderTestSuite(new ProActiveProviderTest());
+        final ProActiveProviderTest providerTest = new ProActiveProviderTest();
+        return new ProviderTestSuite(providerTest) {
+            @Override
+            protected void setUp() throws Exception {
+                providerTest.setUp();
+                super.setUp();
+            }
+
+            @Override
+            protected void tearDown() throws Exception {
+                super.tearDown();
+                providerTest.tearDown();
+            }
+        };
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        deployer = new FileSystemServerDeployer(AbstractVfsTestCase.getTestDirectory());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        if (deployer != null) {
+            deployer.terminate();
+            deployer = null;
+        }
+    };
 
     @Override
     public void prepare(final DefaultFileSystemManager manager) throws Exception {
         final ProActiveFileProvider provider = new ProActiveFileProvider();
-        manager.addProvider("paprmi", provider);
-        manager.addProvider("paprmissh", provider);
-        manager.addProvider("paphttp", provider);
-        manager.addProvider("papibis", provider);
+        for (ProActiveProviderScheme scheme : ProActiveFileName.ProActiveProviderScheme.values()) {
+            manager.addProvider(scheme.getVFSScheme(), provider);
+        }
     }
 
     @Override
     public FileObject getBaseTestFolder(FileSystemManager fs) throws Exception {
-
-        return super.getBaseTestFolder(fs);
+        final String serverURL = deployer.getRemoteFileSystemServerURL();
+        final String vfsRootURL = ProActiveFileName.getServerVFSRootURL(serverURL);
+        return fs.resolveFile(vfsRootURL);
     }
 }
