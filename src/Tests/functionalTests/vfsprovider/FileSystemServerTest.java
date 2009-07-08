6eda8f71d6e21418285d33bcf764cbfa7573a2ba
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-07-08 08:26:23 +0000

    simple test added for basic server functionality
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13432 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/vfsprovider/FileSystemServerTest.java
@@ -0,0 +1,37 @@
+package functionalTests.vfsprovider;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.objectweb.proactive.extra.vfsprovider.exceptions.StreamNotFoundException;
+import org.objectweb.proactive.extra.vfsprovider.exceptions.WrongStreamTypeException;
+import org.objectweb.proactive.extra.vfsprovider.protocol.StreamMode;
+import org.objectweb.proactive.extra.vfsprovider.server.FileSystemServerImpl;
+
+import unitTests.vfsprovider.AbstractIOOperationsTest;
+
+
+/**
+ * Simple usage test.
+ */
+public class FileSystemServerTest extends AbstractIOOperationsTest {
+
+    @Override
+    public String getTestDirFilename() {
+        return "PROACTIVE-FileSystemServerFunctionalTest";
+    }
+    
+    @Test
+    public void test() throws IOException, StreamNotFoundException, WrongStreamTypeException {
+        FileSystemServerImpl server = new FileSystemServerImpl(testDir.getAbsolutePath());
+        server.startAutoClosing();
+        final String path = "/" + TEST_FILENAME;
+        final long stream = server.streamOpen(path, StreamMode.SEQUENTIAL_READ);
+        final int len = (int) server.fileGetInfo(path).getSize() % Integer.MAX_VALUE;
+        final byte[] content = server.streamRead(stream, len);
+        Assert.assertArrayEquals(TEST_FILE_CONTENT.getBytes(), content);
+        server.streamClose(stream);
+        server.stopServer();
+    }
+}
