b9022fbb4a7f84cdf21da5aa341875f3c84110c2
mzawirsk <mzawirsk@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-07-09 15:53:25 +0000

    Simple example of manual DataSpaces configuration (not working yet)
    Apparently, setting deployment id is not as easy as we thought.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13488 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Examples/org/objectweb/proactive/examples/dataspaces/manualconfig/ManualConfigurationExample.java
@@ -0,0 +1,72 @@
+package org.objectweb.proactive.examples.dataspaces.manualconfig;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.node.Node;
+import org.objectweb.proactive.core.node.NodeFactory;
+import org.objectweb.proactive.core.runtime.ProActiveRuntimeImpl;
+import org.objectweb.proactive.extra.dataspaces.api.DataSpacesFileObject;
+import org.objectweb.proactive.extra.dataspaces.api.PADataSpaces;
+import org.objectweb.proactive.extra.dataspaces.core.DataSpacesNodes;
+import org.objectweb.proactive.extra.dataspaces.core.naming.NamingService;
+import org.objectweb.proactive.extra.dataspaces.core.naming.NamingServiceDeployer;
+import org.objectweb.proactive.extra.dataspaces.exceptions.ApplicationAlreadyRegisteredException;
+import org.objectweb.proactive.extra.dataspaces.exceptions.WrongApplicationIdException;
+
+
+/**
+ * Simple example of manual configuration of DataSpaces for 1 node (without using GCM deployment).
+ */
+public class ManualConfigurationExample {
+    public static void main(String[] args) throws ApplicationAlreadyRegisteredException,
+            WrongApplicationIdException, ProActiveException, URISyntaxException, IOException {
+        // start Naming Service
+        final NamingServiceDeployer namingServiceDeployer = new NamingServiceDeployer();
+        final String namingServiceURL = namingServiceDeployer.getNamingServiceURL();
+        final NamingService remoteNamingService = namingServiceDeployer.getRemoteNamingService();
+
+        // use some unique application id
+        final long applicationId = 88 + 666;
+        // register application, without predefined inputs and outputs 
+        remoteNamingService.registerApplication(applicationId, null);
+
+        // configure node for DS
+        final Node halfBofiesNode = NodeFactory.getHalfBodiesNode();
+
+        // hacky force application id on that node
+        // FIXME oops it doesn't work: we have remote object stub + adapter, we cannot cast so easily...
+        ((ProActiveRuntimeImpl) halfBofiesNode.getProActiveRuntime()).setDeploymentId(applicationId);
+
+        // node is configured without scratch
+        DataSpacesNodes.configureNode(halfBofiesNode, null);
+        DataSpacesNodes.configureApplication(halfBofiesNode, namingServiceURL);
+
+        // now we can use PADataSpaces from AO/bodies on that node.
+        // in case of half-bodies node, we can use PADataSpaces from non-AO
+        PADataSpaces.addDefaultInput("http://www.faqs.org/ftp/rfc/rfc2616.txt", null);
+
+        final DataSpacesFileObject fo = PADataSpaces.resolveDefaultInput();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(fo.getContent().getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } finally {
+            fo.close();
+            if (reader != null) {
+                reader.close();
+            }
+        }
+
+        // after using DS, we can clean up
+        DataSpacesNodes.closeNodeConfig(halfBofiesNode);
+        namingServiceDeployer.terminate();
+        // (actually, this part should be also implemented as finally, as we should always close DS on node and NamingService)
+    }
+}
