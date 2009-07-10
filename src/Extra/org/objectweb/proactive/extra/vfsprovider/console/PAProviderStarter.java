04e4ea67b5a9b9e85b526b6769b4536f6c42f937
mkmiecia <mkmiecia@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-07-10 11:36:47 +0000

    bash script and java code for starting PAProvider manually
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/ProActive_DataSpaces@13493 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extra/org/objectweb/proactive/extra/vfsprovider/console/PAProviderStarter.java
@@ -0,0 +1,79 @@
+package org.objectweb.proactive.extra.vfsprovider.console;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.ProActiveRuntimeException;
+import org.objectweb.proactive.extra.vfsprovider.client.ProActiveFileName;
+import org.objectweb.proactive.extra.vfsprovider.server.FileSystemServerDeployer;
+
+
+public class PAProviderStarter {
+
+    private static String rootDirectory;
+
+    private static String providerName;
+
+    private static FileSystemServerDeployer deployer;
+
+    /**
+     * @param args
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        final String name = PAProviderStarter.class.getName();
+
+        try {
+            parseArgs(args);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Usage: java " + name + " <root directory> [PAProvider name]");
+            System.out.println("       java " + name + " --help");
+            System.out.println("Starts the PA Provider for <root directory> with default or specified name.");
+            System.out.println("\t--help\tprints this screen");
+            return;
+        }
+
+        setupHook();
+        startServer();
+    }
+
+    private static void parseArgs(String[] args) {
+        final int len = args.length;
+
+        if (len == 0 || len > 2 || (len == 1 && "--help".equals(args[0])))
+            throw new IllegalArgumentException();
+
+        rootDirectory = args[0];
+        if (len == 2)
+            providerName = args[1];
+    }
+
+    private static void setupHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    stopServer();
+                } catch (ProActiveException e) {
+                    throw new ProActiveRuntimeException(e);
+                }
+            }
+        });
+    }
+
+    private static void startServer() throws IOException, URISyntaxException {
+        if (providerName == null)
+            deployer = new FileSystemServerDeployer(rootDirectory, true);
+        else
+            deployer = new FileSystemServerDeployer(providerName, rootDirectory, true);
+        
+        final String url = ProActiveFileName.getServerVFSRootURL(deployer.getRemoteFileSystemServerURL());
+        System.out.println("PAProvider successfully started.\nVFS URL of this provider: " + url);
+    }
+
+    private static void stopServer() throws ProActiveException {
+        deployer.terminate();
+    }
+}
