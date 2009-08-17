41bd7cda0af503bdbce82beb3f37683abb8152d1
mmenant <mmenant@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-08-17 14:59:23 +0000

    Add parent informations in the data of the DSITag
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Request_Tagging@13853 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/body/tags/tag/DsiTag.java
+++ src/Core/org/objectweb/proactive/core/body/tags/tag/DsiTag.java
@@ -1,5 +1,6 @@
 package org.objectweb.proactive.core.body.tags.tag;
 
+import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.core.UniqueID;
 import org.objectweb.proactive.core.body.tags.Tag;
 
@@ -18,13 +19,18 @@ public class DsiTag extends Tag {
      * as the tag DATA.
      */
     public DsiTag(UniqueID id, long seq) {
-        super(IDENTIFIER, "" + id.getCanonString() + "::" + seq);
+        super(IDENTIFIER, "" + id.getCanonString() + "::" + seq + "::" + null + "::" + 0 );
     }
 
     /**
      * This tag return itself at each propagation.
      */
     public Tag apply() {
+        // Set the current body and seq number as the parent for the future request
+        String[] dataInfos = data.toString().split("::");
+        String currentId = PAActiveObject.getBodyOnThis().getID().getCanonString();
+        long currentSeq = PAActiveObject.getContext().getCurrentRequest().getSequenceNumber();
+        setData(dataInfos[0]+"::"+dataInfos[1]+"::"+currentId+"::"+currentSeq, true);
         return this;
     }
 
