6ac3465f8b4f0ae7718d6edc2d696b31c864b54e
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-09-01 16:55:39 +0000

    Fix broken factory must implements equals/hashcode
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_PROACTIVE730_PAMRSSH@13957 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/ssh/SshRMIClientSocketFactory.java
+++ src/Core/org/objectweb/proactive/core/ssh/SshRMIClientSocketFactory.java
@@ -17,4 +17,14 @@ public class SshRMIClientSocketFactory implements RMIClientSocketFactory, Serial
     public Socket createSocket(String host, int port) throws IOException {
         return tunnelPool.getSocket(host, port);
     }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.getClass().equals(obj.getClass());
+    }
 }
