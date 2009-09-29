a4e17cf5a22ef18e03e3adc5f674aa2c9bbfbb9e
ffonteno <ffonteno@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-09-29 12:30:31 +0000

    Added ProActive licence information Removed some unnecessary import statements
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_Axis2_CXF@14210 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/webservices/cxf/deployer/PADeployer.java
+++ src/Extensions/org/objectweb/proactive/extensions/webservices/cxf/deployer/PADeployer.java
@@ -1,22 +1,34 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+/*
+ * ################################################################
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
  */
-
 package org.objectweb.proactive.extensions.webservices.cxf.deployer;
 
 import java.lang.reflect.Method;
@@ -33,12 +45,15 @@ import org.objectweb.proactive.extensions.webservices.common.MethodUtils;
 import org.objectweb.proactive.extensions.webservices.cxf.servicedeployer.ServiceDeployerItf;
 
 
+/**
+ * This class is in charge of calling the ServiceDeployer service on hosts specified
+ * by urls and invokes its deploy and unDeploy methods with the needed arguments.
+ *
+ * @author The ProActive Team
+ */
 public final class PADeployer {
 
     private static Logger logger = ProActiveLogger.getLogger(Loggers.WEB_SERVICES);
-
-    public PADeployer() {
-    }
 
     /**
      * From an URL, checks whether it begins with "http://"
