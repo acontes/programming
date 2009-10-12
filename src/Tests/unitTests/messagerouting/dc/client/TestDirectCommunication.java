81736a958039ba9e9f2b46beee6419599a5fbe36
fabratu <fabratu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-12 14:38:02 +0000

    Unit test for two agent communicating directly
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_pamr_1.x@14428 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/unitTests/messagerouting/dc/client/TestDirectCommunication.java
@@ -0,0 +1,99 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package unitTests.messagerouting.dc.client;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.util.Sleeper;
+import org.objectweb.proactive.extra.messagerouting.exceptions.MessageRoutingException;
+import org.objectweb.proactive.extra.messagerouting.protocol.AgentID;
+
+import unitTests.UnitTests;
+import unitTests.messagerouting.dc.scenarios.AgentRouterAgentProbes;
+import unitTests.messagerouting.dc.scenarios.AgentRouterAgentProbes.AgentState;
+
+
+/**
+ * Test whether direct communications between Agents
+ * 	correctly occur
+ *
+ * @author fabratu
+ * @version %G%, %I%
+ * @since ProActive 4.10
+ */
+public class TestDirectCommunication extends UnitTests {
+
+    private AgentRouterAgentProbes infrastructure;
+    private static final long TIMEOUT = 1000;
+
+    @Before
+    public void before() throws IOException, ProActiveException {
+        infrastructure = new AgentRouterAgentProbes();
+        infrastructure.startInfrastructure();
+    }
+
+    // onw-way direct communication
+    // from local agent to remote agent
+    @Test
+    public void testOneway() throws MessageRoutingException {
+        Sleeper paSleeper = new Sleeper(TIMEOUT);
+        AgentID remoteAgentID = infrastructure.getRemoteAgent().getAgentID();
+        // the DC_AD message was sent; test if it was processed by the router
+        paSleeper.sleep();
+        Assert.assertTrue(infrastructure.routerMBean.supportsDirectConnections(remoteAgentID.getId()));
+        // the remote agent is taken into account for DC at message transmission
+        Assert.assertTrue(infrastructure.testRemoteAgentState(remoteAgentID, AgentState.NOT_SEEN));
+        // send a DATA_REQ message with empty payload
+        infrastructure.getLocalAgent().sendMsg(remoteAgentID, null, true); // send it oneway
+        // at this point, the remoteAgentID should have been taken into account by the negotiator
+        Assert.assertTrue(infrastructure.testRemoteAgentState(remoteAgentID, AgentState.SEEN_OR_CONNECTED));
+        // after a while, a direct connection should have been established
+        paSleeper.sleep();
+        Assert.assertTrue(infrastructure.testRemoteAgentState(remoteAgentID, AgentState.CONNECTED));
+        // now, sending a second DATA_REQ message should pass through the direct communication channel
+        infrastructure.getLocalAgent().tunnelShutdown();
+        Assert.assertTrue(infrastructure.getLocalAgent().onewaySend(null, infrastructure.getRemoteAgent(),
+                TIMEOUT));
+        // nothing should change in the Agent state
+        Assert.assertTrue(infrastructure.testRemoteAgentState(remoteAgentID, AgentState.CONNECTED));
+    }
+
+    @After
+    public void after() {
+        infrastructure.stopInfrastructure();
+    }
+}
---
+++ src/Tests/unitTests/messagerouting/dc/scenarios/AgentRouterAgentProbes.java
@@ -0,0 +1,127 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package unitTests.messagerouting.dc.scenarios;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.extra.messagerouting.client.AgentImplMBean;
+import org.objectweb.proactive.extra.messagerouting.protocol.AgentID;
+import org.objectweb.proactive.extra.messagerouting.router.RouterImplMBean;
+
+
+/**
+ * Same as AgentRouterAgent, but with probes for gathering
+ * 	additional information:
+ * 	- Router MBean
+ * 	- MBean for both agents
+ * @author fabratu
+ * @version %G%, %I%
+ * @since ProActive 4.10
+ */
+public class AgentRouterAgentProbes extends AgentRouterAgent {
+
+    public RouterImplMBean routerMBean;
+    public AgentImplMBean agentMBean;
+    public AgentImplMBean r_agentMBean;
+
+    public AgentRouterAgentProbes() {
+        super(true, true);
+    }
+
+    public AgentRouterAgentProbes(boolean b, boolean c) {
+        super(b, c);
+    }
+
+    public void startInfrastructure() throws IOException, ProActiveException {
+        super.startInfrastructure();
+
+        // get the mbeans
+        try {
+            this.routerMBean = (RouterImplMBean) this.router;
+        } catch (ClassCastException e) {
+            logger.error(e.getMessage(), e);
+            Assert.fail(RouterImplMBean.class.getName() +
+                " implementation changed. This unit test should also be re-implemented.");
+        }
+
+        try {
+            this.agentMBean = (AgentImplMBean) this.agent;
+            this.r_agentMBean = (AgentImplMBean) this.r_agent;
+        } catch (ClassCastException e) {
+            logger.error(e.getMessage(), e);
+            Assert.fail(AgentImplMBean.class.getName() +
+                " implementation changed. This unit test should also be re-implemented.");
+        }
+    }
+
+    public boolean testRemoteAgentState(AgentID remoteAgent, AgentState expectedState) {
+        List<String> seenList = Arrays.asList(this.agentMBean.getCandidateAgents());
+        List<String> connectedList = Arrays.asList(this.agentMBean.getOutboundAgents());
+        List<String> failedList = Arrays.asList(this.agentMBean.getFailedAgents());
+        boolean expected;
+        switch (expectedState) {
+            case NOT_SEEN:
+                expected = !seenList.contains(remoteAgent.toString()) &&
+                    !connectedList.contains(remoteAgent.toString()) &&
+                    !failedList.contains(remoteAgent.toString());
+                break;
+            case SEEN:
+                expected = seenList.contains(remoteAgent.toString()) &&
+                    !connectedList.contains(remoteAgent.toString()) &&
+                    !failedList.contains(remoteAgent.toString());
+                break;
+            case SEEN_OR_CONNECTED:
+                expected = seenList.contains(remoteAgent.toString()) ||
+                    connectedList.contains(remoteAgent.toString()) &&
+                    !failedList.contains(remoteAgent.toString());
+                break;
+            case CONNECTED:
+                expected = !seenList.contains(remoteAgent.toString()) &&
+                    connectedList.contains(remoteAgent.toString()) &&
+                    !failedList.contains(remoteAgent.toString());
+                break;
+            default:
+                expected = true;
+                break;
+        }
+        return expected;
+    }
+
+    public static enum AgentState {
+        NOT_SEEN, SEEN, SEEN_OR_CONNECTED, CONNECTED
+    }
+
+}
