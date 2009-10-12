a3a483520f78d1d8b89923ba8d72091c8552d6be
fabratu <fabratu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-12 14:32:26 +0000

    format
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_pamr_1.x@14402 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/messagerouting/router/TestMessageAssembler.java
+++ src/Tests/functionalTests/messagerouting/router/TestMessageAssembler.java
@@ -78,10 +78,10 @@ public class TestMessageAssembler {
 
             // Check is router.handleAsynchronously was called once and only once
             try {
-				router.handleAsynchronouslyCalled(message);
-			} catch (MalformedMessageException e) {
-				Assert.fail("Badly rassembled message - corrupted message: " + e.getMessage());
-			}
+                router.handleAsynchronouslyCalled(message);
+            } catch (MalformedMessageException e) {
+                Assert.fail("Badly rassembled message - corrupted message: " + e.getMessage());
+            }
 
         }
     }
