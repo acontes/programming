e53c0cd67dd46ba6b6c92c9eb8c9d3416d4529d5
zliu <zliu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-05-21 15:48:08 +0000

    SCA style controller for proactive components, SCApropertyController
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ComponentSCAControllers@16198 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/component/control/property/SCAPropertyController.java
+++ src/Core/org/objectweb/proactive/core/component/control/property/SCAPropertyController.java
@@ -1,3 +1,39 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2010 INRIA/University of
+ * 				Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2
+ * or a different license than the GPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
 package org.objectweb.proactive.core.component.control.property;
 
 import org.objectweb.fractal.api.control.AttributeController;
@@ -7,12 +43,6 @@ import org.objectweb.proactive.core.component.control.AbstractPAController;
 import org.objectweb.proactive.core.component.control.PAPropertyController;
 
 
-/**
- * Property control interface for SCA primitive components.
- * Take the interface from Frascati
- * 
- * @author Lionel Seinturier <Lionel.Seinturier@univ-lille1.fr>
- */
 public interface SCAPropertyController {
 
     /** <code>NAME</code> of the content controller. */
@@ -30,7 +60,6 @@ public interface SCAPropertyController {
      * 
      * @param type   the property type
      * @param value  the property value
-     * @since 1.1.1
      */
     public void setType(String name, Class<?> type);
 
@@ -49,7 +78,6 @@ public interface SCAPropertyController {
      * 
      * @param name  the property name
      * @return      the property value
-     * @since 1.1.1
      */
     public Class<?> getType(String name);
 
@@ -74,8 +102,6 @@ public interface SCAPropertyController {
     /**
      * Return the names of the properties whose values have been set by invoking
      * {@link #setValue(String, Object)}.
-     * 
-     * @since 1.1.1
      */
     public String[] getPropertyNames();
 
@@ -86,15 +112,12 @@ public interface SCAPropertyController {
      * @param name  the property name
      * @return      <code>true</code> if the property can be injected,
      *              <code>false</code> otherwise
-     * @since 1.1.2
      */
     public boolean containsDeclaredPropertyName(String name);
 
     /**
      * Return the names of the properties which can be injected in the content
      * class.
-     * 
-     * @since 1.1.1
      */
     public String[] getDeclaredPropertyNames();
 
@@ -104,7 +127,6 @@ public interface SCAPropertyController {
      * 
      * @param name  the property name
      * @return      the property type
-     * @since 1.1.2
      */
     public Class<?> getDeclaredPropertyType(String name);
 
