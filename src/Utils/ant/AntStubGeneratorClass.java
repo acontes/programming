a08ca93c5ca4ae3460ea94de7ca27fb86bca2354
bsauvan <bsauvan@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-01-24 12:03:15 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/SHORT_ComponentSCAControllers@18780 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Utils/ant/AntStubGeneratorClass.java
+++ src/Utils/ant/AntStubGeneratorClass.java
@@ -5,48 +5,52 @@
  *    Parallel, Distributed, Multi-Core Computing for
  *    Enterprise Grids & Clouds
  *
- * Copyright (C) 1997-2010 INRIA/University of 
- * 				Nice-Sophia Antipolis/ActiveEon
+ * Copyright (C) 1997-2011 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
  * Contact: proactive@ow2.org or contact@activeeon.com
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
+ * modify it under the terms of the GNU Affero General Public License
  * as published by the Free Software Foundation; version 3 of
  * the License.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
  *
- * If needed, contact us to obtain a release under GPL Version 2 
- * or a different license than the GPL.
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
  *
- *  Initial developer(s):               The ProActive Team
- *                        http://proactive.inria.fr/team_members.htm
+ *  Initial developer(s):               The ActiveEon Team
+ *                        http://www.activeeon.com/
  *  Contributor(s):
  *
  * ################################################################
- * $$PROACTIVE_INITIAL_DEV$$
+ * $$ACTIVEEON_INITIAL_DEV$$
  */
-package org.objectweb.proactive.examples.scilab.monitor;
-
-import java.util.EventListener;
-
-import org.objectweb.proactive.annotation.PublicAPI;
-
+package ant;
 
 /**
+ * {@link AntStubGenerator}'s nested element.
  *
- * This interface is a listener for Scilab events
+ * Declares and Stores a class name
  *
+ * @author ProActive team
+ * @since  ProActive 4.4.0
  */
-@PublicAPI
-public interface MSEventListener extends EventListener {
-    public void actionPerformed(MSEvent evt);
+public class AntStubGeneratorClass {
+    String name;
+
+    public AntStubGeneratorClass() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
