83b54d9746109728c840815e95b0a25015573b99
zistvan <zistvan@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-07-22 08:35:25 +0000

    Copied but not modified yet: LU, SP, IS, MG
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/oasis_multiactivities_npb@20300 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/multiactivenpb/is/RankThread.java
+++ src/Extra/org/objectweb/proactive/extra/multiactivenpb/is/RankThread.java
@@ -1,46 +1,18 @@
 /*
-!-------------------------------------------------------------------------!
-!									  !
-!	 N  A  S     P A R A L L E L	 B E N C H M A R K S  3.0	  !
-!									  !
-!			J A V A 	V E R S I O N			  !
-!									  !
-!                         R a n k T h r e a d                             !
-!                                                                         !
-!-------------------------------------------------------------------------!
-!                                                                         !
-!    RankThread implements thread for RankThread subroutine of IS         !    
-!    benchmark.                                                           !
-!									  !
-!    Permission to use, copy, distribute and modify this software	  !
-!    for any purpose with or without fee is hereby granted.  We 	  !
-!    request, however, that all derived work reference the NAS  	  !
-!    Parallel Benchmarks 3.0. This software is provided "as is" 	  !
-!    without express or implied warranty.				  !
-!									  !
-!    Information on NPB 3.0, including the Technical Report NAS-02-008	  !
+	NAS PARALLEL BENCHMARKS -- MULTI-ACTIVE OBJECT IMPLEMENTATION
+	
+	This benchmark is based on the original multi-threaded NPB implementation.
+	
+!	Information on NPB 3.0, including the Technical Report NAS-02-008	  !
 !    "Implementation of the NAS Parallel Benchmarks in Java",		  !
 !    original specifications, source code, results and information	  !
 !    on how to submit new results, is available at:			  !
 !									  !
-!	    http://www.nas.nasa.gov/Software/NPB/			  !
-!									  !
-!    Send comments or suggestions to  npb@nas.nasa.gov  		  !
-!									  !
-!	   NAS Parallel Benchmarks Group				  !
-!	   NASA Ames Research Center					  !
-!	   Mail Stop: T27A-1						  !
-!	   Moffett Field, CA   94035-1000				  !
-!									  !
-!	   E-mail:  npb@nas.nasa.gov					  !
-!	   Fax:     (650) 604-3957					  !
-!									  !
-!-------------------------------------------------------------------------!
-! Translation to Java and MultiThreaded Code				  !
-!	   M. Frumkin							  !
-!	   M. Schultz							  !
-!-------------------------------------------------------------------------!
-*/
+!	    http://www.nas.nasa.gov/Software/NPB/	
+!
+
+	Author: Zsolt Istvan (zsolt.istvan@gmx.net) 
+ */
 package org.objectweb.proactive.extra.multiactivenpb.is;
 
 import org.objectweb.proactive.extra.multiactivenpb.kernel.*;
