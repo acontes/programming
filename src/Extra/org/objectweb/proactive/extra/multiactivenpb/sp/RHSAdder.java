8a35f583f92d09a50b8dacd6d367c0710de93743
zistvan <zistvan@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-07-22 13:14:12 +0000

    SP kernel modified. Tested
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/branches/oasis_multiactivities_npb@20313 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/multiactivenpb/sp/RHSAdder.java
+++ src/Extra/org/objectweb/proactive/extra/multiactivenpb/sp/RHSAdder.java
@@ -14,92 +14,105 @@
 	Author: Zsolt Istvan (zsolt.istvan@gmx.net) 
  */
 package org.objectweb.proactive.extra.multiactivenpb.sp;
+
 import org.objectweb.proactive.extra.multiactivenpb.kernel.*;
 
-public class RHSAdder extends SPBase{
-  public int id;
-  public boolean done = true;
+public class RHSAdder extends SPBase {
+	public int id;
+	public boolean done = true;
 
-  //private data
-  int lower_bound, upper_bound;
-  
-  public RHSAdder(SP sp,int low, int high){
-    Init(sp);
-    lower_bound=low;
-    upper_bound=high;
-    setPriority(Thread.MAX_PRIORITY);
-    setDaemon(true);
-    master=sp;
-  }
-  void Init(SP sp){
-    //initialize shared data
-    IMAX=sp.IMAX;
-    JMAX=sp.JMAX; 
-    KMAX=sp.KMAX; 
-    problem_size=sp.problem_size; 
-    nx2=sp.nx2;
-    ny2=sp.ny2;
-    nz2=sp.nz2;
-    grid_points=sp.grid_points;
-    niter_default=sp.niter_default;
-    dt_default=sp.dt_default;    
-    u=sp.u;
-    rhs=sp.rhs;
-    forcing=sp.forcing;
-    isize1=sp.isize1;
-    jsize1=sp.jsize1;
-    ksize1=sp.ksize1;
-    us=sp.us;
-    vs=sp.vs;
-    ws=sp.ws;
-    qs=sp.qs;
-    rho_i=sp.rho_i;
-    speed=sp.speed;
-    square=sp.square;
-    jsize2=sp.jsize2;
-    ksize2=sp.ksize2;
-    ue=sp.ue;
-    buf=sp.buf;
-    jsize3=sp.jsize3;
-    lhs=sp.lhs;
-    lhsp=sp.lhsp;
-    lhsm=sp.lhsm;
-    jsize4=sp.jsize4;
-    cv=sp.cv;
-    rhon=sp.rhon;
-    rhos=sp.rhos;
-    rhoq=sp.rhoq;
-    cuf=sp.cuf;
-    q=sp.q;
-    ce=sp.ce;
-  }
-  
-  public void run(){
-    for(;;){ 
-      synchronized(this){ 
-        while(done==true){
-	  try{
-	    wait();
-	synchronized(master){ master.notify();}
-	  }catch(InterruptedException ie){}
-        }
-        step();
-        synchronized(master){done = true;master.notify();}
-      }
-    }  
-  }
+	// private data
+	int lower_bound, upper_bound;
 
-  public void step(){  
-    int i, j, k, m;
-    for(k=lower_bound;k<=upper_bound;k++){
-      for(j=1;j<=ny2;j++){
-	for(i=1;i<=nx2;i++){
-	  for(m=0;m<=4;m++){
-	    u[m+i*isize1+j*jsize1+k*ksize1] = u[m+i*isize1+j*jsize1+k*ksize1] + 
-	      rhs[m+i*isize1+j*jsize1+k*ksize1];
-	  }
+	public RHSAdder(SP sp, int low, int high) {
+		Init(sp);
+		lower_bound = low;
+		upper_bound = high;
+		setPriority(Thread.MAX_PRIORITY);
+		setDaemon(true);
+		master = sp;
 	}
-      }
-    }    
-  }
+
+	void Init(SP sp) {
+		// initialize shared data
+		IMAX = sp.IMAX;
+		JMAX = sp.JMAX;
+		KMAX = sp.KMAX;
+		problem_size = sp.problem_size;
+		nx2 = sp.nx2;
+		ny2 = sp.ny2;
+		nz2 = sp.nz2;
+		grid_points = sp.grid_points;
+		niter_default = sp.niter_default;
+		dt_default = sp.dt_default;
+		u = sp.u;
+		rhs = sp.rhs;
+		forcing = sp.forcing;
+		isize1 = sp.isize1;
+		jsize1 = sp.jsize1;
+		ksize1 = sp.ksize1;
+		us = sp.us;
+		vs = sp.vs;
+		ws = sp.ws;
+		qs = sp.qs;
+		rho_i = sp.rho_i;
+		speed = sp.speed;
+		square = sp.square;
+		jsize2 = sp.jsize2;
+		ksize2 = sp.ksize2;
+		ue = sp.ue;
+		buf = sp.buf;
+		jsize3 = sp.jsize3;
+		lhs = sp.lhs;
+		lhsp = sp.lhsp;
+		lhsm = sp.lhsm;
+		jsize4 = sp.jsize4;
+		cv = sp.cv;
+		rhon = sp.rhon;
+		rhos = sp.rhos;
+		rhoq = sp.rhoq;
+		cuf = sp.cuf;
+		q = sp.q;
+		ce = sp.ce;
+	}
+
+	/*public void run() {
+		for (;;) {
+			synchronized (this) {
+				while (done == true) {
+					try {
+						wait();
+						synchronized (master) {
+							master.notify();
+						}
+					} catch (InterruptedException ie) {
+					}
+				}
+				step();
+				synchronized (master) {
+					done = true;
+					master.notify();
+				}
+			}
+		}
+	}*/
+	
+	public void runOnce() {
+		step();
+	}
+
+	public void step() {
+		int i, j, k, m;
+		for (k = lower_bound; k <= upper_bound; k++) {
+			for (j = 1; j <= ny2; j++) {
+				for (i = 1; i <= nx2; i++) {
+					for (m = 0; m <= 4; m++) {
+						u[m + i * isize1 + j * jsize1 + k * ksize1] = u[m + i
+								* isize1 + j * jsize1 + k * ksize1]
+								+ rhs[m + i * isize1 + j * jsize1 + k * ksize1];
+					}
+				}
+			}
+		}
+	}
 }
